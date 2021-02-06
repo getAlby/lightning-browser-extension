3fc5c3016d5532a78812a32cdbe16094960f3387
Michael Bumann <hello@michaelbumann.com>
2021-02-06 17:05:17 +0100

    Prettier?!
    all those fancy JS formatting thingies...

--- source/Background/index.js
+++ source/Background/index.js
@@ -1,23 +1,25 @@
-import browser from 'webextension-polyfill';
+import browser from "webextension-polyfill";
 
-import connectors from '../lib/connectors';
+import connectors from "../lib/connectors";
 
 let connector;
 let settings;
 
 const initConnector = async () => {
-  return browser.storage.sync.get(['currentAccount', 'accounts']).then((result) => {
-    const account = result.accounts[result.currentAccount];
-    // TODO: check if an account is configured. Guess this also needs to be done on a different level to make sure we display a settings page if nothing is configured.
-    if (account.connector) {
-      connector = new connectors[account.connector](account.config);
-    } else {
-      connector = new connectors.native(account.config);
-    }
+  return browser.storage.sync
+    .get(["currentAccount", "accounts"])
+    .then((result) => {
+      const account = result.accounts[result.currentAccount];
+      // TODO: check if an account is configured. Guess this also needs to be done on a different level to make sure we display a settings page if nothing is configured.
+      if (account.connector) {
+        connector = new connectors[account.connector](account.config);
+      } else {
+        connector = new connectors.native(account.config);
+      }
 
-    return connector.init();
-  });
-}
+      return connector.init();
+    });
+};
 
 browser.storage.onChanged.addListener((changes) => {
   if (changes.currentAccount) {
@@ -28,28 +30,33 @@ browser.storage.onChanged.addListener((changes) => {
   }
 });
 
-browser.storage.sync.get(['currentAccount', 'accounts', 'settings']).then(async (result) => {
-  settings = result.settings || {};
-  await initConnector();
+browser.storage.sync
+  .get(["currentAccount", "accounts", "settings"])
+  .then(async (result) => {
+    settings = result.settings || {};
+    await initConnector();
 
-  // listen to calls from the content script and pass it on to the native application
-  // returns a promise to be handled in the content script
-  browser.runtime.onMessage.addListener((message, sender) => {
-    if (settings.debug) {
-      console.log('Background onMessage: ', message, sender);
-    }
-    // if the application does not match or if it is not a prompt we ignore the call
-    if (message.application !== 'Joule' || !message.prompt) {
-      return false;
-    }
+    // listen to calls from the content script and pass it on to the native application
+    // returns a promise to be handled in the content script
+    browser.runtime.onMessage.addListener((message, sender) => {
+      if (settings.debug) {
+        console.log("Background onMessage: ", message, sender);
+      }
+      // if the application does not match or if it is not a prompt we ignore the call
+      if (message.application !== "Joule" || !message.prompt) {
+        return false;
+      }
 
-    const call = connector[message.type]({args: message.args, origin: message.origin});
-    if (settings.debug) {
-      call.then(r => {
-        console.log('Connector response:', r);
-        return r
-      })
-    }
-    return call;
+      const call = connector[message.type]({
+        args: message.args,
+        origin: message.origin,
+      });
+      if (settings.debug) {
+        call.then((r) => {
+          console.log("Connector response:", r);
+          return r;
+        });
+      }
+      return call;
+    });
   });
-});
--- source/webln/index.js
+++ source/webln/index.js
@@ -1,5 +1,6 @@
-export default class WebLNProvider {
+import { message } from "antd";
 
+export default class WebLNProvider {
   constructor() {
     this.isEnabled = false;
     this.isExecuting = false;
@@ -7,9 +8,9 @@ export default class WebLNProvider {
 
   enable() {
     if (this.isEnabled) {
-      return Promise.resolve({enabled: true});
+      return Promise.resolve({ enabled: true });
     }
-    return this.execute('enable').then(result => {
+    return this.execute("enable").then((result) => {
       this.isEnabled = result.enabled;
       return result;
     });
@@ -17,35 +18,35 @@ export default class WebLNProvider {
 
   getInfo() {
     if (!this.isEnabled) {
-      throw new Error('Provider must be enabled before calling getInfo');
+      throw new Error("Provider must be enabled before calling getInfo");
     }
-    return this.execute('getInfo');
+    return this.execute("getInfo");
   }
 
   sendPayment(paymentRequest) {
     if (!this.isEnabled) {
-      throw new Error('Provider must be enabled before calling sendPayment');
+      throw new Error("Provider must be enabled before calling sendPayment");
     }
-    return this.execute('sendPayment', { paymentRequest });
+    return this.execute("sendPayment", { paymentRequest });
   }
 
   makeInvoice(args) {
     if (!this.isEnabled) {
-      throw new Error('Provider must be enabled before calling makeInvoice');
+      throw new Error("Provider must be enabled before calling makeInvoice");
     }
-    if (typeof args !== 'object') {
+    if (typeof args !== "object") {
       args = { amount: args };
     }
 
-    return this.execute('makeInvoice', args);
+    return this.execute("makeInvoice", args);
   }
 
   signMessage(message) {
     if (!this.isEnabled) {
-      throw new Error('Provider must be enabled before calling signMessage');
+      throw new Error("Provider must be enabled before calling signMessage");
     }
 
-    return this.execute('signMessage', { message });
+    return this.execute("signMessage", { message });
   }
 
   execute(type, args) {
@@ -54,33 +55,39 @@ export default class WebLNProvider {
       // in page script can not directly connect to the background script
       window.postMessage(
         {
-          application: 'Joule',
+          application: "Joule",
           prompt: true,
           type,
           args,
         },
-        '*',
+        "*"
       );
 
       function handleWindowMessage(messageEvent) {
         // check if it is a relevant message
         // there are some other events happening
-        if (!messageEvent.data || !messageEvent.data.response || messageEvent.data.application !== 'Joule') {
+        if (
+          !messageEvent.data ||
+          !messageEvent.data.response ||
+          messageEvent.data.application !== "Joule"
+        ) {
           return;
         }
         if (messageEvent.data.data.error) {
           reject(messageEvent.data.data.error);
         } else {
+          console.log(messageEvent);
+          console.log(messageEvent.data);
           // 1. data: the message data
           // 2. data: the data passed as data to the message
           // 3. data: the actual response data
           resolve(messageEvent.data.data.data);
         }
         // For some reason must happen only at the end of this function
-        window.removeEventListener('message', handleWindowMessage);
+        window.removeEventListener("message", handleWindowMessage);
       }
 
-      window.addEventListener('message', handleWindowMessage);
+      window.addEventListener("message", handleWindowMessage);
     });
   }
 }
