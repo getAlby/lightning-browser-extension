171d731d84f892e31a24423382c2df3b72173735
Michael Bumann <hello@michaelbumann.com>
2021-03-28 17:36:02 +0200

    Provide node defaults
    This makes development easier

--- source/forms/lnd/index.js
+++ source/forms/lnd/index.js
@@ -31,8 +31,9 @@ const LndForm = ({ ref, saveLndAccount, addLndAccountFailure }) => {
       onFinish={(values) => saveLndAccount(values, form)}
       initialValues={{
         name: "LND",
-        url: "",
-        macaroon: "",
+        macaroon:
+          "0201036C6E6402F801030A10A20DB3BCABE52F0186FAFB6CD5A79FED1201301A160A0761646472657373120472656164120577726974651A130A04696E666F120472656164120577726974651A170A08696E766F69636573120472656164120577726974651A210A086D616361726F6F6E120867656E6572617465120472656164120577726974651A160A076D657373616765120472656164120577726974651A170A086F6666636861696E120472656164120577726974651A160A076F6E636861696E120472656164120577726974651A140A057065657273120472656164120577726974651A180A067369676E6572120867656E657261746512047265616400000620AE1050A1B1EDA68D723F2AE0EC4561552E1F2507EFB552F86C3D7DE708BC7E1A",
+        url: "https://regtest-bob.nomadiclabs.net",
       }}
     >
       <FormItem
@@ -41,7 +42,7 @@ const LndForm = ({ ref, saveLndAccount, addLndAccountFailure }) => {
         rules={[
           {
             required: true,
-            message: "Please input your username!",
+            message: "Please give your account a name",
           },
         ]}
       >
@@ -54,7 +55,7 @@ const LndForm = ({ ref, saveLndAccount, addLndAccountFailure }) => {
         rules={[
           {
             required: true,
-            message: "Please input the macroon key!",
+            message: "Please enter the macaroon",
           },
         ]}
       >
@@ -68,7 +69,7 @@ const LndForm = ({ ref, saveLndAccount, addLndAccountFailure }) => {
           {
             type: "url",
             required: true,
-            message: "Please enter the macroon url!",
+            message: "Please enter the node url",
           },
         ]}
       >
