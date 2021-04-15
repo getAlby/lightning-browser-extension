b989305cfc3e64ebae3f749c96f94c52660e0b24
Vlad Stan <stan.v.vlad@gmail.com>
2021-04-15 11:11:28 +0300

    #39 add Set Current button

--- src/app/components/ListData/index.jsx
+++ src/app/components/ListData/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { List } from "antd";
+import { List, Button } from "antd";
 
 const ListData = ({ data, title, onResetCallback }) => {
   return data.length > 0 ? (
@@ -10,7 +10,13 @@ const ListData = ({ data, title, onResetCallback }) => {
         itemLayout="horizontal"
         bordered
         renderItem={(item) => (
-          <List.Item key={item.id} actions={[<a key="123">more</a>]}>
+          <List.Item
+            key={item.id}
+            actions={[
+              <Button type="primary">Set Current</Button>,
+              <a key="123">more</a>,
+            ]}
+          >
             <List.Item.Meta title={item.name} description={item.description} />
           </List.Item>
         )}
