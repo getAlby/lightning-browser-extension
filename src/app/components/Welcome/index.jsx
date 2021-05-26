2166714a39592b5f07169005e3b44d301cc9e01a
shawkisukkar <shawkisukkar@gmail.com>
2021-05-26 13:00:56 +0300

    1/n

--- src/app/components/Welcome/index.jsx
+++ src/app/components/Welcome/index.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
-
+import '../../../index.css';
 import Welcome from "./Welcome";
 
 ReactDOM.render(<Welcome />, document.getElementById("welcome-root"));
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
