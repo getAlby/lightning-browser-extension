1d6dd7fda69510e56cec83ffe89a55f966ea3530
Dylan Companjen <dylancompanjen@gmail.com>
2021-06-14 13:01:23 +0200

    feat: added some initial storybook stories

---
+++ src/app/components/ListData/ListData.stories.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import ListData from ".";
+
+const metadata = {
+  title: "Components/ListData",
+  component: ListData,
+};
+
+export default metadata;
+
+const Template = (args) => (
+  <ListData {...args} onResetCallback={() => alert("Reset callback fn")} />
+);
+
+//👇 Each story then reuses that template
+export const Default = Template.bind({});
+
+Default.args = {
+  data: [
+    {
+      title: "John Doe",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      title: "Karren Koe",
+      description:
+        "Etiam fermentum, nisi ac rhoncus auctor, nibh dui lacinia leo, eu laoreet erat dolor quis felis.",
+    },
+    {
+      title: "Mark Moe",
+      description: "Etiam in blandit augue.",
+    },
+  ],
+  title: "List title",
+};
