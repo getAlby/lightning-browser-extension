413dff9b4e5ea1d0427df6ab1534999ec48a07ed
Dylan Companjen <dylancompanjen@gmail.com>
2021-06-15 21:06:06 +0200

    feat: add inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
       },
       colors: {
         "orange-bitcoin": "#f7931a",
