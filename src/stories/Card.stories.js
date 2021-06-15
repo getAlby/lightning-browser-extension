c56f78550615191ec444da99199386a92a162e69
Shawki Sukkar <57439827+ShawkiS@users.noreply.github.com>
2021-06-15 12:24:25 +0300

    Change card props

--- src/stories/Card.stories.js
+++ src/stories/Card.stories.js
@@ -2,7 +2,7 @@ import React from "react";
 import Card from "../app/components/Onboard/Shared/card";
 import '../app/styles/index.css';
 
-export const Primary = () => <Card topLabel="HeroNode" heading="12,350,283 SATS" lastLabel="€ 32,480.56"/>;
+export const Primary = () => <Card color="red-bitcoin" alias="HeroNode" satoshis="12350283" fiat="32480.56" currency="EUR"/>;
 
 export default {
   title: "Components/Card",
