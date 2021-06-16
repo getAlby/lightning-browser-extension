653de5bb0efae67da366ba9ae139999220a37e06
Dylan Companjen <dylancompanjen@gmail.com>
2021-06-16 12:38:25 +0200

    refactor: add some negative margin to improve icon alignment

--- src/app/components/Appbar/index.jsx
+++ src/app/components/Appbar/index.jsx
@@ -7,7 +7,7 @@ export default function Appbar({ title, subtitle, onOptionsClick }) {
   return (
     <div className="px-5 space-x-4 py-2 flex items-center border-b border-gray-100">
       <img
-        className="w-7 h-7 opacity-50"
+        className="-ml-1 w-7 h-7 opacity-50"
         src={WalletIcon}
         alt=""
         aria-hidden="true"
@@ -20,7 +20,12 @@ export default function Appbar({ title, subtitle, onOptionsClick }) {
         className="ml-auto focus:outline-none transition duration-200 opacity-50 hover:opacity-100"
         onClick={onOptionsClick}
       >
-        <img className="w-7 h-7" src={GearIcon} alt="" aria-hidden="true" />
+        <img
+          className="-mr-1 w-7 h-7"
+          src={GearIcon}
+          alt=""
+          aria-hidden="true"
+        />
       </button>
     </div>
   );
