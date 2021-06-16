f2479876b8119bdd651abb7107515ed32dd62c04
Michael Bumann <hello@michaelbumann.com>
2021-06-16 14:51:38 +0200

    No more salt
    We expect a strong password

--- src/common/lib/crypto.js
+++ src/common/lib/crypto.js
@@ -1,10 +1,10 @@
 import { AES, enc } from "crypto-js";
 
 export function encryptData(data, password, salt) {
-  return AES.encrypt(JSON.stringify(data), password + salt).toString();
+  return AES.encrypt(JSON.stringify(data), password).toString();
 }
 
 export function decryptData(cipher, password, salt) {
-  const decrypted = AES.decrypt(cipher, password + salt);
+  const decrypted = AES.decrypt(cipher, password);
   return JSON.parse(decrypted.toString(enc.Utf8));
 }
