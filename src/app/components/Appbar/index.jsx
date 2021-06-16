4efa61c4ff9c33eb28a8726acd03d16e396fc7a4
Dylan Companjen <dylancompanjen@gmail.com>
2021-06-16 12:32:59 +0200

    feature: added bitcoin design icons

--- src/app/components/Appbar/index.jsx
+++ src/app/components/Appbar/index.jsx
@@ -1,17 +1,26 @@
 import React from "react";
-import { CashIcon, CogIcon } from "@heroicons/react/outline";
 import Skeleton from "react-loading-skeleton";
+import WalletIcon from "@bitcoin-design/bitcoin-icons/svg/outline/wallet.svg";
+import GearIcon from "@bitcoin-design/bitcoin-icons/svg/outline/gear.svg";
 
 export default function Appbar({ title, subtitle, onOptionsClick }) {
   return (
     <div className="px-5 space-x-4 py-2 flex items-center border-b border-gray-100">
-      <CashIcon className="h-6 w-6 text-gray-400" aria-hidden="true" />
+      <img
+        className="w-7 h-7 opacity-50"
+        src={WalletIcon}
+        alt=""
+        aria-hidden="true"
+      />
       <div className="flex-auto">
         <div className="text-xs text-gray-500">{title || <Skeleton />}</div>
         <div className="text-xs">{subtitle || <Skeleton />}</div>
       </div>
-      <button className="ml-auto" onClick={onOptionsClick}>
-        <CogIcon className="h-6 w-6 text-gray-400" aria-hidden="true" />
+      <button
+        className="ml-auto focus:outline-none transition duration-200 opacity-50 hover:opacity-100"
+        onClick={onOptionsClick}
+      >
+        <img className="w-7 h-7" src={GearIcon} alt="" aria-hidden="true" />
       </button>
     </div>
   );
--- webpack.config.js
+++ webpack.config.js
@@ -103,6 +103,10 @@ module.exports = {
         ],
       },
       {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
       },
