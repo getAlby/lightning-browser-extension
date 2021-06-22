6f822a10df3bc9554cc4a1211520139c684860da
Dylan Companjen <dylancompanjen@gmail.com>
2021-06-22 14:02:13 +0200

    feat: allow story customization

--- src/app/components/PublisherCard/PublisherCard.stories.tsx
+++ src/app/components/PublisherCard/PublisherCard.stories.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import PublisherCard from "./";
 
-export const Default = () => (
-  <PublisherCard
-    title="The Biz with John Carvalho"
-    image="https://picsum.photos/id/39/400/400"
-  />
-);
+const Template = (args) => <PublisherCard {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  title: "The Biz with John Carvalho",
+  image: "https://picsum.photos/id/39/400/400",
+};
 
 export default {
   title: "Components/PublisherCard",
