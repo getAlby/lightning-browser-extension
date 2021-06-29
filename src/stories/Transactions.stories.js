a906c08c068c99b5645c9341de1e16402ea2c06e
Dylan Companjen <dylancompanjen@gmail.com>
2021-06-29 15:28:55 +0200

    fix: stories that use webextension-polyfill

--- src/stories/Transactions.stories.js
+++ src/stories/Transactions.stories.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Transactions from "../app/components/Wallet/Transactions/index";
-import '../app/styles/index.css';
+import "../app/styles/index.css";
 
 export const Primary = () => <Transactions />;
 
 export default {
-  title: "Pages/Wallet/Transactions",
+  title: "Screens/Wallet/Transactions",
   component: Transactions,
 };
