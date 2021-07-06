fbe663c54ba13c5ec8d7221fb1ca1580ef01a846
Michael Bumann <hello@michaelbumann.com>
2021-07-06 12:16:53 +0200

    Remove delete publisher function from index page
    I think it is better to only offer that on the show page

--- src/app/screens/Publishers.tsx
+++ src/app/screens/Publishers.tsx
@@ -55,7 +55,6 @@ function Publishers() {
       <PublishersTable
         publishers={data}
         navigateToPublisher={navigateToPublisher}
-        deletePublisher={deletePublisher}
       />
     </>
   );
