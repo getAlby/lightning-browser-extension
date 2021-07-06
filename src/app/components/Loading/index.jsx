dfbbd4d1adc9e5ccc2d54a58a1f94dad5d709754
Michael Bumann <hello@michaelbumann.com>
2021-07-07 00:51:15 +0200

    Fix some warning

--- src/app/components/Loading/index.jsx
+++ src/app/components/Loading/index.jsx
@@ -14,7 +14,7 @@ function Loading() {
         cy="12"
         r="10"
         stroke="currentColor"
-        stroke-width="4"
+        strokeWidth="4"
       ></circle>
       <path
         className="opacity-75"
