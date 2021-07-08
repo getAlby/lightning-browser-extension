f03ea0379c73b919d1bf3483941c1ebf1c8a6ddc
Dylan Companjen <dylancompanjen@gmail.com>
2021-07-08 18:10:19 +0200

    fix: force storybook to use postcss8 instea of 7

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,18 @@ const path = require("path");
 
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
+  ],
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\,css&/,
