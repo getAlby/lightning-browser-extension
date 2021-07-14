34380365f0e16446c2e2b02c2356ea79d1b160f4
Dylan Companjen <dylancompanjen@gmail.com>
2021-07-14 15:14:21 +0200

    feat: remove unused code

--- src/extension/background-script/actions/accounts/lock.js
+++ src/extension/background-script/actions/accounts/lock.js
@@ -1,15 +1,8 @@
-import { decryptData } from "../../../../common/lib/crypto";
 import state from "../../state";
 
 const lock = (message, sender) => {
   state.getState().lock();
   return Promise.resolve({ data: { unlocked: false } });
-  // return state
-  //   .getState()
-  //   .lock()
-  //   .then(() => {
-  //     return Promise.resolve({ data: { unlocked: false } });
-  //   });
 };
 
 export default lock;
