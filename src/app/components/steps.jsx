488ee7ac0f3d8b29796a174a7356056a943ec0ef
Dylan Companjen <dylancompanjen@gmail.com>
2021-08-05 17:57:32 +0200

    feat: add support for steps without links

--- src/app/components/steps.jsx
+++ src/app/components/steps.jsx
@@ -1,41 +1,58 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Steps(props) {
+export default function Steps({ steps }) {
+  function createStep(step) {
+    let outerStyles = "";
+    let innerStyles = "";
+    switch (step.status) {
+      case "complete":
+        outerStyles =
+          "group pl-4 py-2 flex flex-col border-l-4 border-orange-bitcoin md:pl-0 md:pt-4 md:pb-0 md:border-l-0 md:border-t-4";
+        innerStyles =
+          "text-xs text-orange-bitcoin font-semibold tracking-wide uppercase";
+        break;
+      case "current":
+        outerStyles =
+          "pl-4 py-2 flex flex-col border-l-4 border-orange-bitcoin md:pl-0 md:pt-4 md:pb-0 md:border-l-0 md:border-t-4";
+        innerStyles =
+          "text-xs text-orange-bitcoin font-semibold tracking-wide uppercase";
+        break;
+      default:
+        outerStyles =
+          "group pl-4 py-2 flex flex-col border-l-4 border-gray-200 md:pl-0 md:pt-4 md:pb-0 md:border-l-0 md:border-t-4";
+        innerStyles =
+          "text-xs text-gray-500 font-semibold tracking-wide uppercase";
+        if (step.href) {
+          outerStyles += " hover:border-gray-300";
+          innerStyles += " group-hover:text-gray-700";
+        }
+        break;
+    }
+    if (step.href) {
+      return (
+        <Link
+          to={step.href}
+          className={outerStyles}
+          aria-current={step.status === "current" ? "step" : ""}
+        >
+          <span className={innerStyles}>{step.id}</span>
+        </Link>
+      );
+    }
+    return (
+      <div className={outerStyles}>
+        <span className={innerStyles}>{step.id}</span>
+      </div>
+    );
+  }
+
   return (
     <nav className="mt-5" aria-label="Progress">
       <ol className="md:flex md:space-y-0 md:space-x-8">
-        {props.steps.map((step) => (
+        {steps.map((step) => (
           <li key={step.id} className="md:flex-1">
-            {step.status === "complete" ? (
-              <Link
-                to={step.href}
-                className="group pl-4 py-2 flex flex-col border-l-4 border-orange-bitcoin hover:border-hover-orange-bitcoin md:pl-0 md:pt-4 md:pb-0 md:border-l-0 md:border-t-4"
-              >
-                <span className="text-xs text-orange-bitcoin font-semibold tracking-wide uppercase group-hover:text-hover-orange-bitcoin">
-                  {step.id}
-                </span>
-              </Link>
-            ) : step.status === "current" ? (
-              <Link
-                to={step.href}
-                className="pl-4 py-2 flex flex-col border-l-4 border-orange-bitcoin md:pl-0 md:pt-4 md:pb-0 md:border-l-0 md:border-t-4"
-                aria-current="step"
-              >
-                <span className="text-xs text-orange-bitcoin font-semibold tracking-wide uppercase">
-                  {step.id}
-                </span>
-              </Link>
-            ) : (
-              <Link
-                to={step.href}
-                className="group pl-4 py-2 flex flex-col border-l-4 border-gray-200 hover:border-gray-300 md:pl-0 md:pt-4 md:pb-0 md:border-l-0 md:border-t-4"
-              >
-                <span className="text-xs text-gray-500 font-semibold tracking-wide uppercase group-hover:text-gray-700">
-                  {step.id}
-                </span>
-              </Link>
-            )}
+            {createStep(step)}
           </li>
         ))}
       </ol>
