95c5b000643c97603ab9a08aa85017e5d13f3258
Michael Bumann <hello@michaelbumann.com>
2021-08-05 18:40:50 +0200

    sendPayments with lnbits

--- src/extension/background-script/connectors/lnbits.js
+++ src/extension/background-script/connectors/lnbits.js
@@ -1,4 +1,5 @@
 import Base from "./base";
+import { parsePaymentRequest } from "invoices";
 
 class LnBits extends Base {
   getInfo() {
@@ -33,13 +34,38 @@ class LnBits extends Base {
     });
   }
 
-  sendPayment(message) {
-    return super.sendPayment(message, () => {
-      this.request("POST", "/api/v1/payments", {
-        bolt11: message.args.paymentRequest,
-        out: true,
-      });
+  sendPayment(args) {
+    const paymentRequestDetails = parsePaymentRequest({
+      request: args.paymentRequest,
     });
+    const amountInSats = parseInt(paymentRequestDetails.tokens);
+    return this.request("POST", "/api/v1/payments", this.config.adminkey, {
+      bolt11: args.paymentRequest,
+      out: true,
+    })
+      .then((data) => {
+        // TODO: how do we get the total amount here??
+        return this.checkPayment(data.checking_id).then((checkData) => {
+          return {
+            data: {
+              payment_preimage: checkData.preimage,
+              payment_hash: data.payment_hash,
+              payment_route: { total_amt: amountInSats, total_fees: 0 },
+            },
+          };
+        });
+      })
+      .catch((e) => {
+        return { error: e.message };
+      });
+  }
+
+  checkPayment(checkingId) {
+    return this.request(
+      "GET",
+      `/api/v1/payments/${checkingId}`,
+      this.config.readkey
+    );
   }
 
   async request(method, path, apiKey, args, defaultValues) {
