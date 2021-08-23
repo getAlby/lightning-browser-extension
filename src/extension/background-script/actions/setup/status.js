7dac60378a0c02482061b009ff28cdb157a649fe
Michael Bumann <hello@michaelbumann.com>
2021-08-23 13:04:38 +0200

    Fix status check
    configured checks if an account actually exists

--- src/extension/background-script/actions/setup/status.js
+++ src/extension/background-script/actions/setup/status.js
@@ -2,8 +2,9 @@ import state from "../../state";
 
 const status = (message, sender) => {
   const unlocked = state.getState().password !== null;
+  const account = state.getState().getAccount();
   const currentAccountId = state.getState().currentAccountId;
-  const configured = currentAccountId !== null;
+  const configured = account != null;
 
   return Promise.resolve({
     data: {
