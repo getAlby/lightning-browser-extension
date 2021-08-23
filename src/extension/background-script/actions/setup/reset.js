f032e60d000e95fa1ffdd2f51d4a51347031ec71
Michael Bumann <hello@michaelbumann.com>
2021-08-23 14:36:30 +0200

    Cleanup

--- src/extension/background-script/actions/setup/reset.js
+++ src/extension/background-script/actions/setup/reset.js
@@ -1,14 +1,14 @@
-import browser from "webextension-polyfill";
 import state from "../../state";
 
 const reset = async (message, sender) => {
   state.setState({
     settings: { debug: true },
     accounts: {},
+    account: null,
+    connector: null,
     password: null,
     currentAccountId: null,
   });
-  await browser.storage.sync.set({ allowances: [] });
 
   return { data: { reset: true } };
 };
