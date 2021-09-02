210f53931f302294128f57017012cfbb285b3e0d
Dylan Companjen <dylancompanjen@gmail.com>
2021-09-02 09:55:53 +0200

    feat: allow changing camera

---
+++ src/app/components/QrcodeScanner/QrcodeScanner.stories.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import QrcodeScanner from ".";
+
+export const Default = () => (
+  <div className="max-w-2xl">
+    <QrcodeScanner />
+  </div>
+);
+
+export default {
+  title: "Components/QrcodeScanner",
+  component: QrcodeScanner,
+};
--- src/app/components/QrcodeScanner/index.jsx
+++ src/app/components/QrcodeScanner/index.jsx
@@ -10,49 +10,64 @@ function QrcodeScanner({
   qrCodeErrorCallback,
 }) {
   const [isScanning, setScanning] = useState(false);
+  const [cameras, setCameras] = useState([]);
+  const [selectedCamera, setSelectedCamera] = useState("");
   const html5QrCodeRef = useRef(null);
 
   useEffect(() => {
-    return handleStopScanning;
+    return () => {
+      handleStopScanning(false);
+    };
   }, []);
 
   async function handleRequestCameraPermissions() {
     try {
-      const devices = await Html5Qrcode.getCameras();
+      const devices = await window.Html5Qrcode.getCameras(); // Request camera permissions.
       if (devices && devices.length) {
-        setScanning(true);
-        html5QrCodeRef.current = new Html5Qrcode("reader");
-        html5QrCodeRef.current
-          .start(
-            devices[0].id,
-            {
-              fps,
-              qrbox,
-            },
-            (decodedText, decodedResult) => {
-              handleStopScanning();
-              qrCodeSuccessCallback(decodedText);
-            },
-            (errorMessage) => {
-              // qrCodeErrorCallback(errorMessage);
-            }
-          )
-          .catch((e) => {
-            // Start failed.
-          });
+        setCameras(devices);
+        html5QrCodeRef.current = new window.Html5Qrcode("reader");
+        handleStartScanning(devices[0].id);
       }
     } catch (error) {
       // handle error
     }
   }
 
-  function handleStopScanning() {
+  async function handleStartScanning(id) {
+    setScanning(true);
+    setSelectedCamera(id);
+    try {
+      await html5QrCodeRef.current.stop();
+      html5QrCodeRef.current
+        .start(
+          id,
+          {
+            fps,
+            qrbox,
+          },
+          (decodedText, decodedResult) => {
+            handleStopScanning();
+            qrCodeSuccessCallback(decodedText);
+          },
+          (errorMessage) => {
+            // qrCodeErrorCallback(errorMessage);
+          }
+        )
+        .catch((e) => {
+          // Start failed.
+        });
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+
+  function handleStopScanning(isMounted = true) {
     if (html5QrCodeRef.current) {
       html5QrCodeRef.current
         .stop()
         .then((ignore) => {
-          setScanning(false);
           html5QrCodeRef.current.clear();
+          if (isMounted) setScanning(false);
         })
         .catch((e) => {
           // Stop failed.
@@ -79,7 +94,22 @@ function QrcodeScanner({
       <div className="bg-black w-full" id="reader" />
 
       {isScanning && (
-        <div className="my-6">
+        <div className="my-6 text-center">
+          <div className="mb-4">
+            <select
+              className="w-72 border-gray-300 rounded-md shadow-sm text-gray-700 bg-white hover:bg-gray-50"
+              name="cameras"
+              id="cameras"
+              value={selectedCamera}
+              onChange={(e) => handleStartScanning(e.target.value)}
+            >
+              {cameras.map((camera) => (
+                <option key={camera.id} value={camera.id}>
+                  {camera.label}
+                </option>
+              ))}
+            </select>
+          </div>
           <Button label="Stop scanning" onClick={handleStopScanning} />
         </div>
       )}
