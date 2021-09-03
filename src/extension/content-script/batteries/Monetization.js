736bed015b1d4a82f66256ef6358d213f60b4f6a
Michael Bumann <hello@michaelbumann.com>
2021-09-03 02:39:39 +0200

    Fix monetization meta tag extraction

--- src/extension/content-script/batteries/Monetization.js
+++ src/extension/content-script/batteries/Monetization.js
@@ -1,23 +1,24 @@
 import getOriginData from "../../content-script/originData";
 
-const urlMatcher = /^http?:\/\/.*/;
+const urlMatcher = /^https?:\/\/.*/i;
 
-const battery = async () => {
+const battery = () => {
   const monetizationTag = document.querySelector(
     'head > meta[name="monetization"][content^="lightning:" i]'
   );
   if (!monetizationTag) {
-    return;
+    return Promise.resolve();
   }
-  const recipient = monetizationTag.content;
+  const recipient = monetizationTag.content.replace(/lightning:/i, "");
   const metaData = getOriginData();
-  return [
+
+  return Promise.resolve([
     {
-      method: "lnurlp",
+      method: "lnurl",
       recipient: recipient,
       ...metaData,
     },
-  ];
+  ]);
 };
 
 const Monetization = {
