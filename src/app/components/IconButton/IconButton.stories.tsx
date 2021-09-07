e866233a4a553c75d23e2fc21f7d79d84fae53e1
Dylan Companjen <dylancompanjen@gmail.com>
2021-09-07 17:13:17 +0200

    feat: add stories for new components

---
+++ src/app/components/IconButton/IconButton.stories.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import CaretLeftIcon from "@bitcoin-design/bitcoin-icons/svg/filled/caret-left.svg";
+
+import IconButton from ".";
+
+export const Default = () => (
+  <IconButton
+    onClick={() => alert("Go back")}
+    icon={
+      <img className="w-4 h-4" src={CaretLeftIcon} alt="" aria-hidden="true" />
+    }
+  />
+);
+
+export default {
+  title: "Components/Buttons/IconButton",
+  component: IconButton,
+};
--- src/app/components/IconButton/index.tsx
+++ src/app/components/IconButton/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function IconButton({ onClick, icon }) {
   return (
     <button
-      className="flex justify-center items-center w-8 h-8 bg-gray-50 rounded-md border border-gray-100 hover:bg-gray-100 hover:border-gray-200 transition-colors duration-200"
+      className="flex justify-center items-center w-8 h-8 bg-gray-50 rounded-md border border-gray-200 hover:bg-gray-100 transition-colors duration-200"
       onClick={onClick}
     >
       {icon}
