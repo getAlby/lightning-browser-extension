e9964df9da3374d4845e4b37c1d5ac7238838103
Dylan Companjen <dylancompanjen@gmail.com>
2021-11-09 16:08:59 +0100

    refactor: replace promises by messaging

--- src/extension/content-script/batteries/index.ts
+++ src/extension/content-script/batteries/index.ts
@@ -1,31 +1,18 @@
-import utils from "../../../common/lib/utils";
 // import GitHubRepo from "./GitHubRepo";
 import Monetization from "./Monetization";
 import Twitter from "./Twitter";
 import YouTubeVideo from "./YouTubeVideo";
 // import YouTubeChannel from "./YouTubeChannel";
 
-const enhancements = [Monetization, Twitter, YouTubeVideo];
+const enhancements = [Twitter, YouTubeVideo, Monetization];
 
-async function LBE_EXTRACT_LIGHTNING_DATA() {
-  let lightningData = null;
-
-  // get maching extractors/enhancements for the current URL
-  // NOTE: this does not mean that data can be found. Because of that we run all possible ones
-  const matching = enhancements.filter((e) =>
+async function extractLightningData() {
+  const match = enhancements.find((e) =>
     document.location.toString().match(e.urlMatcher)
   );
 
-  const batteriesRunning = matching.map((enhancement) => {
-    return enhancement.battery().then((data) => {
-      if (data) {
-        lightningData = data;
-        utils.call("setIcon", { icon: "active" });
-      }
-    });
-  });
-
-  await Promise.all(batteriesRunning);
-  return lightningData;
+  if (match) {
+    match.battery();
+  }
 }
-export default LBE_EXTRACT_LIGHTNING_DATA;
+export default extractLightningData;
