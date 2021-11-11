078b839012401787c734624b4a687471cd837842
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2021-11-10 21:09:43 -0700

    Add capitalization and punctuation to sentence

--- src/app/screens/Onboard/ConnectLndHub/index.tsx
+++ src/app/screens/Onboard/ConnectLndHub/index.tsx
@@ -76,9 +76,9 @@ export default function ConnectLndHub() {
         <div className="lg:w-1/2">
           <h1 className="text-3xl font-bold">Connect to LNDHub (BlueWallet)</h1>
           <p className="text-gray-500 mt-6">
-            in BlueWallet, choose the wallet you want to connect, open it, click
+            In BlueWallet, choose the wallet you want to connect, open it, click
             on "...", click on Export/Backup to display the QR code and scan it
-            with your webcam
+            with your webcam.
           </p>
           <div className="w-4/5">
             <div className="mt-6">
