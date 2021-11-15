2d5f9ad793b87c6add8997b6041c04387157858f
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2021-11-14 18:47:08 -0700

    Add space

--- src/app/screens/Publishers.tsx
+++ src/app/screens/Publishers.tsx
@@ -41,7 +41,7 @@ function Publishers() {
   return (
     <Container>
       <h2 className="mt-12 mb-6 text-2xl font-bold dark:text-white">
-        ⚡️Websites
+        ⚡️ Websites
       </h2>
       <PublishersTable
         publishers={data}
