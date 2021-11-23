37a31a62ac780f2a66a7a4eea3913d51d793a756
Dylan Companjen <dylancompanjen@gmail.com>
2021-11-23 11:24:27 +0100

    fix: errors due to react-router 6 upgrade

--- src/app/components/UserMenu/UserMenu.stories.tsx
+++ src/app/components/UserMenu/UserMenu.stories.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { MemoryRouter } from "react-router-dom";
+import { Meta } from "@storybook/react/types-6-0";
 
 import UserMenu from ".";
 
-const metadata = {
+const metadata: Meta = {
   title: "Components/UserMenu",
   component: UserMenu,
+  decorators: [
+    (Story) => (
+      <MemoryRouter>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
 };
 
 export default metadata;
