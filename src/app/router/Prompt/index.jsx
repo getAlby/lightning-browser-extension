6481c337b81b675926e7b8e6fc1e1c29d8238ee1
Dylan Companjen <dylancompanjen@gmail.com>
2021-11-23 11:10:48 +0100

    feat: updated breaking changes

--- src/app/router/Prompt/index.jsx
+++ src/app/router/Prompt/index.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
 import "../../styles/index.css";
+import "react-loading-skeleton/dist/skeleton.css";
 
 import Prompt from "./Prompt";
 
