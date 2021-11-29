e0f3b24696e5a30ab09de7973ee9485686b5d531
Dylan Companjen <dylancompanjen@gmail.com>
2021-11-29 18:25:15 +0100

    refactor: memo const

--- src/extension/background-script/actions/ln/makeInvoice.js
+++ src/extension/background-script/actions/ln/makeInvoice.js
@@ -2,13 +2,11 @@ import PubSub from "pubsub-js";
 import state from "../../state";
 
 const makeInvoice = async (message, sender) => {
-  if (message.args.memo === undefined) {
-    message.args.memo = memo.args.defaultMemo || "Alby invoice memo";
-  }
+  const memo = memo.args.memo || memo.args.defaultMemo || "Alby invoice memo";
+  message.args.memo = memo;
   PubSub.publish(`ln.makeInvoice.start`, message);
 
   const amount = parseInt(message.args.amount);
-  const memo = message.args.memo;
 
   const connector = state.getState().getConnector();
   const response = await connector.makeInvoice({
