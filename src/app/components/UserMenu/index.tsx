5d09b1c7aa055f26e8b4affc6809a7caf438c1ef
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2021-11-28 20:49:03 -0700

    Make all dropdown icons same color

--- src/app/components/UserMenu/index.tsx
+++ src/app/components/UserMenu/index.tsx
@@ -56,7 +56,7 @@ export default function UserMenu() {
             navigate("/send");
           }}
         >
-          <SendIcon className="w-6 h-6 -ml-0.5 mr-2 opacity-75" />
+          <SendIcon className="w-6 h-6 -ml-0.5 mr-2 opacity-75 text-gray-500" />
           Send
         </Menu.ItemButton>
         <Menu.ItemButton
@@ -64,7 +64,7 @@ export default function UserMenu() {
             navigate("/receive");
           }}
         >
-          <ReceiveIcon className="w-6 h-6 -ml-0.5 mr-2 opacity-75" />
+          <ReceiveIcon className="w-6 h-6 -ml-0.5 mr-2 opacity-75 text-gray-500" />
           Receive
         </Menu.ItemButton>
         <Menu.ItemButton
