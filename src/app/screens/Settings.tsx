2282838b66cb3e43c6616bd2b8e52ee19e386b34
Dylan Companjen <dylancompanjen@gmail.com>
2021-12-01 18:09:23 +0100

    feat: toggline website enhancements ui

--- src/app/screens/Settings.tsx
+++ src/app/screens/Settings.tsx
@@ -1,36 +1,91 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import browser from "webextension-polyfill";
 import { Html5Qrcode } from "html5-qrcode";
 
 import Container from "../components/Container";
 import Button from "../components/Button";
+import Toggle from "../components/Toggle";
+
+type Props = {
+  title: string;
+  subtitle: string;
+  right: React.ReactNode;
+};
+
+function Setting({ title, subtitle, right }: Props) {
+  return (
+    <div className="py-4 flex justify-between items-center">
+      <div>
+        <span className="text-gray-700 font-medium">{title}</span>
+        <p className="text-gray-400 text-sm">{subtitle}</p>
+      </div>
+      {right}
+    </div>
+  );
+}
 
 function Settings() {
+  const [loading, setLoading] = useState(true);
   const [cameraPermissionsGranted, setCameraPermissionsGranted] =
     useState(false);
+  const [websiteEnhancements, setWebsiteEnhancements] = useState(false);
+
+  useEffect(() => {
+    browser.storage.sync
+      .get({
+        websiteEnhancements: true,
+      })
+      .then((settings) => {
+        setWebsiteEnhancements(settings.websiteEnhancements);
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <Container>
       <h2 className="mt-12 mb-6 text-2xl font-bold">Settings</h2>
-      <div className="shadow bg-white sm:rounded-md sm:overflow-hidden p-6">
-        <h3 className="text-gray-700 font-medium">Camera access</h3>
-        <p className="text-gray-400 text-sm">For scanning QR codes</p>
-        <div className="mt-2">
-          {!cameraPermissionsGranted ? (
-            <Button
-              label="Allow camera access"
-              onClick={async () => {
-                try {
-                  await Html5Qrcode.getCameras();
-                  setCameraPermissionsGranted(true);
-                } catch (e) {
-                  alert(e);
-                }
-              }}
-            />
-          ) : (
-            <p className="text-green-500 font-medium">Permission granted</p>
-          )}
-        </div>
+      <div className="shadow bg-white sm:rounded-md sm:overflow-hidden px-6 py-2 divide-y">
+        <Setting
+          title="Camera access"
+          subtitle="For scanning QR codes"
+          right={
+            !cameraPermissionsGranted ? (
+              <Button
+                label="Allow camera access"
+                onClick={async () => {
+                  try {
+                    await Html5Qrcode.getCameras();
+                    setCameraPermissionsGranted(true);
+                  } catch (e) {
+                    alert(e);
+                  }
+                }}
+              />
+            ) : (
+              <p className="text-green-500 font-medium">Permission granted</p>
+            )
+          }
+        />
+        <Setting
+          title="Website enhancements"
+          subtitle="Tipping enhancements for Twitter, YouTube, etc."
+          right={
+            !loading && (
+              <Toggle
+                checked={websiteEnhancements}
+                onChange={() => {
+                  browser.storage.sync
+                    .set({
+                      websiteEnhancements: !websiteEnhancements,
+                    })
+                    .then(() => {
+                      setWebsiteEnhancements(!websiteEnhancements);
+                    });
+                }}
+              />
+            )
+          }
+        />
       </div>
     </Container>
   );
