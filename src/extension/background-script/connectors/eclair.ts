1fc1c35cffbd1e53119ae4533d10982baac505c1
Dylan Companjen <dylancompanjen@gmail.com>
2022-01-06 20:10:16 +0100

    fix linting errors

--- src/extension/background-script/connectors/eclair.ts
+++ src/extension/background-script/connectors/eclair.ts
@@ -40,30 +40,35 @@ class Eclair implements Connector {
   }
 
   async getBalance(): Promise<GetBalanceResponse> {
-    const channels = await this.request('/channels')
+    const channels = await this.request("/channels");
     const total = channels
-      .map((channel: Record<string, Record<string, Record<string, Record<string, Record<string, number>>>>>) =>
-        channel.data?.commitments?.localCommit?.spec?.toLocal || 0
+      .map(
+        (
+          channel: Record<
+            string,
+            Record<
+              string,
+              Record<string, Record<string, Record<string, number>>>
+            >
+          >
+        ) => channel.data?.commitments?.localCommit?.spec?.toLocal || 0
       )
       .reduce((acc: number, b: number) => acc + b, 0);
     return {
       data: {
-        balance: total / 1000
-      }
-    }
+        balance: total / 1000,
+      },
+    };
   }
 
   async sendPayment(args: SendPaymentArgs): Promise<SendPaymentResponse> {
-    const { paymentHash, paymentPreimage } = await this.request(
-      "/payinvoice",
-      {
-        invoice: args.paymentRequest,
-        blocking: true,
-      }
-    );
+    const { paymentHash, paymentPreimage } = await this.request("/payinvoice", {
+      invoice: args.paymentRequest,
+      blocking: true,
+    });
 
-    const info = await this.request('/getsentinfo', {
-      paymentHash
+    const info = await this.request("/getsentinfo", {
+      paymentHash,
     });
     const { status, recipientAmount } = info[info.length - 1];
 
@@ -74,19 +79,19 @@ class Eclair implements Connector {
         route: {
           total_amt: Math.round(recipientAmount / 1000),
           total_fees: status.feesPaid,
-        }
+        },
       },
     };
   }
 
   async checkPayment(args: CheckPaymentArgs): Promise<CheckPaymentResponse> {
-    const data = await this.request('/getreceivedinfo', {
-      paymentHash: args.paymentHash
+    const data = await this.request("/getreceivedinfo", {
+      paymentHash: args.paymentHash,
     });
 
     return {
       data: {
-        paid: data && data.status.type === 'received',
+        paid: data && data.status.type === "received",
       },
     };
   }
@@ -94,7 +99,7 @@ class Eclair implements Connector {
   async signMessage(args: SignMessageArgs): Promise<SignMessageResponse> {
     const { signature } = await this.request("/signmessage", {
       msg: Base64.stringify(UTF8.parse(args.message)),
-    })
+    });
 
     return {
       data: {
@@ -107,7 +112,7 @@ class Eclair implements Connector {
     const { valid } = await this.request("/verifymessage", {
       msg: Base64.stringify(UTF8.parse(args.message)),
       sig: args.signature,
-    })
+    });
 
     return {
       data: {
@@ -120,7 +125,7 @@ class Eclair implements Connector {
     const res = await this.request("/createinvoice", {
       description: args.memo,
       amountMsat: args.amount * 1000,
-    })
+    });
 
     return {
       data: {
@@ -135,14 +140,15 @@ class Eclair implements Connector {
     params?: Record<string, unknown>,
     defaultValues?: Record<string, unknown>
   ) {
-    const url = new URL(this.config.url.startsWith('http')
-      ? this.config.url
-      : 'http://' + this.config.url
+    const url = new URL(
+      this.config.url.startsWith("http")
+        ? this.config.url
+        : "http://" + this.config.url
     );
     url.pathname = path;
     const headers = new Headers();
     headers.append("Accept", "application/json");
-    const password = Base64.stringify(UTF8.parse(':' + this.config.password));
+    const password = Base64.stringify(UTF8.parse(":" + this.config.password));
     headers.append("Authorization", "Basic " + password);
 
     const body = new FormData();
@@ -153,7 +159,7 @@ class Eclair implements Connector {
     }
 
     const res = await fetch(url.toString(), {
-      method: 'POST',
+      method: "POST",
       headers,
       body,
     });
@@ -165,10 +171,10 @@ class Eclair implements Connector {
         try {
           errBody = JSON.parse(textBody);
           if (!errBody.error) {
-            throw new Error('error response: ' + textBody.slice(0, 200))
+            throw new Error("error response: " + textBody.slice(0, 200));
           }
         } catch (err) {
-          throw new Error('got a non-JSON response: ' + textBody.slice(0, 200))
+          throw new Error("got a non-JSON response: " + textBody.slice(0, 200));
         }
       } catch (err) {
         throw new Error(res.statusText);
