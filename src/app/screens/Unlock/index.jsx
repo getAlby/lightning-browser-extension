0822cca11a47ce0a4643408454982438676f3dd2
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-01-08 16:42:49 -0700

    Update unlock screen Alby logo to proper artwork

--- src/app/screens/Unlock/index.jsx
+++ src/app/screens/Unlock/index.jsx
@@ -41,7 +41,7 @@ function Unlock() {
   return (
     <div className="p-8 bg-white dark:bg-gray-800 shadow-lg">
       <div className="flex space-x-2 justify-center pt-5">
-        <img src="assets/icons/alby_icon_transp.svg" alt="logo" />
+        <img src="assets/icons/tip-with-yellow.png" alt="logo" />
       </div>
       <p className="text-center text-xl font-normal font-serif mt-8 mb-5 dark:text-white">
         Unlock to continue
