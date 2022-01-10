41187c3df82588ca6d259d4cb4cec790c560dbf0
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-01-10 08:53:21 -0700

    Change flex-shrink to new shorthand and update tailwind config for v3

--- src/app/components/TransactionsTable/index.tsx
+++ src/app/components/TransactionsTable/index.tsx
@@ -42,7 +42,7 @@ export default function TransactionsTable({ transactions }: Props) {
               {({ open }) => (
                 <>
                   <div className="flex">
-                    <div className="flex items-center flex-shrink-0 mr-3">
+                    <div className="flex items-center shrink-0 mr-3">
                       {tx.type && renderIcon(tx.type)}
                     </div>
                     <div className="overflow-hidden mr-3">
@@ -65,7 +65,7 @@ export default function TransactionsTable({ transactions }: Props) {
                         ))}
                       </div>
                     )}
-                    <div className="flex ml-auto text-right space-x-3 flex-shrink-0">
+                    <div className="flex ml-auto text-right space-x-3 shrink-0">
                       <div>
                         <p className="text-sm font-medium dark:text-white">
                           {[tx.type && "sent", "sending"].includes(tx.type)
