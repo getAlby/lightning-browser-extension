f39cc47731fb7edc92ef8abda1924d78e147c970
Dylan Companjen <dylancompanjen@gmail.com>
2022-01-11 19:34:38 +0100

    refactor: shared connector routes (DRY)

---
+++ src/app/router/connectorRoutes.tsx
@@ -0,0 +1,11 @@
+import ConnectLnd from "../screens/Onboard/ConnectLnd";
+import ConnectLndHub from "../screens/Onboard/ConnectLndHub";
+import ConnectLnbits from "../screens/Onboard/ConnectLnbits";
+import NewWallet from "../screens/Onboard/NewWallet";
+
+export default [
+  { path: "lnd", element: <ConnectLnd /> },
+  { path: "lnd-hub", element: <ConnectLndHub /> },
+  { path: "lnbits", element: <ConnectLnbits /> },
+  { path: "create-wallet", element: <NewWallet /> },
+];
