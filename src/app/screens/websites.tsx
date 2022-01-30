1cd16305802db69d483ec0d91342af9a82c491bd
umar abubakar <umarabubakarfta@gmail.com>
2022-01-30 08:16:42 +0100

    refactor website screen

--- src/app/screens/websites.tsx
+++ src/app/screens/websites.tsx
@@ -35,96 +35,140 @@ function Websites() {
     },
   ];
 
-  const trading = [
+  const list = [
     {
-      title: "LNMarkets",
-      subtitle: "Instant Bitcoin derivatives trading",
-      logo: lnmarket,
-      to: "https://testnet.lnmarkets.com/",
+      title: "Trading",
+      data: [
+        {
+          title: "LNMarkets",
+          subtitle: "Instant Bitcoin derivatives trading",
+          logo: lnmarket,
+          to: "https://testnet.lnmarkets.com/",
+        },
+        {
+          title: "Kollider",
+          subtitle: "Instant Bitcoin derivatives trading",
+          logo: kollider,
+          shadow: true,
+          to: "https://kollider.xyz/",
+        },
+        {
+          title: "LOFT",
+          shadow: true,
+          subtitle: "Trade investment products in Bitcoin",
+          logo: loft,
+          to: "https://loft.trade/",
+        },
+      ],
+    },
+
+    {
+      title: "gaming",
+      data: [
+        {
+          title: "LNGames",
+          subtitle: "Simple Lightning Network games",
+          logo: lngames,
+          to: "https://lightningnetwork.plus/nodes/03952f8756c036669440e06c782c7f21732886ccf04052e0e6e792f8f5a490f6aa",
+        },
+        {
+          title: "Lightining Poker",
+          subtitle: "Play Poker with Bitcoin",
+          logo: lightiningpoker,
+          to: "https://lightning-poker.com/",
+        },
+        {
+          title: "LNBlackJack",
+          subtitle: "Play Blackjack with Bitcoin",
+          logo: lnblackjack,
+          to: "https://www.lnblackjack.com/",
+        },
+        {
+          title: "Lightining Roulette",
+          subtitle: "Play Roulette with Bitcoin",
+          logo: lightiningroulette,
+          to: "https://www.gentingcasino.com/en-ROW/live-casino/game-shows/lightning-roulette/",
+        },
+      ],
     },
     {
-      title: "Kollider",
-      subtitle: "Instant Bitcoin derivatives trading",
-      logo: kollider,
-      shadow: true,
-      to: "https://kollider.xyz/",
+      title: "Entertaiment",
+      data: [
+        {
+          title: "Stacker.New",
+          subtitle: "Lightning powered Bitcoin news site",
+          logo: stacker,
+          to: "https://stacker.news/",
+        },
+      ],
+    },
+
+    {
+      title: "miscellaneous",
+      data: [
+        {
+          title: "Spackshot",
+          subtitle: "Art created and funded by the community ",
+          logo: sparkshot,
+          to: "",
+        },
+        {
+          title: "Sats for Likes",
+          subtitle: "Earn sats for accomplishing tasks",
+          logo: satsforlikes,
+          to: "https://kriptode.com/satsforlikes/index.html",
+        },
+        {
+          title: "lnshort.it",
+          subtitle: "Service to redirect URLs and generate QR codes ",
+          logo: inshort,
+          shadow: true,
+          to: "https://lnshort.it/",
+        },
+      ],
     },
     {
-      title: "LOFT",
-      shadow: true,
-      subtitle: "Trade investment products in Bitcoin",
-      logo: loft,
-      to: "https://loft.trade/",
-    },
-  ];
-  const gaming = [
-    {
-      title: "LNGames",
-      subtitle: "Simple Lightning Network games",
-      logo: lngames,
-      to: "https://lightningnetwork.plus/nodes/03952f8756c036669440e06c782c7f21732886ccf04052e0e6e792f8f5a490f6aa",
-    },
-    {
-      title: "Lightining Poker",
-      subtitle: "Play Poker with Bitcoin",
-      logo: lightiningpoker,
-      to: "https://lightning-poker.com/",
+      title: "shopping",
+      data: [
+        {
+          title: "Bitrefill",
+          subtitle: "Buy vouchers, refill your phone or pay your bills",
+          logo: bitrefill,
+          to: "https://www.bitrefill.com/?hl=en",
+        },
+        {
+          title: "Lightning Network Stores",
+          shadow: true,
+          subtitle:
+            "Collection of stores and websites accepting Bitcoin payments",
+          logo: lightiningnetworkscores,
+          to: "https://cointelegraph.com/tags/lightning-network",
+        },
+      ],
     },
     {
-      title: "LNBlackJack",
-      subtitle: "Play Blackjack with Bitcoin",
-      logo: lnblackjack,
-      to: "https://www.lnblackjack.com/",
-    },
-    {
-      title: "Lightining Roulette",
-      subtitle: "Play Roulette with Bitcoin",
-      logo: lightiningroulette,
-      to: "https://www.gentingcasino.com/en-ROW/live-casino/game-shows/lightning-roulette/",
-    },
-  ];
-  const entertaiment = [
-    {
-      title: "Stacker.New",
-      subtitle: "Lightning powered Bitcoin news site",
-      logo: stacker,
-      to: "https://stacker.news/",
-    },
-  ];
-  const miscellaneous = [
-    {
-      title: "Spackshot",
-      subtitle: "Art created and funded by the community ",
-      logo: sparkshot,
-      to: "",
-    },
-    {
-      title: "Sats for Likes",
-      subtitle: "Earn sats for accomplishing tasks",
-      logo: satsforlikes,
-      to: "https://kriptode.com/satsforlikes/index.html",
-    },
-    {
-      title: "lnshort.it",
-      subtitle: "Service to redirect URLs and generate QR codes ",
-      logo: inshort,
-      shadow: true,
-      to: "https://lnshort.it/",
-    },
-  ];
-  const shopping = [
-    {
-      title: "Bitrefill",
-      subtitle: "Buy vouchers, refill your phone or pay your bills",
-      logo: bitrefill,
-      to: "https://www.bitrefill.com/?hl=en",
-    },
-    {
-      title: "Lightning Network Stores",
-      shadow: true,
-      subtitle: "Collection of stores and websites accepting Bitcoin payments",
-      logo: lightiningnetworkscores,
-      to: "https://cointelegraph.com/tags/lightning-network",
+      title: "miscellaneous",
+      data: [
+        {
+          title: "Spackshot",
+          subtitle: "Art created and funded by the community ",
+          logo: sparkshot,
+          to: "",
+        },
+        {
+          title: "Sats for Likes",
+          subtitle: "Earn sats for accomplishing tasks",
+          logo: satsforlikes,
+          to: "https://kriptode.com/satsforlikes/index.html",
+        },
+        {
+          title: "lnshort.it",
+          subtitle: "Service to redirect URLs and generate QR codes ",
+          logo: inshort,
+          shadow: true,
+          to: "https://lnshort.it/",
+        },
+      ],
     },
   ];
 
@@ -158,78 +202,24 @@ function Websites() {
         />
       </div>
       <div>
-        <FlashList prefix="Trading" image="" />
-        <div className=" grid gap-4 md:grid-cols-4">
-          {trading.map(({ title, subtitle, logo, to, shadow }) => (
-            <OtherCard
-              title={title}
-              subtitle={subtitle}
-              logo={logo}
-              to={to}
-              shadow={shadow}
-              key={title}
-            />
-          ))}
-        </div>
-      </div>
-      <div>
-        <FlashList prefix="Gamming" image="" />
-        <div className=" grid gap-4 md:grid-cols-4">
-          {gaming.map(({ title, subtitle, logo, to }) => (
-            <OtherCard
-              title={title}
-              subtitle={subtitle}
-              logo={logo}
-              to={to}
-              key={title}
-            />
-          ))}
-        </div>
-      </div>
-      <div>
-        <FlashList prefix="Entertaiment" image="" />
-        <div className=" grid gap-4 md:grid-cols-4">
-          {entertaiment.map(({ title, subtitle, logo, to }) => (
-            <OtherCard
-              title={title}
-              subtitle={subtitle}
-              logo={logo}
-              to={to}
-              key={title}
-            />
-          ))}
-        </div>
-      </div>
-      <div>
-        <FlashList prefix="Shopping" image="" />
-        <div className=" grid gap-4 md:grid-cols-4">
-          {shopping.map(({ title, subtitle, logo, to, shadow }) => (
-            <OtherCard
-              title={title}
-              subtitle={subtitle}
-              logo={logo}
-              to={to}
-              shadow={shadow}
-              key={title}
-            />
-          ))}
-        </div>
-      </div>
-
-      <div>
-        <FlashList prefix="Miscellaneous" image="" />
-        <div className=" grid gap-2 md:grid-cols-4">
-          {miscellaneous.map(({ title, subtitle, logo, to, shadow }) => (
-            <OtherCard
-              title={title}
-              subtitle={subtitle}
-              logo={logo}
-              to={to}
-              shadow={shadow}
-              key={title}
-            />
-          ))}
-        </div>
+        {list.map(({ title, data }) => (
+          <div key={title}>
+            {" "}
+            <FlashList prefix={title} image="" />
+            <div className=" grid gap-4 md:grid-cols-4">
+              {data.map(({ title, subtitle, logo, to, shadow }) => (
+                <OtherCard
+                  title={title}
+                  subtitle={subtitle}
+                  logo={logo}
+                  to={to}
+                  shadow={shadow}
+                  key={title}
+                />
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
     </Container>
   );
