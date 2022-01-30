545c16d43411bfe1a970639c20fe9cefe9bf1a54
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-01-30 14:54:49 -0700

    Another dark mode fix

--- src/app/components/PaymentSummary.tsx
+++ src/app/components/PaymentSummary.tsx
@@ -11,12 +11,12 @@ function PaymentSummary({ amount, amountAlt, description }: Props) {
         <dt className="uppercase font-semibold text-gray-500 text-xs">
           Amount
         </dt>
-        <dd className="mb-0">{amount}</dd>
+        <dd className="mb-0 dark:text-white">{amount}</dd>
         {amountAlt && <dd className="text-gray-500">{amountAlt}</dd>}
         <dt className="mt-4 uppercase font-semibold text-gray-500 text-xs">
           Description
         </dt>
-        <dd className="mb-0">{description}</dd>
+        <dd className="mb-0 dark:text-white">{description}</dd>
       </dl>
     </div>
   );
