9973321543f615fa3a4d3b909ee744f7bb01604a
austinkelsay <austinkelsay@yahoo.com>
2022-02-16 09:44:51 -0700

    Removed unnecessary MutationObserver, updated description querySelector to pull from metatag, and retested on multiple profiles

--- src/extension/content-script/batteries/Reddit.ts
+++ src/extension/content-script/batteries/Reddit.ts
@@ -1,22 +1,22 @@
 import getOriginData from "../originData";
 import setLightningData from "../setLightningData";
 
-declare global {
-  interface Window {
-    LBE_REDDIT_MUTATION_OBSERVER: MutationObserver;
-  }
-}
-
 const urlMatcher = /^https:\/\/www.reddit\.com\/user\/(\w+).*/;
 
-// can we get the user description from the primary column that holds the profile information?
+// can we detect the user description from the description metatag?
 function isOnProfilePage() {
-  return document.querySelector('[class="bVfceI5F_twrnRcVO1328"]') != null;
+  return (
+    document.querySelector<HTMLMetaElement>(
+      'head > meta[name="description"]'
+    ) != null
+  );
 }
 
 function getUserData() {
   if (isOnProfilePage()) {
-    const element = document.querySelector('[class="bVfceI5F_twrnRcVO1328"]');
+    const element = document.querySelector<HTMLMetaElement>(
+      'head > meta[name="description"]'
+    );
     const imageUrl = document.querySelector<HTMLImageElement>(
       `img[alt="User avatar"]`
     )?.src;
@@ -32,31 +32,21 @@ function getUserData() {
 }
 
 function battery(): void {
-  // Reddit loads everything async...so we observe DOM changes to check if data finished loading.
-  function redditDOMChanged(_: MutationRecord[], observer: MutationObserver) {
-    let userData;
-    if ((userData = getUserData())) {
-      observer.disconnect();
-
-      let match;
-      let recipient;
-      // attempt to extract lnurlp: from the description text
-      if (
-        (match = (userData.element.textContent || "").match(/lnurlp:(\S+)/i))
-      ) {
-        recipient = match[1];
-      } else {
-        // else let's look for an ⚡ emoji in the userDescription
-        if (
-          userData.element.textContent &&
-          userData.element.textContent.includes("⚡")
-        ) {
-          const descriptionText = userData.element.textContent.split(" ");
-          // If we detect the empji we can iterate through the text seperated by whitespace to try and find a match
-          for (const text of descriptionText) {
-            if ((match = (text || "").match(/(\S+@\S+)/))) {
-              recipient = match[1];
-            }
+  let userData;
+  if ((userData = getUserData())) {
+    let match;
+    let recipient;
+    // attempt to extract lnurlp: from the description text
+    if ((match = (userData.element.content || "").match(/lnurlp:(\S+)/i))) {
+      recipient = match[1];
+    } else {
+      // else let's look for an ⚡ emoji in the userDescription
+      if (userData.element.content && userData.element.content.includes("⚡")) {
+        const descriptionText = userData.element.content.split(" ");
+        // If we detect the empji we can iterate through the text seperated by whitespace to try and find a match
+        for (const text of descriptionText) {
+          if ((match = (text || "").match(/(\S+@\S+)/))) {
+            recipient = match[1];
           }
         }
       }
@@ -77,18 +67,6 @@ function battery(): void {
       ]);
     }
   }
-
-  if (!window.LBE_REDDIT_MUTATION_OBSERVER) {
-    window.LBE_REDDIT_MUTATION_OBSERVER = new MutationObserver(
-      redditDOMChanged
-    );
-  }
-  window.LBE_REDDIT_MUTATION_OBSERVER.observe(document, {
-    childList: true,
-    subtree: true,
-  });
-
-  redditDOMChanged([], window.LBE_REDDIT_MUTATION_OBSERVER);
 }
 
 const reddit = {
