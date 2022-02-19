77ffe79b64c466c1721c1c4a678defad69013889
austinkelsay <austinkelsay@yahoo.com>
2022-02-19 15:44:49 -0700

    Removed unneeded isOnProfile check, migrated getUserData code into battery function, added regex matching for lightning emoji instead of .includes(), and retested with changes

--- src/extension/content-script/batteries/Reddit.ts
+++ src/extension/content-script/batteries/Reddit.ts
@@ -3,70 +3,43 @@ import setLightningData from "../setLightningData";
 
 const urlMatcher = /^https:\/\/www.reddit\.com\/user\/(\w+).*/;
 
-// can we detect the user description from the description metatag?
-function isOnProfilePage() {
-  return (
-    document.querySelector<HTMLMetaElement>(
-      'head > meta[name="description"]'
-    ) != null
-  );
-}
-
-function getUserData() {
-  if (isOnProfilePage()) {
-    const element = document.querySelector<HTMLMetaElement>(
-      'head > meta[name="description"]'
-    );
-    const imageUrl = document.querySelector<HTMLImageElement>(
-      `img[alt="User avatar"]`
-    )?.src;
-    if (element && imageUrl) {
-      return {
-        element,
-        imageUrl,
-        name: document.title,
-      };
-    }
-  }
-  return null;
-}
-
 function battery(): void {
-  let userData;
-  if ((userData = getUserData())) {
-    let match;
-    let recipient;
-    // attempt to extract lnurlp: from the description text
-    if ((match = (userData.element.content || "").match(/lnurlp:(\S+)/i))) {
-      recipient = match[1];
-    } else {
-      // else let's look for an ⚡ emoji in the userDescription
-      if (userData.element.content && userData.element.content.includes("⚡")) {
-        const descriptionText = userData.element.content.split(" ");
-        // If we detect the empji we can iterate through the text seperated by whitespace to try and find a match
-        for (const text of descriptionText) {
-          if ((match = (text || "").match(/(\S+@\S+)/))) {
-            recipient = match[1];
-          }
-        }
-      }
+  const descriptionElement = document.querySelector<HTMLMetaElement>(
+    'head > meta[name="description"]'
+  );
+  const imageUrl = document.querySelector<HTMLImageElement>(
+    `img[alt="User avatar"]`
+  )?.src;
 
-      // if we still did not find anything ignore it.
-      if (!recipient) {
-        return;
-      }
-
-      setLightningData([
-        {
-          method: "lnurl",
-          recipient,
-          ...getOriginData(),
-          icon: userData.imageUrl,
-          name: userData.name,
-        },
-      ]);
-    }
+  if (!descriptionElement || !imageUrl) {
+    return;
   }
+  let match;
+  let recipient;
+  // attempt to extract lnurlp: from the description text
+  if ((match = (descriptionElement.content || "").match(/lnurlp:(\S+)/i))) {
+    recipient = match[1];
+  } else if (
+    (match = descriptionElement.content.match(
+      /(⚡:?|lightning:|lnurl:)\s?(\S+@\S+)/i
+    ))
+  ) {
+    recipient = match[2];
+  }
+  // if we still did not find anything ignore it.
+  if (!recipient) {
+    return;
+  }
+
+  setLightningData([
+    {
+      method: "lnurl",
+      recipient,
+      ...getOriginData(),
+      icon: imageUrl,
+      name: document.title,
+    },
+  ]);
 }
 
 const reddit = {
