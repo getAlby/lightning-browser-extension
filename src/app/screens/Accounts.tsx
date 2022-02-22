769ec22a42b8949f0d5ababa31e3e7736893895b
Dylan Companjen <dylancompanjen@gmail.com>
2022-02-22 19:03:02 +0100

    feat: improve accounts ux

--- src/app/screens/Accounts.tsx
+++ src/app/screens/Accounts.tsx
@@ -1,15 +1,23 @@
 import { useState, useEffect } from "react";
-import { WalletIcon } from "@bitcoin-design/bitcoin-icons-react/outline";
+import { useNavigate } from "react-router-dom";
+import {
+  EllipsisIcon,
+  PlusIcon,
+  WalletIcon,
+} from "@bitcoin-design/bitcoin-icons-react/filled";
 
 import type { Accounts } from "../../types";
 import api from "../../common/lib/api";
 import { useAuth } from "../context/AuthContext";
 
+import Button from "../components/Button";
 import Container from "../components/Container";
+import Menu from "../components/Menu";
 
 function AccountsScreen() {
   const [accounts, setAccounts] = useState<Accounts>({});
   const auth = useAuth();
+  const navigate = useNavigate();
 
   function getAccounts() {
     api.getAccounts().then((response) => {
@@ -52,46 +60,47 @@ function AccountsScreen() {
       <h2 className="mt-12 mb-6 text-2xl font-bold dark:text-white">
         Accounts
       </h2>
-      <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-        <table className="min-w-full divide-y divide-gray-200">
-          <thead className="bg-gray-50">
-            <tr>
-              <th
-                scope="col"
-                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                Name
-              </th>
-              <th scope="col" className="relative px-6 py-3">
-                <span className="sr-only">Delete</span>
-              </th>
-            </tr>
-          </thead>
-          <tbody className="bg-white divide-y divide-gray-200">
+      <div className="shadow border-b border-gray-200 sm:rounded-lg bg-white">
+        <div className="p-6">
+          <Button
+            icon={<PlusIcon className="w-5 h-5" />}
+            label="Add account"
+            primary
+            onClick={() => navigate(`/accounts/new`)}
+          />
+        </div>
+        <table className="min-w-full">
+          <tbody className="divide-y divide-gray-200">
             {Object.keys(accounts).map((accountId) => {
               const account = accounts[accountId];
               return (
                 <tr key={accountId}>
                   <td className="px-6 py-4 whitespace-nowrap">
                     <div className="flex items-center">
-                      <WalletIcon className="w-8 h-8 -ml-1 text-gray-500" />
+                      <div className="w-12 h-12 flex justify-center items-center rounded-full bg-orange-bitcoin-50">
+                        <WalletIcon className="w-8 h-8 text-black" />
+                      </div>
                       <div className="ml-4">
-                        <div className="text-sm font-medium text-gray-900">
+                        <div className="font-bold text-gray-900">
                           {account.name}
                         </div>
-                        <div className="text-sm text-gray-500">
-                          {account.connector}
-                        </div>
+                        <div className="text-gray-500">{account.connector}</div>
                       </div>
                     </div>
                   </td>
                   <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                    <button
-                      onClick={() => removeAccount(accountId, account.name)}
-                      className="text-red-500 hover:text-red-700"
-                    >
-                      Delete
-                    </button>
+                    <Menu as="div" className="relative">
+                      <Menu.Button className="ml-auto flex items-center text-gray-500 hover:text-black transition-color duration-200 dark:hover:text-white">
+                        <EllipsisIcon className="h-6 w-6 rotate-90" />
+                      </Menu.Button>
+                      <Menu.List position="right">
+                        <Menu.ItemButton
+                          onClick={() => removeAccount(accountId, account.name)}
+                        >
+                          Delete
+                        </Menu.ItemButton>
+                      </Menu.List>
+                    </Menu>
                   </td>
                 </tr>
               );
