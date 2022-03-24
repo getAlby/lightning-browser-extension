37fecd0be3f685422fdadfa3ac2fa16e5daf6b80
Jan Sarenik <jajomojo@gmail.com>
2022-03-24 08:58:03 +0100

    fix: check description_hash only if defined

--- src/common/lib/lnurl.ts
+++ src/common/lib/lnurl.ts
@@ -106,7 +106,8 @@ const lnurl = {
       console.error();
     }
     switch (true) {
-      case paymentRequestDetails.description_hash !== metadataHash: // LN WALLET Verifies that h tag (description_hash) in provided invoice is a hash of metadata string converted to byte array in UTF-8 encoding
+      case paymentRequestDetails.description_hash &&
+        paymentRequestDetails.description_hash !== metadataHash: // LN WALLET Verifies that h tag (description_hash) in provided invoice is a hash of metadata string converted to byte array in UTF-8 encoding
       case paymentRequestDetails.mtokens !== String(amount): // LN WALLET Verifies that amount in provided invoice equals an amount previously specified by user
       case paymentInfo.successAction &&
         !["url", "message", "aes"].includes(paymentInfo.successAction.tag): // If successAction is not null: LN WALLET makes sure that tag value of is of supported type, aborts a payment otherwise
