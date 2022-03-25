9a5abfab564459a28a9981564515d55c328d195e
Michael Bumann <hello@michaelbumann.com>
2022-03-25 12:35:51 +0100

    fix(delete account): update currentAccountId only if needed
    Select a new current account only in the case that the current account was deleted.
    We store the currentAccountId in the state. This has to be updated if the account is deleted.

--- src/extension/background-script/actions/accounts/delete.ts
+++ src/extension/background-script/actions/accounts/delete.ts
@@ -6,20 +6,27 @@ import state from "../../state";
 const deleteAccount = async (message: Message) => {
   const accounts = state.getState().accounts;
 
+  let currentAccountId = state.getState().currentAccountId;
   let accountId = message.args?.id;
+
+  // if no account is specified, delete the current account
   if (accountId === undefined) {
-    accountId = state.getState().currentAccountId;
+    accountId = currentAccountId;
   }
 
   if (typeof accountId === "string" || typeof accountId === "number") {
     delete accounts[accountId];
+    state.setState({ accounts });
 
-    const accountIds = Object.keys(accounts);
-    let currentAccountId = null;
-    if (accountIds.length > 0) {
-      currentAccountId = accountIds[0];
+    // if the current account gets deleted we select a new "current account"
+    if (accountId === currentAccountId) {
+      const accountIds = Object.keys(accounts);
+      if (accountIds.length > 0) {
+        currentAccountId = accountIds[0];
+      }
+      state.setState({ currentAccountId });
     }
-    state.setState({ accounts, currentAccountId });
+
     // make sure we immediately persist the updated accounts
     await state.getState().saveToStorage();
     return {
