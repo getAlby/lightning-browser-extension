3c4a8dd37faee709c37a64c053663a5bd0021030
Dylan Companjen <dylancompanjen@gmail.com>
2022-03-29 09:51:04 +0200

    refactor: re-use connector routes to automate choose connector

--- src/app/router/connectorRoutes.tsx
+++ src/app/router/connectorRoutes.tsx
@@ -1,3 +1,15 @@
+import lnbits from "/static/assets/icons/lnbits.png";
+import lndhub from "/static/assets/icons/lndhub.png";
+import lnd from "/static/assets/icons/lnd.png";
+import galoyBitcoinBeach from "/static/assets/icons/galoy_bitcoin_beach.jpg";
+import galoyBitcoinJungle from "/static/assets/icons/galoy_bitcoin_jungle.png";
+import eclair from "/static/assets/icons/eclair.jpg";
+import alby from "/static/assets/icons/alby.png";
+import umbrel from "/static/assets/icons/umbrel.png";
+import start9 from "/static/assets/icons/start9.png";
+import citadel from "/static/assets/icons/citadel.png";
+import mynode from "/static/assets/icons/mynode.png";
+
 import ConnectLnd from "../screens/connectors/ConnectLnd";
 import ConnectLndHub from "../screens/connectors/ConnectLndHub";
 import ConnectLnbits from "../screens/connectors/ConnectLnbits";
@@ -15,21 +27,81 @@ const galoyPaths: { [key: string]: keyof typeof galoyUrls } = {
 };
 
 export default [
-  { path: "lnd", element: <ConnectLnd /> },
-  { path: "lnd-hub", element: <ConnectLndHub /> },
-  { path: "lnbits", element: <ConnectLnbits /> },
-  { path: "eclair", element: <ConnectEclair /> },
-  { path: "citadel", element: <ConnectCitadel /> },
-  { path: "create-wallet", element: <NewWallet /> },
+  {
+    path: "create-wallet",
+    element: <NewWallet />,
+    title: "Create a new wallet",
+    description: "We create and manage a lightning wallet for you",
+    logo: alby,
+  },
+  {
+    path: "lnd",
+    element: <ConnectLnd />,
+    title: "LND",
+    description: "Connect to your LND node",
+    logo: lnd,
+  },
+  {
+    path: "lnd-hub",
+    element: <ConnectLndHub />,
+    title: "LNDHub (Bluewallet)",
+    description: "Connect to your Bluewallet mobile wallet",
+    logo: lndhub,
+  },
+  {
+    path: "lnbits",
+    element: <ConnectLnbits />,
+    title: "LNbits",
+    description: "Connect to your LNbits account",
+    logo: lnbits,
+  },
+  {
+    path: "eclair",
+    element: <ConnectEclair />,
+    title: "Eclair",
+    description: "Connect to your Eclair node",
+    logo: eclair,
+  },
+  {
+    path: "citadel",
+    element: <ConnectCitadel />,
+    title: "Citadel",
+    description: "Connect to your local Citadel",
+    logo: citadel,
+  },
+  {
+    path: "umbrel",
+    element: <ConnectUmbrel />,
+    title: "Umbrel",
+    description: "Connect to your Umbrel",
+    logo: umbrel,
+  },
+  {
+    path: "mynode",
+    element: <ConnectMyNode />,
+    title: "myNode",
+    description: "Connect to your myNode",
+    logo: mynode,
+  },
+  {
+    path: "start9",
+    element: <ConnectStart9 />,
+    title: "Start9",
+    description: "Connect to your Embassy",
+    logo: start9,
+  },
   {
     path: galoyPaths.bitcoinBeach,
     element: <ConnectGaloy instance={galoyPaths.bitcoinBeach} />,
+    title: "Bitcoin Beach Wallet",
+    description: "Create or connect to a Bitcoin Beach (Galoy) account",
+    logo: galoyBitcoinBeach,
   },
   {
     path: galoyPaths.bitcoinJungle,
     element: <ConnectGaloy instance={galoyPaths.bitcoinJungle} />,
+    title: "Bitcoin Jungle Wallet",
+    description: "Create or connect to a Bitcoin Jungle (Galoy) account",
+    logo: galoyBitcoinJungle,
   },
-  { path: "umbrel", element: <ConnectUmbrel /> },
-  { path: "start9", element: <ConnectStart9 /> },
-  { path: "mynode", element: <ConnectMyNode /> },
 ];
