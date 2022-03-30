15ce97d35297acc67dcbf7e9eb151ef89bf65ecb
kiwiidb <kwinten.debacker@hotmail.com>
2022-03-30 14:58:32 +0200

    fix: setting text

--- src/app/screens/Settings.tsx
+++ src/app/screens/Settings.tsx
@@ -59,7 +59,7 @@ function Settings() {
 
         <Setting
           title="Legacy signing for LNDhub and LNBits"
-          subtitle="Message signing and login with LNDhub and LNbits accounts has been changed. If you logged in with these accounts you can enable the old signing method."
+          subtitle="Message signing and login with LNDhub and LNbits accounts has been changed. If you logged in with these accounts you can still enable the old signing method. This option will be removed later, make sure to switch to the new login."
         >
           {!loading && (
             <Toggle
