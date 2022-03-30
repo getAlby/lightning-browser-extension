32ab797ca89670051bd0da3167d83dbfd324db30
Michael Bumann <hello@michaelbumann.com>
2022-03-30 20:16:11 +0200

    fix: list calls in content-scripts

--- src/extension/content-script/index.js
+++ src/extension/content-script/index.js
@@ -3,7 +3,16 @@ import getOriginData from "./originData";
 import shouldInject from "./shouldInject";
 import injectScript from "./injectScript";
 
-//import { enhancements, loadEnhancements } from "../inpage-script/enhancements";
+const weblnCalls = [
+  "enable",
+  "getInfo",
+  "lnurl",
+  "sendPaymentOrPrompt",
+  "keysendOrPrompt",
+  "makeInvoice",
+  "signMessageOrPrompt",
+  "verifyMessage",
+];
 import extractLightningData from "./batteries";
 
 if (shouldInject()) {
@@ -18,13 +27,16 @@ if (shouldInject()) {
 
   // message listener to listen to inpage webln calls
   // those calls get passed on to the background script
-  // (the inpage script can not do that directly, but only the inpage script can make webln availabe to the page)
+  // (the inpage script can not do that directly, but only the inpage script can make webln available to the page)
   window.addEventListener("message", (ev) => {
     // Only accept messages from the current window
     if (ev.source !== window) {
       return;
     }
     if (ev.data && ev.data.application === "LBE" && !ev.data.response) {
+      if (!weblnCalls.includes(ev.data.type)) {
+        return;
+      }
       const messageWithOrigin = {
         ...ev.data,
         origin: getOriginData(),
