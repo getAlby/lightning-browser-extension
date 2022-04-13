764a00f6bcb38a10955a0e32c32730820343b552
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-04-13 17:59:51 -0600

    refactor: add comments to custom effect hook to explain use

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -7,15 +7,22 @@ export function classNames(...classes: (string | boolean)[]) {
 
 export function useThemeEffect(dependencies: undefined | string) {
   useEffect(() => {
+    // api call to get settings
     api.getSettings().then((response) => {
       const settings = response;
+      // check if settings theme selection is system (this is the default)
       if (settings.theme === "system") {
+        // checks if the users prefers dark mode and if true then adds dark class to HTML element
         if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
           document.documentElement.classList.add("dark");
-        } else {
+        }
+        // if false removes dark class - there is no class by default but this is in case the user switches between themes
+        else {
           document.documentElement.classList.remove("dark");
         }
-      } else if (settings.theme === "dark") {
+      }
+      // last two conditionals for if user selects light or dark mode
+      else if (settings.theme === "dark") {
         document.documentElement.classList.add("dark");
       } else if (settings.theme === "light") {
         document.documentElement.classList.remove("dark");
