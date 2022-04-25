f157ce2b57ff73261a842bb8b6171fdaa65c3519
Dylan Companjen <dylancompanjen@gmail.com>
2022-04-25 10:17:26 +0200

    refactor: card height

--- src/app/screens/Publishers/index.tsx
+++ src/app/screens/Publishers/index.tsx
@@ -68,21 +68,21 @@ function Publishers() {
         {websites.map(({ title, items }) => (
           <div className="mb-6" key={title}>
             <h4 className="mb-4 text-xl font-bold">{title}</h4>
-            <div className="grid gap-4 md:grid-cols-4">
+            <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
               {items.map(({ title, subtitle, logo, url }) => (
                 <a key={url} href={url} target="_blank" rel="noreferrer">
-                  <div className="bg-white shadow-md flex space-x-3 h-44 px-4 rounded-lg pt-7 hover:bg-gray-50 cursor-pointer w-full">
-                    <div className="flex space-x-2">
+                  <div className="bg-white shadow-md flex p-4 h-32 rounded-lg hover:bg-gray-50 cursor-pointer w-full">
+                    <div className="flex space-x-3">
                       <img
                         src={logo}
                         alt="image"
                         className="h-14 w-14 rounded-xl shadow-md object-cover"
                       />
                       <div>
-                        <h2 className="font-medium  font-serif text-base">
+                        <h2 className="font-medium font-serif text-base">
                           {title}
                         </h2>
-                        <p className="font-serif text-sm font-normal text-gray-500">
+                        <p className="font-serif text-sm font-normal text-gray-500 line-clamp-3">
                           {subtitle}
                         </p>
                       </div>
