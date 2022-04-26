52a963f50edb304a4589c3fb29fde6ed6aa61c83
Janna <jg.general@protonmail.com>
2022-04-25 20:46:17 -0500

    style: changed logo on welcome screen
    Use alby-logo on intro for recognition

--- src/app/screens/Onboard/Intro/index.tsx
+++ src/app/screens/Onboard/Intro/index.tsx
@@ -41,8 +41,12 @@ export default function Intro() {
     <div>
       <div className="relative lg:grid lg:grid-cols-3 lg:gap-x-8 mt-14 bg-white dark:bg-gray-800 px-10 py-12 items-center">
         <div className="lg:col-span-1">
-          <div className="max-w-xs">
-            <img src="assets/icons/satsymbol.svg" alt="sat" className="w-64" />
+          <div className="max-w-xs ml-7">
+            <img
+              src="assets/icons/alby-logo-figure-400.svg"
+              alt="Alby"
+              className="w-72"
+            />
           </div>
         </div>
         <div className="mt-10 lg:mt-0 lg:col-span-2">
