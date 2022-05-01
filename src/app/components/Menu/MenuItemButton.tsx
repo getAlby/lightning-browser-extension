d5bb0baf2c26fd2be5244abbcbc5382d255eae64
Dylan Companjen <dylancompanjen@gmail.com>
2022-05-01 22:28:17 +0200

    fix: menu bg color bugs

--- src/app/components/Menu/MenuItemButton.tsx
+++ src/app/components/Menu/MenuItemButton.tsx
@@ -20,10 +20,10 @@ function MenuItemButton({
       {({ active }) => (
         <button
           className={classNames(
-            active ? "bg-gray-100" : "",
+            active ? "bg-gray-100 dark:bg-white/10" : "",
             danger ? "text-red-700" : "text-gray-700",
             disabled ? "cursor-not-allowed" : "cursor-pointer",
-            "flex items-center block w-full text-left px-4 py-2 text-sm hover:bg-gray-100 dark:text-white dark:hover:bg-gray-600"
+            "flex items-center block w-full text-left px-4 py-2 text-sm dark:text-white"
           )}
           disabled={disabled}
           onClick={onClick}
