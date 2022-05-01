7dcf0321a7573237e85f480caa83f4492718f7a8
escapedcat <github@htmlcss.de>
2022-05-01 11:03:11 +0800

    chore: import missing type

--- src/common/lib/utils.ts
+++ src/common/lib/utils.ts
@@ -2,7 +2,7 @@ import { PaymentRequestObject } from "bolt11";
 import PubSub from "pubsub-js";
 import browser, { Runtime } from "webextension-polyfill";
 import { SendPaymentResponse } from "~/extension/background-script/connectors/connector.interface";
-import { Message, OriginData } from "~/types";
+import { Message, OriginData, OriginDataInternal } from "~/types";
 
 const utils = {
   call: <T = Record<string, unknown>>(
