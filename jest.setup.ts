a87efcebc2e59b381f48fef7a74d3ff789e28d0b
escapedcat <github@htmlcss.de>
2022-05-04 10:09:27 +0800

    chore: adjust TS eslint rules

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,6 +1,8 @@
 // avoid "This script should only be loaded in a browser extension." issue
 // https://github.com/clarkbw/jest-webextension-mock/issues/149#issuecomment-1116307310
+// @ts-ignore: chrome available during tests via jest-webextension-mock
 if (!chrome.runtime) chrome.runtime = {};
+// @ts-ignore: chrome available during tests via jest-webextension-mock
 if (!chrome.runtime.id) chrome.runtime.id = "history-delete";
 
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
