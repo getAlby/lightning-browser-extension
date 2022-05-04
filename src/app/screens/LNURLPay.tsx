4ff4494eab22df7d08659efcc107201c3594f54e
Dylan Companjen <dylancompanjen@gmail.com>
2022-05-04 12:04:59 +0200

    refactor: more legible text

--- src/app/screens/LNURLPay.tsx
+++ src/app/screens/LNURLPay.tsx
@@ -36,7 +36,7 @@ const Dt = ({ children }: { children: React.ReactNode }) => (
 );
 
 const Dd = ({ children }: { children: React.ReactNode }) => (
-  <dd className="mb-4 dark:text-gray-400">{children}</dd>
+  <dd className="mb-4 text-gray-600 dark:text-gray-500">{children}</dd>
 );
 
 function LNURLPay(props: Props) {
