83e75dbc07fcaff6e746bfed4e64b685b7306b6b
escapedcat <github@htmlcss.de>
2022-05-04 09:36:06 +0800

    chore: support webextension-polyfill-0.9.0

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,3 +1,8 @@
+// avoid "This script should only be loaded in a browser extension." issue
+// https://github.com/clarkbw/jest-webextension-mock/issues/149#issuecomment-1116307310
+if (!chrome.runtime) chrome.runtime = {};
+if (!chrome.runtime.id) chrome.runtime.id = "history-delete";
+
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
