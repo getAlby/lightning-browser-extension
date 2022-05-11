80761f73ea2205e6e3bfa8cf7d1c909fddd0c331
Dylan <dylancompanjen@gmail.com>
2022-05-11 14:57:27 +0200

    Update jest.setup.ts
    Co-authored-by: Michael Bumann <hello@michaelbumann.com>

--- jest.setup.ts
+++ jest.setup.ts
@@ -4,5 +4,5 @@
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
 
-// fix "This script should only be loaded in a browser extension."
+// fix "This script should only be loaded in a browser extension." e.g. https://github.com/mozilla/webextension-polyfill/issues/218
 if (!chrome.runtime.id) chrome.runtime.id = "history-delete";
