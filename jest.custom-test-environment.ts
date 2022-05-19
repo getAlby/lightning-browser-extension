59edffe644779fe15e2020588c1b0978b5487ce0
escapedcat <github@htmlcss.de>
2022-05-19 16:23:14 +0800

    refactor: jest v28 Uint8Array handling
    some QRcode scan related tests fail if we do not define Uint8Array
    
    Co-authored-by: Dylan <dylancompanjen@gmail.com>

--- jest.custom-test-environment.ts
+++ jest.custom-test-environment.ts
@@ -2,14 +2,19 @@ import { TestEnvironment } from "jest-environment-jsdom";
 
 class CustomEnvironment extends TestEnvironment {
   constructor({ globalConfig, projectConfig }, context) {
-    super({ globalConfig, projectConfig }, context);
+    super(
+      {
+        globalConfig,
+        projectConfig: {
+          ...projectConfig,
+          globals: { ...projectConfig.globals, Uint8Array: Uint8Array },
+        },
+      },
+      context
+    );
     const config = projectConfig;
-
-    Object.assign({}, config, {
-      globals: Object.assign({}, config.globals, {
-        Uint8Array: Uint8Array,
-      }),
-    });
+  }
+}
   }
 }
 
