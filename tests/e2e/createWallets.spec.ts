4fd4c1411cb7b0e02cfe823f9f90b164e76cc22e
escapedcat <github@htmlcss.de>
2022-05-24 09:49:35 +0800

    test(createWallet): add LND

--- tests/e2e/createWallets.spec.ts
+++ tests/e2e/createWallets.spec.ts
@@ -119,4 +119,33 @@ test.describe("Create or connect wallets", () => {
 
     await commonCreateWalletSuccessCheck({ browser, welcomePage, $document });
   });
+
+  test("successfully connects to LND", async () => {
+    const { browser, welcomePage, $document } =
+      await commonCreateWalletUserCreate();
+
+    // click at "LNDHub (BlueWallet)"
+    const createNewWalletButton = await getByText($document, "LND");
+    createNewWalletButton.click();
+
+    // wait for the field label instead of headline (headline text already exists on the page before)
+    await waitFor(() => getByText($document, "REST API host and port"));
+
+    const restApiUrl = "https://lnd1.regtest.getalby.com";
+    const lndUrlField = await getByLabelText(
+      $document,
+      "REST API host and port"
+    );
+    await lndUrlField.type(restApiUrl);
+
+    const macroon =
+      "0201036C6E6402F801030A10E2133A1CAC2C5B4D56E44E32DC64C8551201301A160A0761646472657373120472656164120577726974651A130A04696E666F120472656164120577726974651A170A08696E766F69636573120472656164120577726974651A210A086D616361726F6F6E120867656E6572617465120472656164120577726974651A160A076D657373616765120472656164120577726974651A170A086F6666636861696E120472656164120577726974651A160A076F6E636861696E120472656164120577726974651A140A057065657273120472656164120577726974651A180A067369676E6572120867656E657261746512047265616400000620C4F9783E0873FA50A2091806F5EBB919C5DC432E33800B401463ADA6485DF0ED";
+    const macroonField = await getByLabelText(
+      $document,
+      "Macaroon (HEX format)"
+    );
+    await macroonField.type(macroon);
+
+    await commonCreateWalletSuccessCheck({ browser, welcomePage, $document });
+  });
 });
