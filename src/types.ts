91aaa74a8899e9e07e92579e848c3253015db08b
escapedcat <github@htmlcss.de>
2022-05-30 16:29:57 +0800

    refactor(account): info message-type #652
    add test

--- src/types.ts
+++ src/types.ts
@@ -127,6 +127,10 @@ export interface MessageAccountSelect extends MessageDefault {
   action: "selectAccount";
 }
 
+export interface MessageAccountInfo extends Omit<MessageDefault, "args"> {
+  type: "accountInfo";
+}
+
 interface LNURLChannelServiceResponse {
   uri: string; // Remote node address of form node_key@ip_address:port_number
   callback: string; // a second-level URL which would initiate an OpenChannel message from target LN node
