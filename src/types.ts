ccccb366a9eedd3133a342a17661cd65564fa433
escapedcat <github@htmlcss.de>
2022-05-31 16:23:13 +0800

    refactor(account): switch getAccounts to TS #652
    add test

--- src/types.ts
+++ src/types.ts
@@ -116,6 +116,9 @@ export interface MessageAccountInfo extends Omit<MessageDefault, "args"> {
   type: "accountInfo";
 }
 
+export interface MessageAccountAll extends Omit<MessageDefault, "args"> {
+  type: "getAccounts";
+}
 interface LNURLChannelServiceResponse {
   uri: string; // Remote node address of form node_key@ip_address:port_number
   callback: string; // a second-level URL which would initiate an OpenChannel message from target LN node
