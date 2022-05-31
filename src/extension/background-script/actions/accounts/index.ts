583d9d3e3e33c8fae1477182d26ee15bc06f1849
escapedcat <github@htmlcss.de>
2022-05-31 14:12:45 +0800

    refactor(state): isUnlocked handling #652
    chore: remove unsued code

--- src/extension/background-script/actions/accounts/index.ts
+++ src/extension/background-script/actions/accounts/index.ts
@@ -4,8 +4,7 @@ import deleteAccount from "./delete";
 import edit from "./edit";
 import info from "./info";
 import lock from "./lock";
-import remove from "./remove";
 import select from "./select";
 import unlock from "./unlock";
 
-export { all, unlock, lock, add, edit, select, info, remove, deleteAccount };
+export { all, unlock, lock, add, edit, select, info, deleteAccount };
