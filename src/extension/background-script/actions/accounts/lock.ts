216eeb147feff84d7522c603dcdc0616127f95d4
escapedcat <github@htmlcss.de>
2022-06-01 16:38:37 +0800

    refactor(account): lock #652
    add test

--- src/extension/background-script/actions/accounts/lock.ts
+++ src/extension/background-script/actions/accounts/lock.ts
@@ -1,6 +1,7 @@
 import state from "../../state";
+import type { MessageAccountLock } from "~/types";
 
-const lock = async (message, sender) => {
+const lock = async (message: MessageAccountLock) => {
   await state.getState().lock();
   return {
     data: { unlocked: false },
--- src/types.ts
+++ src/types.ts
@@ -111,6 +111,11 @@ export interface MessageAccountAll extends Omit<MessageDefault, "args"> {
 export interface MessageAccountAll extends Omit<MessageDefault, "args"> {
   type: "getAccounts";
 }
+
+export interface MessageAccountLock extends Omit<MessageDefault, "args"> {
+  type: "lock";
+}
+
 interface LNURLChannelServiceResponse {
   uri: string; // Remote node address of form node_key@ip_address:port_number
   callback: string; // a second-level URL which would initiate an OpenChannel message from target LN node
