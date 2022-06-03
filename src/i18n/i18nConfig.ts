2ce87dc530ea937740c467940a4eb93f7daba789
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-06-02 18:38:38 -0600

    fix: WIP fix typescript errors

--- src/i18n/i18nConfig.ts
+++ src/i18n/i18nConfig.ts
@@ -1,8 +1,32 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
+import type { CustomDetector } from "i18next-browser-languagedetector";
 import en from "./locales/en.json";
 import hi from "./locales/hi.json";
+import api from "~/common/lib/api";
+
+export const settings: CustomDetector = {
+  name: "settings",
+
+  lookup(_options) {
+    api.getSettings().then((setting) => {
+      i18n.changeLanguage(setting.locale);
+    });
+    return undefined;
+  },
+
+  cacheUserLanguage(_lng, _options) {
+    // lng -> current language, will be called after init and on changeLanguage
+    // store it
+    api.getSettings().then((setting) => {
+      i18n.changeLanguage(setting.locale);
+    });
+  },
+};
+
+const languageDetector = new LanguageDetector();
+languageDetector.addDetector(settings);
 
 export const resources = {
   en,
@@ -14,11 +38,25 @@ i18n
   .use(initReactI18next)
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(LanguageDetector)
+  .use(languageDetector)
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     //debug: true,
+    detection: {
+      // order and from where user language should be detected
+      order: [
+        "settings",
+        "querystring",
+        "cookie",
+        "localStorage",
+        "sessionStorage",
+        "navigator",
+        "htmlTag",
+        "path",
+        "subdomain",
+      ],
+    },
     fallbackLng: "en",
     resources,
   });
