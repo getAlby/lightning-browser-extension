d0506f2b28a57797156beea4eb300310f8d4183c
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-06-02 18:55:52 -0600

    fix: prettier with new import sorting

--- src/i18n/i18nConfig.ts
+++ src/i18n/i18nConfig.ts
@@ -2,9 +2,10 @@ import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import type { CustomDetector } from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
+import api from "~/common/lib/api";
+
 import en from "./locales/en.json";
 import hi from "./locales/hi.json";
-import api from "~/common/lib/api";
 
 export const settings: CustomDetector = {
   name: "settings",
