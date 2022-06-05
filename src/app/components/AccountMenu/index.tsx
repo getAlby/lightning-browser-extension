fb0bf8b210a345a18ca6e93a460b285a9bc29776
im-adithya <imadithyavardhan@gmail.com>
2022-06-05 12:07:42 +0530

    fix: use span instead of div

--- src/app/components/AccountMenu/index.tsx
+++ src/app/components/AccountMenu/index.tsx
@@ -86,9 +86,9 @@ function AccountMenu({ title, subtitle, showOptions = true }: Props) {
                 title={account.name}
               >
                 <WalletIcon className="w-6 h-6 -ml-0.5 mr-2 shrink-0 opacity-75 text-gray-700 dark:text-neutral-300" />
-                <div className="overflow-hidden text-ellipsis whitespace-nowrap">
+                <span className="overflow-hidden text-ellipsis whitespace-nowrap">
                   {account.name}&nbsp;
-                </div>
+                </span>
               </Menu.ItemButton>
             );
           })}
