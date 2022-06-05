c53c1b0ac8e7afd44025c61e09ca4f922b0be735
Dylan Companjen <dylancompanjen@gmail.com>
2022-06-05 16:47:19 +0200

    style: highlight with dot and checkmark

--- src/app/components/Menu/MenuItemButton.tsx
+++ src/app/components/Menu/MenuItemButton.tsx
@@ -1,3 +1,4 @@
+import { CheckIcon } from "@bitcoin-design/bitcoin-icons-react/filled";
 import { Menu } from "@headlessui/react";
 import { classNames } from "~/app/utils/index";
 
@@ -24,13 +25,17 @@ function MenuItemButton({
             active ? "bg-gray-100 dark:bg-white/10" : "",
             danger ? "text-red-700" : "text-gray-700",
             disabled ? "cursor-not-allowed" : "cursor-pointer",
-            selected ? "font-bold" : "",
             "flex items-center block w-full text-left px-4 py-2 text-sm dark:text-white"
           )}
           disabled={disabled}
           onClick={onClick}
         >
           {children}
+          {selected && (
+            <span className="ml-auto w-3.5 h-3.5 rounded-full bg-orange-bitcoin flex justify-center items-center">
+              <CheckIcon className="w-3 h-3 text-white" />
+            </span>
+          )}
         </button>
       )}
     </Menu.Item>
