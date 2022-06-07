e6112df50bd514a3630f2ba13dfa231103a2e1f3
escapedcat <github@htmlcss.de>
2022-06-07 13:00:47 +0800

    style: linting #597

--- src/app/components/form/NumberField.test.tsx
+++ src/app/components/form/NumberField.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
+
 import type { Props } from "./NumberField";
 import NumberField from "./NumberField";
 
