e55ba8afe380c7e5cdf1ebba2d84c3d4727473f6
Riccardo Balbo <riccardo0blb@gmail.com>
2022-06-09 16:51:06 +0200

    fix: cache twitch channel id

--- src/extension/content-script/batteries/Twitch.ts
+++ src/extension/content-script/batteries/Twitch.ts
@@ -1,3 +1,5 @@
+import browser from "webextension-polyfill";
+
 import getOriginData from "../originData";
 import setLightningData from "../setLightningData";
 
@@ -12,42 +14,69 @@ const battery = async (): Promise<void> => {
   if (!channel) return; // not a channel
   if (!validationRegex.test(channel)) return; // invalid channel
 
-  // Grab client ID from the page HTML
-  const clientIdMatch =
-    document.documentElement.outerHTML.match(clientIdExtractor);
-  const clientId = clientIdMatch ? clientIdMatch[1] : "";
-  if (!clientId) return; // client id not found
+  let channelData;
 
-  // Simulate internal API call to obtain userData.
-  // A public api should be used here instead, but i couldn't find an alternative
-  // that didn't require oauth (thus breaking user experience).
-  let channelData = await fetch(`https://gql.twitch.tv/gql`, {
-    headers: {
-      "Client-Id": clientId,
-      "User-Agent": navigator.userAgent,
-    },
-    method: "POST",
-    body: JSON.stringify([
-      {
-        operationName: "ChannelRoot_AboutPanel",
-        variables: {
-          channelLogin: channel,
-          skipSchedule: true,
-        },
-        extensions: {
-          persistedQuery: {
-            version: 1,
-            sha256Hash:
-              "6089531acef6c09ece01b440c41978f4c8dc60cb4fa0124c9a9d3f896709b6c6",
+  // try this twice: first with the cachedClientId, then, if it fails, with a newly fetched clientId
+  for (let i = 0; i < 2; i++) {
+    let clientId = (await browser.storage.local.get(["twitch-clientId"]))[
+      "twitch-clientId"
+    ];
+
+    if (!clientId) {
+      // Grab and cache client ID from the page HTML. This shouldn't change too often
+      const clientIdMatch =
+        document.documentElement.outerHTML.match(clientIdExtractor);
+      clientId = clientIdMatch ? clientIdMatch[1] : "";
+      if (!clientId) return; // client id not found
+      else {
+        await browser.storage.local.set({
+          "twitch-clientId": clientId,
+        });
+      }
+    }
+
+    // Simulate internal API call to obtain channelData.
+    // A public api should be used here instead, but i couldn't find an alternative
+    // that didn't require oauth (thus breaking user experience).
+    channelData = await fetch(`https://gql.twitch.tv/gql`, {
+      headers: {
+        "Client-Id": clientId,
+        "User-Agent": navigator.userAgent,
+      },
+      method: "POST",
+      body: JSON.stringify([
+        {
+          operationName: "ChannelRoot_AboutPanel",
+          variables: {
+            channelLogin: channel,
+            skipSchedule: true,
+          },
+          extensions: {
+            persistedQuery: {
+              version: 1,
+              sha256Hash:
+                "6089531acef6c09ece01b440c41978f4c8dc60cb4fa0124c9a9d3f896709b6c6",
+            },
           },
         },
-      },
-    ]),
-  }).then((res) => res.json());
+      ]),
+    }).then((res) => res.json());
 
-  if (!channelData || !channelData[0] || !channelData[0].data.user) return; // not a valid channel?
+    channelData =
+      !channelData || !channelData[0] || !channelData[0].data.user
+        ? null
+        : channelData[0].data.user;
 
-  channelData = channelData[0].data.user;
+    if (channelData) break;
+    else {
+      // not found: invalidate cache and try again
+      await browser.storage.local.set({
+        "twitch-clientId": undefined,
+      });
+    }
+  }
+
+  if (!channelData) return; // not a valid channel?
 
   const channelDescription = channelData.description ?? "";
   const lightningAddr = findLightningAddressInText(channelDescription);
