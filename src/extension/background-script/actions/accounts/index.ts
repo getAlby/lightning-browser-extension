0e35ffa858ba7f22a3664493a65712b1ca7fbc53
im-adithya <imadithyavardhan@gmail.com>
2022-06-20 23:15:15 +0530

    chore: lint fixes

--- src/extension/background-script/actions/accounts/index.ts
+++ src/extension/background-script/actions/accounts/index.ts
@@ -1,21 +1,11 @@
 import add from "./add";
 import all from "./all";
 import decryptedDetails from "./decryptedDetails";
-import remove from "./remove";
 import edit from "./edit";
 import info from "./info";
 import lock from "./lock";
+import remove from "./remove";
 import select from "./select";
 import unlock from "./unlock";
 
-export {
-  all,
-  unlock,
-  lock,
-  add,
-  edit,
-  select,
-  info,
-  remove,
-  decryptedDetails,
-};
+export { all, unlock, lock, add, edit, select, info, remove, decryptedDetails };
