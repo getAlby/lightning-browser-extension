c2b16d0e3dd9c7009ac16990d41a64493532c2be
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-06-24 15:32:14 -0600

    refactor: remove not needed code

--- src/i18n/i18nConfig.ts
+++ src/i18n/i18nConfig.ts
@@ -1,6 +1,5 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import type { CustomDetector } from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 import api from "~/common/lib/api";
 
@@ -10,29 +9,9 @@ import en_components from "./locales/en/components.json";
 import en from "./locales/en/translation.json";
 import hi from "./locales/hi/translation.json";
 
-export const settings: CustomDetector = {
-  name: "settings",
-
-  lookup(_options) {
-    let settingsLang;
-    api.getSettings().then((setting) => {
-      i18n.changeLanguage(setting.locale);
-      settingsLang = setting.locale;
-    });
-    return settingsLang;
-  },
-
-  cacheUserLanguage(_lng, _options) {
-    // lng -> current language, will be called after init and on changeLanguage
-    // store it
-    api.getSettings().then((setting) => {
-      i18n.changeLanguage(setting.locale);
-    });
-  },
-};
-
-const languageDetector = new LanguageDetector();
-languageDetector.addDetector(settings);
+api.getSettings().then((setting) => {
+  i18n.changeLanguage(setting.locale);
+});
 
 // Load namespaces
 export const resources = {
@@ -52,25 +31,11 @@ i18n
   .use(initReactI18next)
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(languageDetector)
+  .use(LanguageDetector)
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     //debug: true,
-    detection: {
-      // order and from where user language should be detected
-      order: [
-        "settings",
-        "querystring",
-        "cookie",
-        "localStorage",
-        "sessionStorage",
-        "navigator",
-        "htmlTag",
-        "path",
-        "subdomain",
-      ],
-    },
     fallbackLng: "en",
     resources,
   });
