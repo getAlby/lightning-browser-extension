894e5ed8f50dae67674c23ebf5d1c30a4a537e99
escapedcat <github@htmlcss.de>
2022-06-30 11:08:47 +0800

    perf(jest): remove ts-jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,8 @@
-import { pathsToModuleNameMapper } from "ts-jest";
-
-import { compilerOptions } from "./tsconfig.json";
-
 module.exports = {
   verbose: true,
   transform: {
-    "^.+\\.mjs?$": [ // "dexie" needs this
+    "^.+\\.mjs?$": [
+      // "dexie" needs this
       "@swc/jest",
       {
         jsc: {
@@ -15,6 +12,7 @@ module.exports = {
             },
           },
         },
+        target: "es2016",
       },
     ],
     "^.+\\.(t|j)sx?$": [
@@ -26,14 +24,19 @@ module.exports = {
               runtime: "automatic",
             },
           },
+          target: "es2016",
         },
       },
     ],
   },
   transformIgnorePatterns: ["node_modules/(?!(@runcitadel))/"],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: "<rootDir>/",
-  }),
+  moduleNameMapper: {
+    // needs to align with "tsconfig.json"-paths
+    // swc does not provide "pathsToModuleNameMapper" as ts-jest does
+    "^~/(.*)$": "<rootDir>/src/$1",
+    "^@components/(.*)$": "<rootDir>/src/app/components/$1",
+    "^@screens/(.*)$": "<rootDir>/src/app/screens/$1",
+  },
   setupFiles: ["jest-webextension-mock", "fake-indexeddb/auto"],
   testEnvironment: "./jest.custom-test-environment.ts",
   setupFilesAfterEnv: ["./jest.setup.ts"],
