9981d7b9e08352ca5c7c2675a3aa69a45340614c
Pavanjoshi914 <pavanj914@gmail.com>
2022-07-04 04:17:51 +0530

    feat: validate metadata using AJV validator signed-off-by: pavan joshi <pavanj914@gmail.com>

--- src/app/components/PaymentSummary.tsx
+++ src/app/components/PaymentSummary.tsx
@@ -1,10 +1,48 @@
+import Ajv from "ajv";
+import { audioObjectSchema } from "~/extension/ln/webln/MetadataSchemas/audioObject";
+
 type Props = {
   amount: string | React.ReactNode;
   amountAlt?: string;
   description?: string | React.ReactNode;
+  metadata?: { [key: string]: string };
 };
 
-function PaymentSummary({ amount, amountAlt, description }: Props) {
+const ajv = new Ajv({ allErrors: true, strict: false });
+const validate = ajv.compile(audioObjectSchema);
+
+function PaymentSummary({ amount, amountAlt, description, metadata }: Props) {
+  if (validate(metadata)) {
+    // data is MyData here
+    // console.log(metadata.name)
+  } else {
+    //console.log(validate.errors)
+  }
+
+  const metadataElements: JSX.Element[] = [];
+  if (metadata != undefined) {
+    for (const key in metadata) {
+      if (key[0] != "@") {
+        metadataElements.push(
+          <>
+            <dt className="mt-4 font-medium text-gray-800 dark:text-white">
+              {key}
+            </dt>
+            <dd className="mb-0 text-gray-600 dark:text-neutral-500 break-all">
+              {key == "image" ? (
+                <img src={`data:image/png;base64, ${metadata[key]}`} />
+              ) : (
+                metadata[key]
+              )}
+            </dd>
+          </>
+        );
+      }
+    }
+  } else {
+    metadataElements.push(<p>&quot;No Metadata Present&quot;</p>);
+  }
+
   return (
     <dl className="mb-0">
       <dt className="font-medium text-gray-800 dark:text-white">Amount</dt>
@@ -18,6 +56,12 @@ function PaymentSummary({ amount, amountAlt, description }: Props) {
       <dd className="mb-0 text-gray-600 dark:text-neutral-500 break-all">
         {description}
       </dd>
+      <dt className="mt-4 font-medium text-gray-800 dark:text-white">
+        Metadata
+      </dt>
+      {metadataElements.map((metadata) => (
+        <>{metadata}</>
+      ))}
     </dl>
   );
 }
