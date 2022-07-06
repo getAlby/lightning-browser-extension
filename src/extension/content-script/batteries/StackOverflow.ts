f82d2978987d79f23de3d43671c9e68edc2bd174
im-adithya <imadithyavardhan@gmail.com>
2022-07-06 13:26:48 +0530

    feat: add tipping for accepted answers

--- src/extension/content-script/batteries/StackOverflow.ts
+++ src/extension/content-script/batteries/StackOverflow.ts
@@ -16,6 +16,10 @@ const battery = async (): Promise<void> => {
     lightningData = await handleProfilePage(userOrQuestionId);
   }
 
+  if (route === "questions") {
+    lightningData = await handleQuestionPage(userOrQuestionId);
+  }
+
   if (lightningData) setLightningData([lightningData]);
 };
 
@@ -30,12 +34,48 @@ function htmlToText(html: string) {
   return temp.textContent;
 }
 
+async function handleQuestionPage(questionId: string): Promise<Battery | null> {
+  const questionInfo = await fetch(
+    // The filter can be generated here: https://api.stackexchange.com/docs/users-by-ids
+    `https://api.stackexchange.com/2.2/questions/${questionId}?site=stackoverflow&filter=!9bOY8fLl6`
+  ).then((res) => res.json());
+  if (questionInfo.error_id) {
+    return null;
+  }
+
+  const questionData = questionInfo.items[0];
+
+  if (!questionData || !questionData.accepted_answer_id) {
+    return null;
+  }
+
+  const answerInfo = await fetch(
+    // The filter can be generated here: https://api.stackexchange.com/docs/users-by-ids
+    `https://api.stackexchange.com/2.2/answers/${questionData.accepted_answer_id}?site=stackoverflow&filter=!-)QWsbXSB(JQ`
+  ).then((res) => res.json());
+
+  const answerData = answerInfo.items[0];
+
+  if (!answerData || !answerData.owner) {
+    return null;
+  }
+
+  const answererId = answerData.owner.user_id;
+  const lightningData = await handleProfilePage(answererId);
+
+  return lightningData;
+}
+
 async function handleProfilePage(userId: string): Promise<Battery | null> {
-  const userProfile = await fetch(
+  const userInfo = await fetch(
     // The filter can be generated here: https://api.stackexchange.com/docs/users-by-ids
     `https://api.stackexchange.com/2.2/users/${userId}?site=stackoverflow&filter=!-B3yqvQ2YYGK1t-Hg_ydU`
   ).then((res) => res.json());
-  const userData = userProfile.items[0];
+  if (userInfo.error_id) {
+    return null;
+  }
+
+  const userData = userInfo.items[0];
 
   if (!userData) {
     return null;
