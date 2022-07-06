2984072f35be7c84eaf4992ba40372e5c451e9d0
escapedcat <github@htmlcss.de>
2022-07-06 15:32:03 +0800

    test(transactionsList): invoices w/wo boostagram #389

--- src/app/components/TransactionsTable/index.test.tsx
+++ src/app/components/TransactionsTable/index.test.tsx
@@ -40,15 +40,6 @@ const invoices: Props = {
       date: "4 days ago",
     },
     {
-      id: "lnbcrt888880n1p3tad30pp56j6g34wctydrfx4wwdwj3schell8uqug6jnlehlkpw02mdfd9wlqdq0v36k6urvd9hxwuccqzpgxqyz5vqsp5995q4egstsvnyetwvpax6jw8q0fnn4tyz3gp35k3yex29emhsylq9qyyssq0yxpx6peyn4vsepwj3l68w9sc5dqnkt07zff6aw4kqvcfs0fpu4jpfh929w6vqrgtjfkmrlwghq4s9t4mnwrh4dlkm6wjem5uq8eu4gpwqln0j",
-      type: "received",
-      totalAmount: "88888",
-      totalAmountFiat: "$17.36",
-      preimage: "",
-      title: "dumplings",
-      date: "5 days ago",
-    },
-    {
       id: "lnbcrt6543210n1p3tadjepp5rv6ufq4vumg66l9gcyxqhy89n6w90mx0mh6gcj0sawrf6xuep5ssdq5g9kxy7fqd9h8vmmfvdjscqzpgxqyz5vqsp5f9yzxeqjw33ule4rffuh0py32gjjsx8z48cd4xjl8ej3rn7zdtdq9qyyssqe6qvkfe260myc9ypgs5n63xzwcx82fderg8p5ysh6c2fvpz5xu4ksvhs5av0wwestk5pmucmhk8lpjhmy7wqyq9c29xgm9na2q5xv5spy5kukj",
       type: "received",
       totalAmount: "654321",
@@ -56,6 +47,44 @@ const invoices: Props = {
       preimage: "",
       title: "Alby invoice",
       date: "6 days ago",
+    },
+  ],
+};
+
+const invoicesWithBoostagram: Props = {
+  transactions: [
+    {
+      id: "lnbcrt666660n1p3tad0hpp5kkguywerj5lqspc4p2a7f53yfnkuywxmxnuawe3lu4gdg0ezc2tqdqjd3sk6cn0ypkxzmtzducqzpgxqyz5vqsp529wvk52ckjkrfkll9q3w6ep6lrsg35se66jjpm5ssmumck7xxy6s9qyyssqzq3zsstfs7gzklgkdnxy2hsp4jfavw8xj4hv5300yww3053jx76h57e3ypsuvg36zwd49xm2nfr2lrfvylwrxs7yhpckjytvlaju0hsq7p9wna",
+      type: "received",
+      totalAmount: "66666",
+      totalAmountFiat: "$13.02",
+      preimage: "",
+      title: "lambo lambo",
+      date: "4 days ago",
+    },
+    {
+      id: "lnbcrt888880n1p3tad30pp56j6g34wctydrfx4wwdwj3schell8uqug6jnlehlkpw02mdfd9wlqdq0v36k6urvd9hxwuccqzpgxqyz5vqsp5995q4egstsvnyetwvpax6jw8q0fnn4tyz3gp35k3yex29emhsylq9qyyssq0yxpx6peyn4vsepwj3l68w9sc5dqnkt07zff6aw4kqvcfs0fpu4jpfh929w6vqrgtjfkmrlwghq4s9t4mnwrh4dlkm6wjem5uq8eu4gpwqln0j",
+      type: "received",
+      totalAmount: "88888",
+      totalAmountFiat: "$17.36",
+      preimage: "",
+      title: "dumplings",
+      date: "5 days ago",
+      boostagram: {
+        app_name: "Fountain",
+        name: "Friedemann",
+        podcast: "Honigdachs",
+        url: "https://coinspondent.de/honigdachs-der-bitcoin-podcast-aus-leipzig",
+        episode: undefined,
+        itemID: undefined,
+        ts: undefined,
+        message: "Du bist so 1 geiles podcast",
+        sender_id: "123456",
+        sender_name: "Alby",
+        time: "00:00",
+        action: "boost",
+        value_msat_total: 123456,
+      },
     },
   ],
 };
@@ -81,14 +110,12 @@ describe("TransactionsTable", () => {
       user.click(disclosureButton);
     });
 
-    await waitFor(() =>
-      expect(
-        screen.getByText("https://openai.com/dall-e-2/")
-      ).toBeInTheDocument()
-    );
+    expect(
+      await screen.findByText("https://openai.com/dall-e-2/")
+    ).toBeInTheDocument();
   });
 
-  test("renders invoices", async () => {
+  test("renders invoice without boostagram", async () => {
     render(
       <BrowserRouter>
         <TransactionsTable {...invoices} />
@@ -102,5 +129,31 @@ describe("TransactionsTable", () => {
 
     const disclosureButtons = screen.queryByRole("button");
     expect(disclosureButtons).not.toBeInTheDocument();
+  });
+
+  test("renders invoice with boostagram", async () => {
+    const user = userEvent.setup();
+
+    render(
+      <BrowserRouter>
+        <TransactionsTable {...invoicesWithBoostagram} />
+      </BrowserRouter>
+    );
+
+    expect(screen.getByText("dumplings")).toBeInTheDocument();
+    expect(screen.getByText(/received - 5 days ago/)).toBeInTheDocument();
+    expect(screen.getByText(/\+88888 sats/)).toBeInTheDocument();
+    expect(screen.getByText(/~\$17.36/)).toBeInTheDocument();
+
+    const disclosureButtons = screen.getAllByRole("button");
+    expect(disclosureButtons).toHaveLength(1);
+
+    await act(() => {
+      user.click(disclosureButtons[0]);
+    });
+
+    expect(
+      await screen.findByText(/message: Du bist so 1 geiles podcast/)
+    ).toBeInTheDocument();
   });
 });
