dbacdcf23c39bc449e40dc9d6880230fc4bacd8a
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-07-06 21:59:18 -0600

    refactor: move into async function so we can await and set order

--- src/i18n/i18nConfig.ts
+++ src/i18n/i18nConfig.ts
@@ -9,10 +9,6 @@ import en_components from "./locales/en/components.json";
 import en from "./locales/en/translation.json";
 import hi from "./locales/hi/translation.json";
 
-api.getSettings().then((setting) => {
-  i18n.changeLanguage(setting.locale);
-});
-
 // Load namespaces
 export const resources = {
   en: {
@@ -26,18 +22,25 @@ export const resources = {
   },
 };
 
-i18n
-  // pass the i18n instance to react-i18next.
-  .use(initReactI18next)
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(LanguageDetector)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
-  .init({
-    //debug: true,
-    fallbackLng: "en",
-    resources,
-  });
+const loadLanguage = async () => {
+  await i18n
+    // pass the i18n instance to react-i18next.
+    .use(initReactI18next)
+    // detect user language
+    // learn more: https://github.com/i18next/i18next-browser-languageDetector
+    .use(LanguageDetector)
+    // init i18next
+    // for all options read: https://www.i18next.com/overview/configuration-options
+    .init({
+      //debug: true,
+      fallbackLng: "en",
+      resources,
+    });
+
+  const setting = await api.getSettings();
+  i18n.changeLanguage(setting.locale);
+};
+
+loadLanguage();
 
 export default i18n;
