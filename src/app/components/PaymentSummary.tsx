bdb804cc85041f08feb2d78fe48cf251a3807b85
Pavanjoshi914 <pavanj914@gmail.com>
2022-07-19 20:49:28 +0530

    feat: use zod to validate metadata create metadata validator function which can validate type and image encoding of metadata create basic schema for audioObject compile schemas dynamically depending on the type of metadata passed and validate them add key prop to uniquely identify metadata in the list synchronize dependency versions of plugins with upstream/master in yarn.lock file signed-off-by: Pavan Joshi <pavanj914@gmail.com>

--- src/app/components/PaymentSummary.tsx
+++ src/app/components/PaymentSummary.tsx
@@ -1,10 +1,42 @@
+import { Fragment } from "react";
+import { MetadataValidator } from "~/schema/metadataValidator";
+
 type Props = {
   amount: string | React.ReactNode;
   amountAlt?: string;
   description?: string | React.ReactNode;
+  metadata?: { [key: string]: string };
 };
 
-function PaymentSummary({ amount, amountAlt, description }: Props) {
+function PaymentSummary({ amount, amountAlt, description, metadata }: Props) {
+  const metadataElements: JSX.Element[] = [];
+  if (metadata != undefined) {
+    const isMetadataValid = MetadataValidator(metadata);
+
+    if (isMetadataValid) {
+      for (const key in metadata) {
+        metadataElements.push(
+          <>
+            <dt className="mt-4 font-medium text-gray-800 dark:text-white">
+              {key}
+            </dt>
+            <dd className="mb-0 text-gray-600 dark:text-neutral-500 break-all">
+              {key == "image" ? (
+                <img src={`data:image/png;base64, ${metadata[key]}`} />
+              ) : (
+                metadata[key]
+              )}
+            </dd>
+          </>
+        );
+      }
+    } else {
+      metadataElements.push(<p>&quot;No valid Metadata Provided&quot;</p>);
+    }
+  } else {
+    metadataElements.push(<p>&quot;No Metadata Present&quot;</p>);
+  }
+
   return (
     <dl className="mb-0">
       <dt className="font-medium text-gray-800 dark:text-white">Amount</dt>
@@ -18,6 +50,12 @@ function PaymentSummary({ amount, amountAlt, description }: Props) {
       <dd className="mb-0 text-gray-600 dark:text-neutral-500 break-all">
         {description}
       </dd>
+      <dt className="mt-4 font-medium text-gray-800 dark:text-white">
+        Metadata
+      </dt>
+      {metadataElements.map((metadata, key) => (
+        <Fragment key={key}>{metadata}</Fragment>
+      ))}
     </dl>
   );
 }
