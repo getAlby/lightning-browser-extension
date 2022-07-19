e7bea6e2353a06782b5ce323d69bd0cc9805c139
escapedcat <github@htmlcss.de>
2022-07-19 11:07:03 +0800

    fix: module import #389

--- src/app/components/TransactionsTable/index.test.tsx
+++ src/app/components/TransactionsTable/index.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/React";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { act } from "react-dom/test-utils";
 import { I18nextProvider } from "react-i18next";
