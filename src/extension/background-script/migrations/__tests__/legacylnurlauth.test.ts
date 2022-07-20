0dada0ad4244c93389bc8052ede966365538d24d
Michael Bumann <hello@michaelbumann.com>
2022-07-20 15:44:03 +0200

    test: proper migration tests

--- src/extension/background-script/migrations/__tests__/legacylnurlauth.test.ts
+++ src/extension/background-script/migrations/__tests__/legacylnurlauth.test.ts
@@ -8,6 +8,7 @@ describe("With lnurlAuth enabled allowance", () => {
   afterEach(() => {
     jest.clearAllMocks();
     state.getState().reset();
+    db.allowances.toCollection().delete();
   });
 
   const mockAllowances: Allowance[] = [
@@ -40,6 +41,12 @@ describe("With lnurlAuth enabled allowance", () => {
 });
 
 describe("Without lnurlAuth enabled allowance", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+    state.getState().reset();
+    db.allowances.toCollection().delete();
+  });
+
   const mockAllowances: Allowance[] = [
     {
       enabled: true,
@@ -69,11 +76,39 @@ describe("Without lnurlAuth enabled allowance", () => {
   });
 });
 
-/*
 describe("Migrations already executed", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+    state.getState().reset();
+    db.allowances.toCollection().delete();
+  });
+
+  const mockAllowances: Allowance[] = [
+    {
+      enabled: true,
+      host: "numbergoup.ngu",
+      id: 1,
+      imageURL: "https://numbergoup.ngu/favicon.ico",
+      lastPaymentAt: "0",
+      lnurlAuth: true,
+      name: "ngu",
+      remainingBudget: 500,
+      totalBudget: 500,
+      createdAt: "123456",
+      payments: [],
+      paymentsAmount: 0,
+      paymentsCount: 0,
+      percentage: "0",
+      usedBudget: 0,
+      tag: "",
+    },
+  ];
+
   test("does not run the migration twice", async () => {
-    state.setState({ migrations: ["foo"] });
-    //...
+    state.setState({ migrations: ["migrateisUsingLegacyLnurlAuthKeySetting"] });
+    expect(state.getState().settings.isUsingLegacyLnurlAuthKey).toEqual(false);
+    await db.allowances.bulkAdd(mockAllowances);
+    await migrate();
+    expect(state.getState().settings.isUsingLegacyLnurlAuthKey).toEqual(false);
   });
 });
-*/
