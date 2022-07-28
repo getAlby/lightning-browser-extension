55109152eb35b3245b9e78c1d3b4759403099f32
Michael Bumann <hello@michaelbumann.com>
2022-07-28 18:46:42 +0200

    feat(alby wallet): directly go to final success screen and save the account after creation

--- src/app/screens/connectors/NewWallet/index.tsx
+++ src/app/screens/connectors/NewWallet/index.tsx
@@ -13,13 +13,14 @@ import utils from "~/common/lib/utils";
 const walletCreateUrl =
   process.env.WALLET_CREATE_URL || "https://app.regtest.getalby.com/api/users";
 
+interface LNDHubCreateResponse {
+  login: string;
+  password: string;
+  url: string;
+  lnAddress: string;
+}
+
 export default function NewWallet() {
-  const [lndHubData, setLndHubData] = useState({
-    login: "",
-    password: "",
-    url: "",
-    lnAddress: "",
-  });
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordView, setPasswordView] = useState(false);
@@ -52,7 +53,8 @@ export default function NewWallet() {
       .then((res) => res.json())
       .then((data) => {
         if (data.lndhub?.login && data.lndhub?.password && data.lndhub?.url) {
-          setLndHubData({
+          setLoading(false);
+          next({
             ...data.lndhub,
             lnAddress: data.lightning_address,
           });
@@ -63,6 +65,7 @@ export default function NewWallet() {
               data
             )}`
           );
+          setLoading(false);
         }
       })
       .catch((e) => {
@@ -70,17 +73,14 @@ export default function NewWallet() {
         toast.error(
           `${t("pre_connect.errors.create_wallet_error")} ${e.message}`
         );
-      })
-      .finally(() => {
         setLoading(false);
       });
   }
 
-  async function next(event: React.FormEvent<HTMLFormElement>) {
+  async function next(lndhub: LNDHubCreateResponse) {
     setLoading(true);
-    event.preventDefault();
 
-    const { login, password, url, lnAddress } = lndHubData;
+    const { login, password, url, lnAddress } = lndhub;
     const name = lnAddress || "Alby"; // use the ln address as name or Alby to default
     const account = {
       name,
@@ -121,116 +121,92 @@ export default function NewWallet() {
 
   return (
     <ConnectorForm
-      title={
-        lndHubData.login === ""
-          ? t("pre_connect.title")
-          : t("post_connect.title")
-      }
+      title={t("pre_connect.title")}
       submitLabel={tCommon("actions.continue")}
       submitLoading={loading}
-      onSubmit={lndHubData.login ? next : signup}
-      submitDisabled={password === "" || email === ""}
+      onSubmit={signup}
+      submitDisabled={loading || password === "" || email === ""}
     >
-      {lndHubData.login ? (
-        <>
-          <div className="mt-6 dark:text-white">
-            <p>
-              <strong>
-                {t("post_connect.account_ready")}
-                <br />
-              </strong>
-            </p>
-            {lndHubData.lnAddress && (
-              <p>
-                {t("post_connect.lightning_address")} {lndHubData.lnAddress}
-              </p>
-            )}
-          </div>
-        </>
-      ) : (
-        <>
-          <div className="mt-6 dark:text-white">
-            <strong>
-              {t("pre_connect.login_account")}
-              <br />
-              {t("pre_connect.host_wallet")}
-            </strong>
-          </div>
+      <div className="mt-6 dark:text-white">
+        <strong>
+          {t("pre_connect.login_account")}
+          <br />
+          {t("pre_connect.host_wallet")}
+        </strong>
+      </div>
 
-          <div className="mt-6">
-            <TextField
-              id="email"
-              label={t("pre_connect.email_label")}
-              type="email"
-              required
-              onChange={(e) => {
-                setEmail(e.target.value.trim());
-              }}
-            />
-          </div>
-          <div className="mt-6">
-            <TextField
-              id="password"
-              label={tCommon("password")}
-              type={passwordView ? "text" : "password"}
-              minLength={6}
-              pattern=".{6,}"
-              title="at least 6 characters"
-              required
-              onChange={(e) => {
-                setPassword(e.target.value.trim());
-              }}
-              endAdornment={
-                <button
-                  type="button"
-                  className="mr-1 flex justify-center items-center w-10 h-8"
-                  onClick={() => setPasswordView(!passwordView)}
-                >
-                  {passwordView ? (
-                    <HiddenIcon className="h-6 w-6" />
-                  ) : (
-                    <VisibleIcon className="h-6 w-6" />
-                  )}
-                </button>
-              }
-            />
-          </div>
-          <div className="mt-6">
-            <p className="mb-2 text-gray-700 dark:text-neutral-400">
-              Your Alby account also comes with an optional{" "}
-              <a
-                className="underline"
-                href="https://lightningaddress.com/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                {t("pre_connect.optional_lightning_note.part2")}
-              </a>
-              {t("pre_connect.optional_lightning_note.part3")} (
-              <a
-                className="underline"
-                href="https://lightningaddress.com/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                {t("pre_connect.optional_lightning_note.part4")}
-              </a>
-              )
-            </p>
-            <div>
-              <TextField
-                id="lnAddress"
-                label={t("pre_connect.optional_lightning_address_label")}
-                suffix={t("pre_connect.optional_lightning_address_suffix")}
-                type="text"
-                onChange={(e) => {
-                  setLnAddress(e.target.value.trim().split("@")[0]); // in case somebody enters a full address we simple remove the domain
-                }}
-              />
-            </div>
-          </div>
-        </>
-      )}
+      <div className="mt-6">
+        <TextField
+          id="email"
+          label={t("pre_connect.email_label")}
+          type="email"
+          required
+          onChange={(e) => {
+            setEmail(e.target.value.trim());
+          }}
+        />
+      </div>
+      <div className="mt-6">
+        <TextField
+          id="password"
+          label={tCommon("password")}
+          type={passwordView ? "text" : "password"}
+          minLength={6}
+          pattern=".{6,}"
+          title="at least 6 characters"
+          required
+          onChange={(e) => {
+            setPassword(e.target.value.trim());
+          }}
+          endAdornment={
+            <button
+              type="button"
+              className="mr-1 flex justify-center items-center w-10 h-8"
+              onClick={() => setPasswordView(!passwordView)}
+            >
+              {passwordView ? (
+                <HiddenIcon className="h-6 w-6" />
+              ) : (
+                <VisibleIcon className="h-6 w-6" />
+              )}
+            </button>
+          }
+        />
+      </div>
+      <div className="mt-6">
+        <p className="mb-2 text-gray-700 dark:text-neutral-400">
+          Your Alby account also comes with an optional{" "}
+          <a
+            className="underline"
+            href="https://lightningaddress.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {t("pre_connect.optional_lightning_note.part2")}
+          </a>
+          {t("pre_connect.optional_lightning_note.part3")} (
+          <a
+            className="underline"
+            href="https://lightningaddress.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {t("pre_connect.optional_lightning_note.part4")}
+          </a>
+          )
+        </p>
+        <div>
+          <TextField
+            id="lnAddress"
+            label={t("pre_connect.optional_lightning_address_label")}
+            suffix={t("pre_connect.optional_lightning_address_suffix")}
+            type="text"
+            onChange={(e) => {
+              setLnAddress(e.target.value.trim().split("@")[0]); // in case somebody enters a full address we simple remove the domain
+            }}
+          />
+        </div>
+      </div>
     </ConnectorForm>
   );
 }
