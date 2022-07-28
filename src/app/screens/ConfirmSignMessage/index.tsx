2291bfe7088d8e1437d3ec0c8406cfd4907bbe28
im-adithya <imadithyavardhan@gmail.com>
2022-07-28 18:36:34 +0530

    chore: revamp confirmsignmessage screen

--- src/app/screens/ConfirmSignMessage/index.tsx
+++ src/app/screens/ConfirmSignMessage/index.tsx
@@ -1,10 +1,9 @@
-import ConfirmOrCancel from "@components/ConfirmOrCancel";
-import Container from "@components/Container";
 //import Checkbox from "../../components/Form/Checkbox";
-import PublisherCard from "@components/PublisherCard";
 import SuccessMessage from "@components/SuccessMessage";
 import { useState, useRef } from "react";
 import { toast } from "react-toastify";
+import NewConfirmOrCancel from "~/app/newcomponents/NewConfirmOrCancel";
+import NewPublisherCard from "~/app/newcomponents/NewPublisherCard";
 import { USER_REJECTED_ERROR } from "~/common/constants";
 import msg from "~/common/lib/msg";
 import utils from "~/common/lib/utils";
@@ -21,7 +20,7 @@ function ConfirmSignMessage(props: Props) {
   const originRef = useRef(props.origin || getOriginData());
   //const [rememberMe, setRememberMe] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [succesMessage, setSuccessMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
 
   async function confirm() {
     //if (rememberMe) {
@@ -55,58 +54,60 @@ function ConfirmSignMessage(props: Props) {
   }
 
   return (
-    <div>
-      <PublisherCard
-        title={originRef.current.name}
-        image={originRef.current.icon}
-      />
-      <div className="py-4">
-        <Container maxWidth="sm">
-          {!succesMessage ? (
-            <>
-              <dl className="shadow bg-white dark:bg-surface-02dp p-4 rounded-lg mb-8">
-                <dt className="font-semibold text-gray-500">
-                  {originRef.current.host} asks you to sign:
-                </dt>
-                <dd className="mb-6 dark:text-white">{messageRef.current}</dd>
-              </dl>
-
-              <div className="mb-8">
-                {/*
-              <div className="flex items-center">
-                <Checkbox
-                  id="remember_me"
-                  name="remember_me"
-                  checked={rememberMe}
-                  onChange={(event) => {
-                    setRememberMe(event.target.checked);
-                  }}
-                />
-                <label
-                  htmlFor="remember_me"
-                  className="ml-2 block text-sm text-gray-900 font-medium dark:text-white"
-                >
-                  Remember and auto sign in the future
-                </label>
-              </div>
-              */}
-              </div>
-
-              <ConfirmOrCancel
-                disabled={loading}
-                loading={loading}
-                onConfirm={confirm}
-                onCancel={reject}
-              />
-            </>
-          ) : (
-            <SuccessMessage
-              message={succesMessage}
-              onClose={() => window.close()}
-            />
-          )}
-        </Container>
+    <div className="h-full">
+      <div className="h-2/5 border-b border-gray-200 dark:border-neutral-500">
+        <NewPublisherCard
+          title={originRef.current.name}
+          image={originRef.current.icon}
+        />
       </div>
+      {!successMessage ? (
+        <div className="flex flex-col justify-between h-3/5">
+          <dl className="m-6 shadow bg-white dark:bg-surface-02dp p-4 rounded-lg">
+            <dt className="font-semibold text-gray-500">
+              {originRef.current.host} asks you to sign:
+            </dt>
+            <dd className="mb-6 dark:text-white">{messageRef.current}</dd>
+          </dl>
+
+          <div className="mb-8">
+            {/*
+          <div className="flex items-center">
+            <Checkbox
+              id="remember_me"
+              name="remember_me"
+              checked={rememberMe}
+              onChange={(event) => {
+                setRememberMe(event.target.checked);
+              }}
+            />
+            <label
+              htmlFor="remember_me"
+              className="ml-2 block text-sm text-gray-900 font-medium dark:text-white"
+            >
+              Remember and auto sign in the future
+            </label>
+          </div>
+          */}
+          </div>
+
+          <div className="text-center p-2">
+            <NewConfirmOrCancel
+              disabled={loading}
+              loading={loading}
+              onConfirm={confirm}
+              onCancel={reject}
+            />
+          </div>
+        </div>
+      ) : (
+        <div className="m-6">
+          <SuccessMessage
+            message={successMessage}
+            onClose={() => window.close()}
+          />
+        </div>
+      )}
     </div>
   );
 }
