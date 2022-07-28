c063596055e72b2234687568374055fe36e98eb3
im-adithya <imadithyavardhan@gmail.com>
2022-07-28 18:34:52 +0530

    chore: revamp confirmpayment screen

--- src/app/screens/ConfirmPayment/index.tsx
+++ src/app/screens/ConfirmPayment/index.tsx
@@ -1,8 +1,5 @@
 import BudgetControl from "@components/BudgetControl";
-import ConfirmOrCancel from "@components/ConfirmOrCancel";
-import Container from "@components/Container";
 import PaymentSummary from "@components/PaymentSummary";
-import PublisherCard from "@components/PublisherCard";
 import SuccessMessage from "@components/SuccessMessage";
 import lightningPayReq from "bolt11";
 import { useEffect, useRef, useState } from "react";
@@ -10,6 +7,8 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import { useAccount } from "~/app/context/AccountContext";
 import { useSettings } from "~/app/context/SettingsContext";
+import NewConfirmOrCancel from "~/app/newcomponents/NewConfirmOrCancel";
+import NewPublisherCard from "~/app/newcomponents/NewPublisherCard";
 import { USER_REJECTED_ERROR } from "~/common/constants";
 import msg from "~/common/lib/msg";
 import utils from "~/common/lib/utils";
@@ -100,51 +99,53 @@ function ConfirmPayment(props: Props) {
   }
 
   return (
-    <div>
-      <PublisherCard
-        title={originRef.current.name}
-        image={originRef.current.icon}
-      />
-      <div className="py-4">
-        <Container maxWidth="sm">
-          {!successMessage ? (
-            <>
-              <h1 className="dark:text-white font-bold mb-4">
-                Approve payment
-              </h1>
-              <div className="mb-6">
-                <PaymentSummary
-                  amount={invoiceRef.current?.satoshis}
-                  description={invoiceRef.current?.tagsObject.description}
-                />
-              </div>
-
-              <BudgetControl
-                fiatAmount={fiatAmount}
-                remember={rememberMe}
-                onRememberChange={(event) => {
-                  setRememberMe(event.target.checked);
-                }}
-                budget={budget}
-                onBudgetChange={(event) => setBudget(event.target.value)}
-              />
-
-              <ConfirmOrCancel
-                disabled={loading}
-                loading={loading}
-                onConfirm={confirm}
-                onCancel={reject}
-                label="Pay now"
-              />
-            </>
-          ) : (
-            <SuccessMessage
-              message={successMessage}
-              onClose={() => window.close()}
-            />
-          )}
-        </Container>
+    <div className="overflow-y-auto no-scrollbar h-full">
+      <div className="h-2/5 border-b border-gray-200 dark:border-neutral-500">
+        <NewPublisherCard
+          title={originRef.current.name}
+          image={originRef.current.icon}
+        />
       </div>
+      {!successMessage ? (
+        <div className="flex flex-col justify-between h-3/5">
+          <div className="pt-4 px-4">
+            <h1 className="dark:text-white font-bold mb-4">Approve payment</h1>
+            <div className="mb-6">
+              <PaymentSummary
+                amount={invoiceRef.current?.satoshis}
+                description={invoiceRef.current?.tagsObject.description}
+              />
+            </div>
+
+            <BudgetControl
+              fiatAmount={fiatAmount}
+              remember={rememberMe}
+              onRememberChange={(event) => {
+                setRememberMe(event.target.checked);
+              }}
+              budget={budget}
+              onBudgetChange={(event) => setBudget(event.target.value)}
+            />
+          </div>
+
+          <div className="text-center p-2">
+            <NewConfirmOrCancel
+              disabled={loading}
+              loading={loading}
+              onConfirm={confirm}
+              onCancel={reject}
+              label="Pay now"
+            />
+          </div>
+        </div>
+      ) : (
+        <div className="m-6">
+          <SuccessMessage
+            message={successMessage}
+            onClose={() => window.close()}
+          />
+        </div>
+      )}
     </div>
   );
 }
