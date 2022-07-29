f6d55bd940e5a41257b755b55ffff4da4bff46df
escapedcat <github@htmlcss.de>
2022-07-29 15:55:31 +0800

    chore(connector): adjust unsupported message for getInvoices #389

--- src/extension/background-script/connectors/eclair.ts
+++ src/extension/background-script/connectors/eclair.ts
@@ -52,9 +52,11 @@ class Eclair implements Connector {
   // not yet implemented
   getInvoices() {
     console.error(
-      `${this.constructor.name} does not implement the getInvoices call`
+      `Not yet supported with the currently used account: ${this.constructor.name}`
     );
-    return new Error("Not yet supported with the currently used account.");
+    return new Error(
+      `${this.constructor.name}: "getInvoices" is not yet supported. Contact us if you need it.`
+    );
   }
 
   async getBalance(): Promise<GetBalanceResponse> {
