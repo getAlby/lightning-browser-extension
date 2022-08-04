43c8809c7996a4329cfdcdfddca396e727cc5403
Michael Bumann <hello@michaelbumann.com>
2022-08-05 01:31:43 +0200

    style: screenheader font size

--- src/app/components/ScreenHeader/index.tsx
+++ src/app/components/ScreenHeader/index.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 function ScreenHeader({ title }: Props) {
   return (
-    <div className="text-center text-xl font-semibold dark:text-white py-2 border-b border-gray-200 dark:border-neutral-500">
+    <div className="text-center text-lg font-semibold dark:text-white py-2 border-b border-gray-200 dark:border-neutral-500">
       {title}
     </div>
   );
