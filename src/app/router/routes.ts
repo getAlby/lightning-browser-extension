9e3cc1787217ffd6deaa7a1d018fd430fbde625e
Lisa Oppermann <lisa@lisabaut.de>
2022-08-10 21:08:01 +0200

    refactor: remove unused file

--- src/app/router/routes.ts
+++
@@ -1,9 +0,0 @@
-// PATHs names react-router is matched to in path={}
-export const PATHS = {
-  lnurlPay: "lnurlPay",
-} as const;
-
-// URLs which should match the router PATHs to call `navigate(URL)`
-export const URLS = {
-  lnurlPay: `/${PATHS.lnurlPay}`,
-};
