8a68fd488048562c5e726f8c552f697984c6eef7
im-adithya <imadithyavardhan@gmail.com>
2022-08-17 19:30:45 +0530

    feat: i18n for enable

--- src/app/screens/Enable/index.tsx
+++ src/app/screens/Enable/index.tsx
@@ -3,6 +3,7 @@ import ConfirmOrCancel from "@components/ConfirmOrCancel";
 import Container from "@components/Container";
 import PublisherCard from "@components/PublisherCard";
 import { useState, useEffect, useCallback, useRef } from "react";
+import { useTranslation } from "react-i18next";
 import ScreenHeader from "~/app/components/ScreenHeader";
 import { USER_REJECTED_ERROR } from "~/common/constants";
 import msg from "~/common/lib/msg";
@@ -19,6 +20,10 @@ function Enable(props: Props) {
   const [remember] = useState(true);
   const [, setEnabled] = useState(false);
   const [budget] = useState(null);
+  const { t } = useTranslation("translation", {
+    keyPrefix: "enable",
+  });
+  const { t: tCommon } = useTranslation("common");
 
   const enable = useCallback(() => {
     setEnabled(true);
@@ -83,25 +88,21 @@ function Enable(props: Props) {
           />
 
           <div className="dark:text-white pt-6">
-            <p className="mb-4">Allow {props.origin.host} to:</p>
+            <p className="mb-4">{t("allow", { host: props.origin.host })}</p>
 
             <div className="mb-4 flex items-center">
               <CheckIcon className="w-5 h-5 mr-2" />
-              <p className="dark:text-white">
-                Request approval for transactions
-              </p>
+              <p className="dark:text-white">{t("request_one")}</p>
             </div>
             <div className="mb-4 flex items-center">
               <CheckIcon className="w-5 h-5 mr-2" />
-              <p className="dark:text-white">
-                Request invoices and lightning information
-              </p>
+              <p className="dark:text-white">{t("request_two")}</p>
             </div>
           </div>
         </div>
         <div className="mb-4 text-center flex flex-col">
           <ConfirmOrCancel
-            label="Connect"
+            label={tCommon("actions.connect")}
             onConfirm={enable}
             onCancel={reject}
           />
@@ -110,7 +111,7 @@ function Enable(props: Props) {
             href="#"
             onClick={block}
           >
-            Block and ignore {props.origin.host}
+            {t("block_and_ignore", { host: props.origin.host })}
           </a>
         </div>
       </Container>
