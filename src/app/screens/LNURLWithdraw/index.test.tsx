d9a5db627039fe60e3ef97669bc39f19a08a1192
escapedcat <github@htmlcss.de>
2022-08-22 15:15:59 +0800

    test(msw): use general setup for all tests #1302

--- src/app/screens/LNURLWithdraw/index.test.tsx
+++ src/app/screens/LNURLWithdraw/index.test.tsx
@@ -1,7 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { rest } from "msw";
-import { setupServer } from "msw/node";
 import { act } from "react-dom/test-utils";
 import { MemoryRouter } from "react-router-dom";
 import { settingsFixture as mockSettings } from "~/../tests/fixtures/settings";
@@ -11,46 +9,6 @@ import { makeInvoice } from "~/common/lib/api";
 import type { LNURLWithdrawServiceResponse, OriginData } from "~/types";
 
 import LNURLWithdraw from "./index";
-
-const server = setupServer(
-  rest.get(
-    "https://api.coindesk.com/v1/bpi/currentprice/usd.json",
-    (req, res, ctx) => {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          time: {
-            updated: "Jun 10, 2022 09:06:00 UTC",
-            updatedISO: "2022-06-10T09:06:00+00:00",
-            updateduk: "Jun 10, 2022 at 10:06 BST",
-          },
-          disclaimer:
-            "This data was produced from the CoinDesk Bitcoin Price Index (USD). Non-USD currency data converted using hourly conversion rate from openexchangerates.org",
-          bpi: {
-            USD: {
-              code: "USD",
-              rate: "29,991.8360",
-              description: "United States Dollar",
-              rate_float: 29991.836,
-            },
-          },
-        })
-      );
-    }
-  ),
-  rest.get(
-    "https://legend.lnbits.com/withdraw/api/v1/lnurl/cb/D7paixQdqcsm3VJrstczsQ",
-    (req, res, ctx) => {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          status: "ERROR",
-          reason: "Link not working",
-        })
-      );
-    }
-  )
-);
 
 jest.spyOn(SettingsContext, "useSettings").mockReturnValue({
   settings: mockSettings,
@@ -129,21 +87,6 @@ jest.mock("~/common/lib/api", () => ({
   })),
   makeInvoice: jest.fn(() => ({})),
 }));
-
-beforeAll(() => {
-  // Enable the mocking in tests.
-  server.listen();
-});
-
-afterEach(() => {
-  // Reset any runtime handlers tests may use.
-  server.resetHandlers();
-});
-
-afterAll(() => {
-  // Clean up once the tests are done.
-  server.close();
-});
 
 describe("LNURLWithdraw", () => {
   afterEach(() => {
