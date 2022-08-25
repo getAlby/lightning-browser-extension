ae9754d2760a5fee1b8bac91c7329c6b589a8bb9
escapedcat <github@htmlcss.de>
2022-08-25 17:27:26 +0800

    test(e2e): basic receive & send to ln-address

--- tests/e2e/createWallets.spec.ts
+++ tests/e2e/createWallets.spec.ts
@@ -119,6 +119,79 @@ test.describe("Create or connect wallets", () => {
     await browser.close();
   });
 
+  test("create invoice", async () => {
+    const { page, browser, extensionId } = await loadExtension();
+    await page.waitForTimeout(1000);
+
+    const optionsPage = `chrome-extension://${extensionId}/options.html`;
+    await page.goto(optionsPage);
+    await page.waitForTimeout(1000);
+
+    const $optionsdocument = await getDocument(page);
+    const passwordField = await getByPlaceholderText(
+      $optionsdocument,
+      "Password"
+    );
+    await passwordField.type(user.password);
+
+    const unlockButton = await findByText($optionsdocument, "Unlock");
+    unlockButton.click();
+
+    // create invoice
+    await (await findByText($optionsdocument, "Receive")).click();
+
+    const amountField = await getByLabelText($optionsdocument, "Amount");
+    await amountField.type("888");
+
+    const descriptionField = await getByLabelText(
+      $optionsdocument,
+      "Description"
+    );
+    await descriptionField.type("It is a lucky number");
+
+    await (await getByText($optionsdocument, "Create Invoice")).click();
+    await page.waitForTimeout(2000);
+    // copy invoice
+    await (await findByText($optionsdocument, "Copy")).click();
+
+    await browser.close();
+  });
+
+  test("send to a LN-adddress", async () => {
+    const { page, browser, extensionId } = await loadExtension();
+    await page.waitForTimeout(1000);
+
+    const optionsPage = `chrome-extension://${extensionId}/options.html`;
+    await page.goto(optionsPage);
+    await page.waitForTimeout(1000);
+
+    const $optionsdocument = await getDocument(page);
+    const passwordField = await getByPlaceholderText(
+      $optionsdocument,
+      "Password"
+    );
+    await passwordField.type(user.password);
+
+    const unlockButton = await findByText($optionsdocument, "Unlock");
+    unlockButton.click();
+
+    // go to send
+    await page.waitForTimeout(1000);
+    await (await getByText($optionsdocument, "Send")).click();
+    const invoiceInput = await findByText(
+      $optionsdocument,
+      "Invoice, Lightning Address or LNURL"
+    );
+    await invoiceInput.type("bumi@getalby.com");
+    await (await getByText($optionsdocument, "Continue")).click();
+    await page.waitForTimeout(2000);
+
+    await findByText($optionsdocument, "bumi@getalby.com");
+    await findByText($optionsdocument, "Sats for bumi");
+
+    await browser.close();
+  });
+
   test("successfully connects to LNBits wallet", async () => {
     const { browser, page, $document } = await commonCreateWalletUserCreate();
 
