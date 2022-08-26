f088e880631c074f7c9ce07965cb982e44ad3d72
escapedcat <github@htmlcss.de>
2022-08-26 17:58:53 +0800

    feat(successmessage): i18n #1311

--- src/app/components/SuccessMessage/index.tsx
+++ src/app/components/SuccessMessage/index.tsx
@@ -1,4 +1,5 @@
 import { MouseEventHandler } from "react";
+import { useTranslation } from "react-i18next";
 
 type Props = {
   message: string;
@@ -6,15 +7,20 @@ type Props = {
 };
 
 function SuccessMessage({ message, onClose }: Props) {
+  const { t: tCommon } = useTranslation("common");
+
   return (
     <>
       <dl className="shadow bg-white dark:bg-surface-02dp pt-4 px-4 rounded-lg mb-6 overflow-hidden">
-        <dt className="text-sm font-semibold text-gray-500">Success</dt>
+        <dt className="text-sm font-semibold text-gray-500">
+          {tCommon("success")}
+        </dt>
         <dd className="text-sm mb-4 dark:text-white break-all">{message}</dd>
       </dl>
+
       <div className="text-center">
         <button className="underline text-sm text-gray-500" onClick={onClose}>
-          Close
+          {tCommon("actions.close")}
         </button>
       </div>
     </>
