9303cb851052b972638c8d9739d079035632b959
escapedcat <github@htmlcss.de>
2022-08-30 12:38:13 +0800

    revert(send): re-add invoice decode check #770

--- src/app/screens/Send/index.tsx
+++ src/app/screens/Send/index.tsx
@@ -9,6 +9,7 @@ import Header from "@components/Header";
 import IconButton from "@components/IconButton";
 import QrcodeScanner from "@components/QrcodeScanner";
 import TextField from "@components/form/TextField";
+import lightningPayReq from "bolt11";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
@@ -87,6 +88,7 @@ function Send() {
       } else if (isPubKey(invoice)) {
         navigate(`/keysend?destination=${invoice}`);
       } else {
+        lightningPayReq.decode(invoice); // throws if invalid.
         navigate("/confirmPayment", {
           state: {
             args: {
