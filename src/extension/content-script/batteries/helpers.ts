274d288770aeb76c7286598aa2625afeca55986b
im-adithya <imadithyavardhan@gmail.com>
2022-09-01 19:58:31 +0530

    feat: support alby links in batteries

--- src/extension/content-script/batteries/helpers.ts
+++ src/extension/content-script/batteries/helpers.ts
@@ -9,6 +9,10 @@ export const findLightningAddressInText = (text: string): string | null => {
   // variation selector-16 character: https://regex101.com/r/Bf2GpN/1
   const match = text.match(/((⚡|⚡️):?|lightning:|lnurl:)\s?(\S+@[\w-.]+)/i);
   if (match) return match[3];
+  const matchAlbyLink = text.match(
+    /http(s)?:\/\/(www[.])?getalby\.com\/p\/(\w+)/
+  );
+  if (matchAlbyLink) return matchAlbyLink[3] + "@getalby.com";
   return null;
 };
 
