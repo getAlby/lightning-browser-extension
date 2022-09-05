f384106e6dd34201d2e026c39b5b03b3202fa358
Adithya Vardhan <imadithyavardhan@gmail.com>
2022-09-06 02:40:33 +0530

    SoundCloud Battery (#1364)
    * fix: add fallback lightning emoji to regex
    
    * feat: add soundcloud battery
    
    * chore: add comment reg emoji in regex
    
    * feat: support alby links in batteries
    
    * feat: use href of profile links

--- src/extension/content-script/batteries/helpers.ts
+++ src/extension/content-script/batteries/helpers.ts
@@ -5,8 +5,14 @@ import type { Battery } from "~/types";
 import { ExtensionIcon } from "../../background-script/actions/setup/setIcon";
 
 export const findLightningAddressInText = (text: string): string | null => {
-  const match = text.match(/(⚡:?|lightning:|lnurl:)\s?(\S+@[\w-.]+)/i);
-  if (match) return match[2];
+  // The second lightning emoji is succeeded by an invisible
+  // variation selector-16 character: https://regex101.com/r/Bf2GpN/1
+  const match = text.match(/((⚡|⚡️):?|lightning:|lnurl:)\s?(\S+@[\w-.]+)/i);
+  if (match) return match[3];
+  const matchAlbyLink = text.match(
+    /http(s)?:\/\/(www[.])?getalby\.com\/p\/(\w+)/
+  );
+  if (matchAlbyLink) return matchAlbyLink[3] + "@getalby.com";
   return null;
 };
 
