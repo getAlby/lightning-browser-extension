615b93b03f08a2d9a923536ae9dde929cf3c94a5
escapedcat <github@htmlcss.de>
2022-09-12 15:48:49 +0800

    test(e2e): lnurl-auth
    - use unlock for helper for send to ln-address

--- tests/e2e/002-walletFeatures.spec.ts
+++ tests/e2e/002-walletFeatures.spec.ts
@@ -28,7 +28,10 @@ const unlockExtension = async ({ page, extensionId }) => {
 test.describe("Wallet features", () => {
   test("opens publishers screen", async () => {
     const { page, browser, extensionId } = await loadExtension();
-    const $optionsdocument = await unlockExtension({ page, extensionId });
+    const $optionsdocument = await unlockExtension({
+      page,
+      extensionId,
+    });
 
     await findByText($optionsdocument, "Your ⚡️ Websites");
     await findByText($optionsdocument, "Other ⚡️ Websites");
@@ -38,7 +41,10 @@ test.describe("Wallet features", () => {
 
   test("create invoice", async () => {
     const { page, browser, extensionId } = await loadExtension();
-    const $optionsdocument = await unlockExtension({ page, extensionId });
+    const $optionsdocument = await unlockExtension({
+      page,
+      extensionId,
+    });
 
     // create invoice
     await (await findByText($optionsdocument, "Receive")).click();
@@ -62,25 +68,13 @@ test.describe("Wallet features", () => {
 
   test("send to a LN-address", async () => {
     const { page, browser, extensionId } = await loadExtension();
-    await new Promise((r) => setTimeout(r, 1000));
-
-    const optionsPage = `chrome-extension://${extensionId}/options.html`;
-    await page.goto(optionsPage);
-    await new Promise((r) => setTimeout(r, 1000));
-
-    const $optionsdocument = await getDocument(page);
-    const passwordField = await getByPlaceholderText(
-      $optionsdocument,
-      "Your unlock password"
-    );
-    await passwordField.type("unlock-password");
-
-    const unlockButton = await findByText($optionsdocument, "Unlock");
-    unlockButton.click();
-    await new Promise((r) => setTimeout(r, 1000));
+    const $optionsdocument = await unlockExtension({
+      page,
+      extensionId,
+    });
 
     // go to send
-    await (await getByText($optionsdocument, "Send")).click();
+    await (await findByText($optionsdocument, "Send")).click();
     const invoiceInput = await findByText(
       $optionsdocument,
       "Invoice, Lightning Address or LNURL"
@@ -94,4 +88,31 @@ test.describe("Wallet features", () => {
 
     await browser.close();
   });
+
+  // test("auth via LNURL", async () => {
+  //   const { page, browser, extensionId } = await loadExtension();
+  //   const $optionsdocument = await unlockExtension({
+  //     page,
+  //     extensionId,
+  //   });
+
+  //   // go to send
+  //   await (await findByText($optionsdocument, "Send")).click();
+  //   const invoiceInput = await findByText(
+  //     $optionsdocument,
+  //     "Invoice, Lightning Address or LNURL"
+  //   );
+  //   await invoiceInput.type(
+  //     "lightning:LNURL1DP68GURN8GHJ7MRWW4EXCTNXD9SHG6NPVCHXXMMD9AKXUATJDSKKCMM8D9HR7ARPVU7KCMM8D9HZV6E384NXVVEKXSEXYWRYX3SN2EPCX5EKYV35VVEK2VNPXESKZVN9XVCX2VNYX5CXYCFHXUCRJV3JX5UNJV3J8Q6N2WTYVS6NGVE48Q6X2VP3YCHUNC"
+  //   );
+  //   await (await getByText($optionsdocument, "Continue")).click();
+
+  //   await findByText(
+  //     $optionsdocument,
+  //     "Do you want to login to lnurl.fiatjaf.com?"
+  //   );
+  //   await findByText($optionsdocument, "Login");
+
+  //   await browser.close();
+  // });
 });
