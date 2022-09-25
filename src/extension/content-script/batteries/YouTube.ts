374d486b0553c840c8e876e6dd37de1e1b78b777
René Aaron <rene@twentyuno.net>
2022-09-25 11:42:53 +0200

    feat(battery): cleanup

--- src/extension/content-script/batteries/YouTube.ts
+++ src/extension/content-script/batteries/YouTube.ts
@@ -1,8 +1,8 @@
+import axios from "axios";
+
 import getOriginData from "../originData";
 import { findLightningAddressInText, setLightningData } from "./helpers";
 import updateBoostButton from "./inpage-components/boost-button";
-import axios from "axios";
-import LNURLAuth from "~/app/screens/LNURLAuth";
 
 declare global {
   interface Window {
@@ -13,7 +13,6 @@ declare global {
 const urlMatcher = /^https:\/\/www\.youtube.com\/.*/;
 const channelUrlMatcher = /^https:\/\/www\.youtube.com\/(channel|c)\/([^/]+).*/;
 const battery = async (): Promise<void> => {
-
   if (!window.LBE_MUTATION_OBSERVER) {
     window.LBE_MUTATION_OBSERVER = new MutationObserver(
       debounce(youtubeDOMChanged, 500)
@@ -24,7 +23,7 @@ const battery = async (): Promise<void> => {
     childList: true,
     subtree: true,
   });
-  
+
   // On slow connections the observer is added after the DOM is fully loaded.
   // Therefore the callback twitterDOMChanged needs to also be called manually.
   youtubeDOMChanged();
@@ -37,62 +36,12 @@ const YouTubeVideo = {
 
 export default YouTubeVideo;
 
-async function getVideoInfo() : Promise<PublisherInfo | null> {
-  if (!document.location.toString().match(/^https:\/\/www\.youtube.com\/watch.*/)) {
-    return null;
-  }
-
-  let text = "";
-  document
-    .querySelectorAll(
-      "ytd-watch-flexy:not([hidden]) #columns #primary #primary-inner #meta-contents #description .content"
-    )
-    .forEach((e) => {
-      text += ` ${e.textContent}`;
-    });
-  const channelLink = document.querySelector<HTMLAnchorElement>(
-    "#columns #primary #primary-inner #meta-contents .ytd-channel-name a"
-  );
-  if (!text || !channelLink) {
-    return null;
-  }
-  let match;
-  let lnurl: string = "";
-  // check for an lnurl
-  if ((match = text.match(/(lnurlp:)(\S+)/i))) {
-    lnurl = match[2];
-  }
-  // if there is no lnurl we check for a zap emoji with a lightning address
-  // we check for the @-sign to try to limit the possibility to match some invalid text (e.g. random emoji usage)
-  else if ((match = findLightningAddressInText(text))) {
-    lnurl = match;
-  } else {
-    // load the about page to check for a lightning address
-    const match = channelLink.href.match(
-      /^https:\/\/www\.youtube.com\/(channel|c)\/([^/]+).*/
-    );
-    if (match) {
-      const lnurlResult = await findLnurlFromYouTubeAboutPage(match[1], match[2]);
-      if(lnurlResult) {
-        lnurl = lnurlResult;
-      }
-    }
-  }
-
-  const image =
-  document.querySelector<HTMLImageElement>(
-    "#columns #primary #primary-inner #meta-contents img"
-  )?.src || "";
-
-  return { lnurl, name: channelLink.textContent ?? undefined, image };
-}
-
 async function youtubeDOMChanged() {
-  let info = await getChannelInfo();
-  if(!info) {
-    info = await getVideoInfo();
+  let info = await getVideoInfo();
+  if (!info) {
+    info = await getChannelInfo();
   }
-  
+
   updateBoostButton(info);
 
   if (!info?.lnurl) {
@@ -113,30 +62,83 @@ async function youtubeDOMChanged() {
 
 export const debounce = <F extends (...args: Parameters<F>) => ReturnType<F>>(
   func: F,
-  waitFor: number,
+  waitFor: number
 ) => {
-  let timeout: NodeJS.Timeout
+  let timeout: NodeJS.Timeout;
 
   const debounced = (...args: Parameters<F>) => {
-    clearTimeout(timeout)
-    timeout = setTimeout(() => func(...args), waitFor)
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), waitFor);
+  };
+
+  return debounced;
+};
+
+async function getVideoInfo(): Promise<PublisherInfo | null> {
+  if (
+    !document.location.toString().match(/^https:\/\/www\.youtube.com\/watch.*/)
+  ) {
+    return null;
   }
 
-  return debounced
+  let text = "";
+  document
+    .querySelectorAll(
+      "ytd-watch-flexy:not([hidden]) #columns #primary #primary-inner #meta-contents #description .content"
+    )
+    .forEach((e) => {
+      text += ` ${e.textContent}`;
+    });
+  const channelLink = document.querySelector<HTMLAnchorElement>(
+    "#columns #primary #primary-inner #meta-contents .ytd-channel-name a"
+  );
+  if (!text || !channelLink) {
+    return null;
+  }
+  let match;
+  let lnurl = "";
+  // check for an lnurl
+  if ((match = text.match(/(lnurlp:)(\S+)/i))) {
+    lnurl = match[2];
+  }
+  // if there is no lnurl we check for a zap emoji with a lightning address
+  // we check for the @-sign to try to limit the possibility to match some invalid text (e.g. random emoji usage)
+  else if ((match = findLightningAddressInText(text))) {
+    lnurl = match;
+  } else {
+    // load the about page to check for a lightning address
+    const match = channelLink.href.match(
+      /^https:\/\/www\.youtube.com\/(channel|c)\/([^/]+).*/
+    );
+    if (match) {
+      const lnurlResult = await findLnurlFromYouTubeAboutPage(
+        match[1],
+        match[2]
+      );
+      if (lnurlResult) {
+        lnurl = lnurlResult;
+      }
+    }
+  }
+
+  const image =
+    document.querySelector<HTMLImageElement>(
+      "#columns #primary #primary-inner #meta-contents img"
+    )?.src ||
+    document.querySelector<HTMLImageElement>(
+      "#columns #primary #primary-inner #owner #avatar img" // support maybe new UI being rolled out 2022/09
+    )?.src ||
+    "";
+
+  return { lnurl, name: channelLink.textContent ?? undefined, image };
 }
 
-class PublisherInfo {
-  lnurl?: string;
-  image?: string;
-  name?: string;
-}
-
-async function getChannelInfo() : Promise<PublisherInfo | null> {
+async function getChannelInfo(): Promise<PublisherInfo | null> {
   const match = document.location.toString().match(channelUrlMatcher);
   if (!match) {
     return null;
   }
-  
+
   const name =
     document.querySelector(
       "#inner-header-container yt-formatted-string.ytd-channel-name"
@@ -164,8 +166,8 @@ async function getChannelInfo() : Promise<PublisherInfo | null> {
   return {
     lnurl,
     name,
-    image
-  }
+    image,
+  };
 }
 
 const findLnurlFromHeaderLink = (
@@ -210,165 +212,8 @@ const findLnurlFromYouTubeAboutPage = (
     });
 };
 
-/* 
-let streamInterval: FixMe;
-
-const startPaymentStream = async ({
-  lnurl,
-  amount,
-  comment,
-}: {
-  lnurl: string;
-  amount: number;
-  comment?: string;
-}) => {
-  console.log("▶️", "startPaymentStream()");
-
-  // Make sure there is only one event loop
-  if(streamInterval) {
-    clearInterval(streamInterval);
-  }
-
-  overlay.innerHTML = `⚡ <b>${amount}</b> sats / min`;
-
-  // Trigger the first payment
-  var success = await albySendPayment({ lnurl, amount, comment });
-  if(!success) {
-    overlay.innerHTML = "🐝 Stream sats with Alby";
-    amount = 0;
-    return;
-  }
-
-  // Then setup the loop
-  streamInterval = setInterval(async () => {
-    var result = await albySendPayment({ lnurl, amount, comment });
-    if(!result)
-    {
-      stopPaymentStream();
-    }
-  }, 10 * 1000);
-};
-
-const stopPaymentStream = () => {
-  console.log("⏹️", "stopPaymentStream()");
-
-  overlay.innerHTML = "🐝 Stream sats with Alby";
-  amount = 0;
-
-  clearInterval(streamInterval);
-};
-
-const pausePaymentStream = () => {
-  console.log("⏸️", "pausePaymentStream()");
-
-  if(amount) {
-    overlay.innerHTML = "⏸️ Paused";
-  }
-  
-  clearInterval(streamInterval);
-};
-
-const albySendPayment = async ({
-  lnurl,
-  amount,
-  comment,
-}: {
-  lnurl: string;
-  amount: number;
-  comment?: string;
-}): Promise<boolean> => {
-  const webln = new WebLNProvider();
-  const alby = new AlbyProvider();
-  
-  try {
-    await webln.enable();
-    console.log("enable");
-    var response = await alby.lnurl("reneaaron@getalby.com", 100, "🚀") as any;
-    const result = await webln.sendPayment(response.data.pr);
-    console.log({ result });
-    // confetti or something similar
-  } catch (error) {
-    console.info("cancelled", error);
-    return false;
-  }
-
-  return true;
-};
-
-  // const videoElement = document.querySelector<HTMLVideoElement>(
-  //   "#movie_player .html5-video-container video.html5-main-video"
-  // );
-
-  // if(videoElement) {
-  //   setupVideo(videoElement, lnurl);
-  // }
-  
-let overlay: HTMLDivElement;
-let amount: number;
-function setupVideo(videoElement: HTMLVideoElement, lnurl: string) {
-
-  overlay = document.createElement("div");
-  overlay.innerHTML = "🐝 Stream sats with Alby";
-  overlay.classList.add("alby-video-overlay");
-  overlay.onclick = async function (e) {
-
-    e.preventDefault();
-    e.stopPropagation();
-
-    var sats = prompt("How many sats per minute?", "21");
-    if (sats) {
-      var satsValue = parseInt(sats);
-      if (satsValue) {
-        amount = satsValue;
-        await startPaymentStream({ lnurl, amount: satsValue, comment: "🐝 Streaming sats" })
-        return;
-      }
-    }
-
-    stopPaymentStream();
-  }
-
-  overlay.addEventListener('dblclick', function (e) {
-    e.preventDefault();
-    e.stopPropagation();
-  });
-
-  // Show and hide the overlay
-  setTimeout(function () {
-    overlay.style.opacity = "100";
-
-    setTimeout(function () {
-      overlay.style.opacity = "0";
-    }, 10000);
-
-  }, 1000);
-
-  var container = document.querySelector(".html5-video-container");
-  if (container) {
-    container.appendChild(overlay);
-    container.addEventListener('mouseenter', () => {
-      overlay.style.opacity = "100";
-    });
-    container.addEventListener('mouseleave', () => {
-      overlay.style.opacity = "0";
-    });
-  }
-
-
-  videoElement.onplay = async function (e) {
-    console.log("▶️ video.onplay()");
-    if (amount) {
-      await startPaymentStream({
-        lnurl,
-        amount: amount,
-        comment: "🐝 Streaming sats",
-      });
-    }
-  };
-
-  videoElement.onpause = function (e) {
-    console.log("⏸️ video.onpause()");
-    pausePaymentStream();
-  };
+class PublisherInfo {
+  lnurl?: string;
+  image?: string;
+  name?: string;
 }
-*/
--- src/extension/inpage-components/src/BoostButton.tsx
+++ src/extension/inpage-components/src/BoostButton.tsx
@@ -1,41 +1,52 @@
-import React from 'react';
-import styles from './BoostButton.module.css';
-import CountUp from 'react-countup';
-import {ReactComponent as LoadingIndicator} from './loading.svg';
-import {ReactComponent as AlbyLogo} from './logo.svg';
-export default class BoostButton extends React.Component<BoostButtonProps, BoostButtonState> {
+import React from "react";
+import CountUp from "react-countup";
 
+import styles from "./BoostButton.module.css";
+import { ReactComponent as LoadingIndicator } from "./loading.svg";
+import { ReactComponent as AlbyLogo } from "./logo.svg";
+
+enum Step {
+  Start = 0,
+  Pay = 1,
+  Error = 2,
+  Thankyou = 3,
+}
+
+export default class BoostButton extends React.Component<
+  BoostButtonProps,
+  BoostButtonState
+> {
   state: BoostButtonState = {
     loading: false,
     amount: 0,
     pressed: false,
-    step: 'start',
-    errorMessage: '',
-  }
+    step: Step.Start,
+    errorMessage: "",
+  };
 
   timeout: NodeJS.Timer | null = null;
   pressedInterval: NodeJS.Timer | null = null;
-  
+
   async mouseDown() {
-    if(this.state.step != 'start') {
+    if (this.state.step !== Step.Start) {
       return;
     }
 
-    if(this.timeout) {
+    if (this.timeout) {
       clearTimeout(this.timeout);
     }
-    
+
     this.setState({ pressed: true });
 
     let pressedCount = 0;
     this.pressedInterval = setInterval(() => {
       pressedCount++;
-      this.setState({ amount: this.state.amount + pressedCount});
+      this.setState({ amount: this.state.amount + pressedCount });
     }, 50);
   }
 
   async mouseUp() {
-    if(this.state.step != 'start') {
+    if (this.state.step !== Step.Start) {
       return;
     }
 
@@ -43,94 +54,110 @@ export default class BoostButton extends React.Component<BoostButtonProps, Boost
 
     this.pressedInterval && clearInterval(this.pressedInterval);
     this.timeout && clearTimeout(this.timeout);
-    
+
     this.timeout = setTimeout(async () => {
       await this.pay();
     }, 2000);
-    
   }
 
   async mouseLeave() {
     this.setState({ pressed: false });
-    
+
     this.pressedInterval && clearInterval(this.pressedInterval);
-    
-    if(this.state.pressed) {
+
+    if (this.state.pressed) {
       this.timeout = setTimeout(async () => {
         await this.pay();
       }, 2000);
-    }    
+    }
   }
 
   async pay() {
     this.setState({ loading: true });
 
     try {
-      var result = await window.alby.lnurl(this.props.lnurl, this.state.amount, "🚀") as any;
+      var result = (await window.alby.lnurl(
+        this.props.lnurl,
+        this.state.amount,
+        "🚀"
+      )) as any;
       await window.webln.enable();
       await window.webln.sendPayment(result.pr);
-      
+
       // TODO: Add preimage validation
 
-      this.setState({ step: 'thankyou' });
-    }
-    catch(e) {
+      this.setState({ step: Step.Thankyou });
+    } catch (e) {
       console.error(e);
-      this.setState({ step: 'error', errorMessage: "Payment failed" });
+      this.setState({ step: Step.Error, errorMessage: "Payment failed" });
     }
 
-    setTimeout(() => { this.reset() }, 5000);
+    setTimeout(() => {
+      this.reset();
+    }, 5000);
   }
 
   reset() {
-    this.setState({ step: 'start', amount: 0, loading: false })
+    this.setState({ step: Step.Start, amount: 0, loading: false });
   }
-  
+
   render() {
     const localStyles = {
-      transform: this.state.pressed ? 'scale(0.9)' : "none",
+      transform: this.state.pressed ? "scale(0.9)" : "none",
     } as any;
 
-    if(this.state.step == 'thankyou' && this.props.image) {
+    if (this.state.step === Step.Thankyou && this.props.image) {
       localStyles.backgroundImage = `url('${this.props.image}')`;
     }
 
-    if(this.state.step == 'error') {
+    if (this.state.step === Step.Error) {
       localStyles.backgroundImage = `linear-gradient(180deg, #ff6b6b, #f34646)`;
     }
 
-    return <div className={styles.BoostButton} 
-              style={localStyles}
-              onMouseDown={() => this.mouseDown()} 
-              onMouseUp={() => this.mouseUp()}
-              onMouseLeave={() => this.mouseLeave()}>
-              {this.state.step == 'start' && this.state.loading && <div>
-                <LoadingIndicator/>
-              </div>}
-              {this.state.step == 'error' && <div>
-                ❌<br/>{this.state.errorMessage}
-              </div>
-              }
-              {this.state.step !='pay' && !this.state.loading && 
-                <div>
-                  {this.state.amount === 0 && 
-                    <AlbyLogo/>
-                  }
-                  {this.state.amount > 0 && 
-                    <div>
-                      <div className={styles.amount}>
-                        <CountUp preserveValue={true} duration={0.2} end={this.state.amount} />
-                      </div>                    
-                      <div className={styles.sats}>sats</div>
-                    </div>
-                  }
+    return (
+      <div
+        className={styles.BoostButton}
+        style={localStyles}
+        onMouseDown={() => this.mouseDown()}
+        onMouseUp={() => this.mouseUp()}
+        onMouseLeave={() => this.mouseLeave()}
+      >
+        {this.state.step === Step.Start && this.state.loading && (
+          <div>
+            <LoadingIndicator />
+          </div>
+        )}
+        {this.state.step === Step.Error && (
+          <div>
+            ❌<br />
+            {this.state.errorMessage}
+          </div>
+        )}
+        {this.state.step === Step.Start && !this.state.loading && (
+          <div>
+            {this.state.amount === 0 && <AlbyLogo />}
+            {this.state.amount > 0 && (
+              <div>
+                <div className={styles.amount}>
+                  <CountUp
+                    preserveValue={true}
+                    duration={0.2}
+                    end={this.state.amount}
+                  />
                 </div>
-              }
-              {this.state.step == 'thankyou' && <div>
-                {!this.props.image && <AlbyLogo/>}
-                  <span className={styles.thanks}>Thanks! 🙌</span>
-              </div>}               
-           </div>;
+                <div className={styles.sats}>sats</div>
+              </div>
+            )}
+          </div>
+        )}
+        {this.state.step === Step.Thankyou && (
+          <div>
+            {!this.props.image && <AlbyLogo />}
+            <span className={styles.thanks}>Thanks! 🙌</span>
+          </div>
+        )}
+      </div>
+    );
   }
 }
 
@@ -142,7 +169,7 @@ class BoostButtonProps {
 class BoostButtonState {
   loading: boolean = false;
   amount: number = 0;
-  pressed: boolean = false;  
-  step: string = "";
+  pressed: boolean = false;
+  step: Step = Step.Start;
   errorMessage: string = "";
 }
