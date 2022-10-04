f8e57b85ba4ccbd59764afdfa1e758024d1f7043
im-adithya <imadithyavardhan@gmail.com>
2022-10-04 13:02:12 +0530

    feat: implement webbtc

--- src/extension/inpage-script/webln.js
+++ src/extension/inpage-script/webln.js
@@ -1,5 +1,7 @@
 import WebLNProvider from "../ln/webln";
+import WebBTCProvider from "../webbtc";
 
 if (document) {
   window.webln = new WebLNProvider();
+  window.webbtc = new WebBTCProvider();
 }
