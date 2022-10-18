dca334cdc26134d040f1bea36a52b26fbf571ddb
René Aaron <rene@twentyuno.net>
2022-10-18 20:49:52 +0200

    fix: change for loop

--- src/extension/background-script/actions/nostr/helpers.ts
+++ src/extension/background-script/actions/nostr/helpers.ts
@@ -9,8 +9,7 @@ export function validateEvent(event: Event) {
   if (typeof event.created_at !== "number") return false;
 
   if (!Array.isArray(event.tags)) return false;
-  for (let i = 0; i < event.tags.length; i++) {
-    const tag = event.tags[i];
+  for (const tag of event.tags) {
     if (!Array.isArray(tag)) return false;
     for (let j = 0; j < tag.length; j++) {
       if (typeof tag[j] === "object") return false;
