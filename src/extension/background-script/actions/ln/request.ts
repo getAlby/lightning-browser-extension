1c1db9445e57b6794a873323308bfd52146b4cd8
Michael Bumann <hello@michaelbumann.com>
2022-11-04 12:32:01 +0100

    feat: add permission check to request action

--- src/extension/background-script/actions/ln/request.ts
+++ src/extension/background-script/actions/ln/request.ts
@@ -1,5 +1,6 @@
-import { Message } from "~/types";
+import { Message, OriginData } from "~/types";
 
+import db from "../../db";
 import state from "../../state";
 
 const request = async (message: Message) => {
@@ -11,10 +12,42 @@ const request = async (message: Message) => {
         `${message.args.method} is not supported by your account`
       );
     }
-    response = await connector.requestMethod(
-      message.args.method as string,
-      message.args.params
-    );
+
+    const { method, params } = message.args;
+    const host = (message.origin as OriginData).host;
+    // TODO: validate host, method, params
+
+    const allowance = await db.allowances
+      .where("host")
+      .equalsIgnoreCase(host as string)
+      .first();
+
+    if (!allowance?.id) {
+      return { error: "Host not enabled" };
+    }
+
+    const permission = await db.permissions
+      .where("host")
+      .equalsIgnoreCase(host as string) // or rather get by allowanceID?
+      .and((p) => p.method === method)
+      .first();
+
+    if (permission && permission.enabled) {
+      response = await connector.requestMethod(method as string, params);
+    } else {
+      // prompt the user
+      console.error("TODO: missing prompt");
+      response = await connector.requestMethod(method as string, params);
+      await db.permissions.add({
+        createdAt: Date.now().toString(),
+        allowanceId: allowance.id,
+        host: host as string,
+        method: method as string,
+        enabled: true,
+        blocked: false,
+      });
+      await db.saveToStorage();
+    }
   } catch (e) {
     console.error(e);
     response = {
