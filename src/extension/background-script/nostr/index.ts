b222477a27e8c9a630abe6b3c0d86ae9540bb5c4
René Aaron <rene@twentyuno.net>
2022-11-27 15:31:26 +0100

    fix: ts fixes

--- src/extension/background-script/nostr/index.ts
+++ src/extension/background-script/nostr/index.ts
@@ -1,15 +1,16 @@
 import * as secp256k1 from "@noble/secp256k1";
-import { decryptData, encryptData } from "~/common/lib/crypto";
-import { Event } from "~/extension/ln/nostr/types";
-import { signEvent } from "../actions/nostr/helpers";
-import state from "../state";
-import aes from 'browserify-cipher';
-import {Buffer} from 'buffer'
-import { AES } from 'crypto-js';
+import { Buffer } from "buffer";
+import { AES } from "crypto-js";
+import * as CryptoJS from "crypto-js";
+import Base64 from "crypto-js/enc-base64";
 import Hex from "crypto-js/enc-hex";
 import Utf8 from "crypto-js/enc-utf8";
-import Base64 from "crypto-js/enc-base64";
-import * as CryptoJS from 'crypto-js';
+import { decryptData, encryptData } from "~/common/lib/crypto";
+import { Event } from "~/extension/ln/nostr/types";
+
+import { signEvent } from "../actions/nostr/helpers";
+import state from "../state";
+
 class Nostr {
   getPrivateKey() {
     const password = state.getState().password as string;
@@ -43,27 +44,31 @@ class Nostr {
   }
 
   encrypt(pubkey: string, text: string) {
-    let key = secp256k1.getSharedSecret(this.getPrivateKey(), '02' + pubkey);
+    const key = secp256k1.getSharedSecret(this.getPrivateKey(), "02" + pubkey);
     const normalizedKey = Buffer.from(key.slice(1, 33));
     const hexNormalizedKey = secp256k1.utils.bytesToHex(normalizedKey);
-    var hexKey = Hex.parse(hexNormalizedKey);
+    const hexKey = Hex.parse(hexNormalizedKey);
 
-    const encrypted = AES.encrypt(text, hexKey, { iv: CryptoJS.lib.WordArray.random(16) });
-    
-    return `${(encrypted.toString())}?iv=${(encrypted.iv.toString(CryptoJS.enc.Base64))}`;
+    const encrypted = AES.encrypt(text, hexKey, {
+      iv: CryptoJS.lib.WordArray.random(16),
+    });
+
+    return `${encrypted.toString()}?iv=${encrypted.iv.toString(
+      CryptoJS.enc.Base64
+    )}`;
   }
-  
+
   decrypt(pubkey: string, ciphertext: string) {
-    let [cip, iv] = ciphertext.split('?iv=')
-    let key = secp256k1.getSharedSecret(this.getPrivateKey(), '02' + pubkey);
+    const [cip, iv] = ciphertext.split("?iv=");
+    const key = secp256k1.getSharedSecret(this.getPrivateKey(), "02" + pubkey);
     const normalizedKey = Buffer.from(key.slice(1, 33));
     const hexNormalizedKey = secp256k1.utils.bytesToHex(normalizedKey);
-    var hexKey = Hex.parse(hexNormalizedKey);
-    
+    const hexKey = Hex.parse(hexNormalizedKey);
+
     const decrypted = AES.decrypt(cip, hexKey, {
-      iv: Base64.parse(iv)
+      iv: Base64.parse(iv),
     });
-  
+
     return Utf8.stringify(decrypted);
   }
 }
