4529d15385ae0d5a48f9b0ecba3659bf837ea17a
Jan Kögel <i.jan.koegel@gmail.com>
2022-12-07 22:19:33 +0100

    refactor: getOS: don't use deprecated appVersion.
    From MDN:
    https://developer.mozilla.org/en-US/docs/Web/API/Navigator/appVersion
    
    > Deprecated: This feature is no longer recommended. Though some browsers might still support it, it may have already been removed from the relevant web standards, may be in the process of being dropped, or may only be kept for compatibility purposes. ... Be aware that this feature may cease to work at any time.
    
    There's no such warning for userAgent and here's a source on how to use
    it instead:
    https://www.educative.io/answers/how-to-detect-the-operating-system-of-the-client-machine-using-js

--- src/app/components/CompanionDownloadInfo.tsx
+++ src/app/components/CompanionDownloadInfo.tsx
@@ -13,10 +13,11 @@ function CompanionDownloadInfo({ hasTorCallback }: Props) {
   });
 
   function getOS() {
-    if (navigator.appVersion.indexOf("Win") != -1) return "Windows";
-    if (navigator.appVersion.indexOf("Mac") != -1) return "MacOS";
-    if (navigator.appVersion.indexOf("X11") != -1) return "UNIX";
-    if (navigator.appVersion.indexOf("Linux") != -1) return "Linux";
+    const userAgent = navigator.userAgent;
+    if (userAgent.indexOf("Win") !== -1) return "Windows";
+    if (userAgent.indexOf("Mac") !== -1) return "MacOS";
+    if (userAgent.indexOf("X11") !== -1) return "UNIX";
+    if (userAgent.indexOf("Linux") !== -1) return "Linux"
   }
 
   // TODO: check if the companion app is already installed
