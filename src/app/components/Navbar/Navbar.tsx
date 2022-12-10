d7c1c2c1f08236433bf71a839b126be5ef72580c
Jan Kögel <i.jan.koegel@gmail.com>
2022-12-10 21:53:46 +0100

    refactor: Navbar: constrain max width to same width as the content.
    Reason: on desktop the navigation items in the top left and right
    corners were easy to miss. With this change, everything is closer
    together and easier to notice.

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -7,17 +7,19 @@ type Props = {
 
 export default function Navbar({ children }: Props) {
   return (
-    <div className="px-4 py-2 bg-white flex justify-between items-center border-b border-gray-200 dark:bg-surface-01dp dark:border-white/10">
-      <div className="flex w-8/12 md:w-4/12 lg:w-3/12">
-        <AccountMenu />
-      </div>
-      {children && (
-        <div>
-          <nav className="flex space-x-8">{children}</nav>
+    <div className="px-4 py-2 bg-white border-b border-gray-200 dark:bg-surface-01dp dark:border-white/10">
+      <div className="max-w-screen-lg flex justify-between mx-auto w-full px-3 items-center">
+        <div className="flex w-8/12 md:w-4/12 lg:w-3/12">
+          <AccountMenu />
         </div>
-      )}
-      <div className="md:w-4/12 lg:w-3/12 flex justify-end items-center">
-        <UserMenu />
+        {children && (
+          <div>
+            <nav className="flex space-x-8">{children}</nav>
+          </div>
+        )}
+        <div className="md:w-4/12 lg:w-3/12 flex justify-end items-center">
+          <UserMenu />
+        </div>
       </div>
     </div>
   );
