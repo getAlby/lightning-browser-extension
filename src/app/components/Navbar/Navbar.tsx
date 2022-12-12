86c295a2dc1c73b950b500e8ad2f593b2eb0f712
Jan Kögel <i.jan.koegel@gmail.com>
2022-12-12 11:40:36 +0100

    + adjust horizontal padding. + remove superfluous <div>.

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -8,15 +8,11 @@ type Props = {
 export default function Navbar({ children }: Props) {
   return (
     <div className="px-4 py-2 bg-white border-b border-gray-200 dark:bg-surface-01dp dark:border-white/10">
-      <div className="max-w-screen-lg flex justify-between mx-auto w-full px-3 items-center">
+      <div className="max-w-screen-lg flex justify-between mx-auto w-full px-4 items-center">
         <div className="flex w-8/12 md:w-4/12 lg:w-3/12">
           <AccountMenu />
         </div>
-        {children && (
-          <div>
-            <nav className="flex space-x-8">{children}</nav>
-          </div>
-        )}
+        {children && <nav className="flex space-x-8">{children}</nav>}
         <div className="md:w-4/12 lg:w-3/12 flex justify-end items-center">
           <UserMenu />
         </div>
