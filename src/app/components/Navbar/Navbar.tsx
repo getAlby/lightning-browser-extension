b0abd6ee1bbbff946b43d84195232f0a3ae6c5c7
Jan Kögel <i.jan.koegel@gmail.com>
2022-12-14 12:09:13 +0100

    remove superfluous wrapper divs.

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -9,15 +9,9 @@ export default function Navbar({ children }: Props) {
   return (
     <div className="px-4 py-2 bg-white border-b border-gray-200 dark:bg-surface-01dp dark:border-white/10">
       <div className="max-w-screen-lg flex justify-between mx-auto w-full lg:px-4 items-center">
-        <div className="flex">
-          <AccountMenu />
-        </div>
-
+        <AccountMenu />
         {children && <nav className="flex space-x-8 lg:-ml-2">{children}</nav>}
-
-        <div className="flex justify-end items-center">
-          <UserMenu />
-        </div>
+        <UserMenu />
       </div>
     </div>
   );
