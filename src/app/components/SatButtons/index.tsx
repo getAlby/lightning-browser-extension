9efa98d474491e0624fb0fbdb7ca900b50dcecdc
fanismichalakis <fanis.michalakis@gmail.com>
2022-12-19 18:55:48 +0100

    fix: better rounding and amount suggestion

--- src/app/components/SatButtons/index.tsx
+++ src/app/components/SatButtons/index.tsx
@@ -9,22 +9,48 @@ type Props = {
   max?: number;
 };
 
-function roundThousands(number: number) {
-  return number < 1000 ? Math.round(number) : Math.floor(number / 1000) * 1000;
-}
-
 function stringifyThousands(number: number) {
   return number < 1000
     ? `${Math.round(number)}`
-    : `${Math.floor(number / 1000)}K`;
+    : number < 1_000_000
+    ? `${Math.floor(number / 1000)}K`
+    : `${Math.floor(number / 1_000_000)}M`;
+}
+
+function tensRounder(number: number, index: number) {
+  const rounded = Math.round(number);
+  const tens = Math.round(Math.log(rounded) * Math.LOG10E) - 1;
+  const integralFunction = index === 0 ? Math.ceil : Math.trunc;
+  return integralFunction(rounded / Math.pow(10, tens)) * Math.pow(10, tens);
 }
 
 function amountsArray(min: number, max: number) {
-  const distance = Math.min(max - min, 10000);
-  const safeMin = max < 100 ? min : Math.max(min, 100);
-  return [safeMin, min + distance / 10, min + distance / 2, min + distance].map(
-    (amount) => roundThousands(amount)
-  );
+  let safeMin = Math.max(Math.ceil(min), 100);
+  const safeMax = Math.min(Math.floor(max), 100_000_000);
+  if (safeMin >= safeMax) {
+    safeMin = Math.ceil(min);
+  }
+  const distance = Math.round(
+    Math.log(Math.round(safeMax / safeMin)) * Math.LOG10E
+  ); // we define the distance as the number of tens of the ratio
+  if (distance >= 3) {
+    const factor = Math.pow(10, distance - 3);
+    return distance % 2 === 0
+      ? [safeMin, factor * 50 * safeMin, factor * 100 * safeMin, safeMax].map(
+          (x, index) => tensRounder(x, index)
+        )
+      : [safeMin, factor * 10 * safeMin, factor * 100 * safeMin, safeMax].map(
+          (x, index) => tensRounder(x, index)
+        );
+  } else if (distance === 2) {
+    return [safeMin, 5 * safeMin, 10 * safeMin, safeMax].map((x, index) =>
+      tensRounder(x, index)
+    );
+  } else {
+    return [safeMin, (safeMin + safeMax) / 2, safeMax].map((x, index) =>
+      tensRounder(x, index)
+    );
+  }
 }
 
 function SatButtons({ onClick, disabled, min = 100, max = 10000 }: Props) {
