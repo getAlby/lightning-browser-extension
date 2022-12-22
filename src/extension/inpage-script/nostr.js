5d45dc3fe663d069ce70c74cb00e62ff51f9bc02
Michael Bumann <hello@michaelbumann.com>
2022-12-22 10:36:06 +0100

    feat(nostr): publish a nostr ready event

--- src/extension/inpage-script/nostr.js
+++ src/extension/inpage-script/nostr.js
@@ -2,4 +2,6 @@ import NostrProvider from "../ln/nostr";
 
 if (document) {
   window.nostr = new NostrProvider();
+  const readyEvent = new Event("nostr:ready");
+  window.dispatchEvent(readyEvent);
 }
