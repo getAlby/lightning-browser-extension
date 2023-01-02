16a2d39679c714329d43e09fd0d7b2827ab93750
René Aaron <rene@twentyuno.net>
2023-01-02 15:32:59 +0100

    fix: copy

--- src/app/components/ConnectorPath/index.tsx
+++ src/app/components/ConnectorPath/index.tsx
@@ -8,7 +8,7 @@ type Props = {
 function ConnectorPath({ title, description, content, actions }: Props) {
   return (
     <div className="shadow-lg p-12 rounded-xl bg-white dark:bg-surface-02dp text-center">
-      <h1 className="text-3xl font-bold dark:text-white">{title}</h1>
+      <h1 className="text-2xl font-bold dark:text-white">{title}</h1>
       <p className="text-gray-500 mt-6 dark:text-neutral-400">{description}</p>
       <div className="h-56 flex flex-col justify-center items-center">
         {content}
