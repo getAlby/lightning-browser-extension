27c374a5dad55b0a90ed44b365cf8a463e1794a4
Adithya Vardhan <imadithyavardhan@gmail.com>
2023-01-04 21:40:59 +0530

    feat(boost): send ln data from linktree to button

--- src/extension/content-script/batteries/LinkTree.ts
+++ src/extension/content-script/batteries/LinkTree.ts
@@ -1,9 +1,11 @@
+import { Battery } from "~/types";
+
 import getOriginData from "../originData";
-import { findLightningAddressInText, setLightningData } from "./helpers";
+import { findLightningAddressInText } from "./helpers";
 
 const urlMatcher = /^https:\/\/linktr.ee\/([\S]+)$/;
 
-function battery(): void {
+function battery(): Battery | void {
   const linkElement = document.querySelector<HTMLAnchorElement>(
     "a[href*='getalby.com']"
   );
@@ -20,18 +22,16 @@ function battery(): void {
   const address = findLightningAddressInText(text ?? "");
   if (!address) return;
 
-  setLightningData([
-    {
-      method: "lnurl",
-      address,
-      ...getOriginData(),
-      description: description?.innerText ?? "",
-      name: document.querySelector<HTMLHeadingElement>("h1")?.innerText ?? "",
-      icon:
-        document.querySelector<HTMLImageElement>('[data-testid="ProfileImage"]')
-          ?.src ?? "",
-    },
-  ]);
+  return {
+    method: "lnurl",
+    address,
+    ...getOriginData(),
+    description: description?.innerText ?? "",
+    name: document.querySelector<HTMLHeadingElement>("h1")?.innerText ?? "",
+    icon:
+      document.querySelector<HTMLImageElement>('[data-testid="ProfileImage"]')
+        ?.src ?? "",
+  };
 }
 
 const linkTree = {
