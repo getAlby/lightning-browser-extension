4640bb1881317d94494c8393ea88bc2119dbdac4
Roland Bewick <roland.bewick@gmail.com>
2023-01-12 13:36:03 +0700

    fix: set password input width and title centering

--- src/app/screens/Onboard/SetPassword/index.tsx
+++ src/app/screens/Onboard/SetPassword/index.tsx
@@ -44,12 +44,14 @@ export default function SetPassword() {
     <form onSubmit={handleSubmit}>
       <div className="relative mt-14 lg:flex lg:space-x-8 bg-white dark:bg-surface-02dp pt-12 pb-4 lg:py-12 px-10">
         <div className="lg:w-1/2">
-          <h1 className="text-2xl font-bold dark:text-white">{t("title")}</h1>
+          <h1 className="text-2xl font-bold dark:text-white max-sm:text-center">
+            {t("title")}
+          </h1>
           <div className="lg:hidden mt-4">{unlockScreenshot}</div>
           <p className="text-gray-500 my-6 dark:text-gray-400">
             {t("description")}
           </p>
-          <div className="w-4/5 mb-6">
+          <div className="lg:w-4/5 mb-6">
             <PasswordForm
               i18nKeyPrefix="welcome.set_password"
               formData={formData}
@@ -72,6 +74,7 @@ export default function SetPassword() {
             !formData.password ||
             formData.password !== formData.passwordConfirmation
           }
+          className="max-sm:w-full"
         />
       </div>
     </form>
