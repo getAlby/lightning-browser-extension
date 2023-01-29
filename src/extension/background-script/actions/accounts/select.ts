ed4973b9704fbde84769b02b3fde515b5a3e6a95
Michael Bumann <hello@michaelbumann.com>
2023-01-29 18:48:08 +0200

    Update src/extension/background-script/actions/accounts/select.ts

--- src/extension/background-script/actions/accounts/select.ts
+++ src/extension/background-script/actions/accounts/select.ts
@@ -14,7 +14,7 @@ const select = async (message: MessageAccountSelect) => {
 
     state.setState({
       account,
-      nostr: null,
+      nostr: null, // reset memoized nostr instance
       connector: null, // reset memoized connector
       currentAccountId: accountId,
     });
