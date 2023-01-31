c21b6978e108148db7158615870f0a6046ab1bdd
Lukas Jakob <lu.jakob@googlemail.com>
2023-01-30 21:38:26 -0600

    feat: transactions, add data and list

--- src/app/screens/Transactions/index.tsx
+++ src/app/screens/Transactions/index.tsx
@@ -1,34 +1,59 @@
 import Container from "@components/Container";
 import TransactionsTable from "@components/TransactionsTable";
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Transaction } from "~/types";
+import { toast } from "react-toastify";
+import msg from "~/common/lib/msg";
+import { Payment, Transaction } from "~/types";
 
 function Transactions() {
-  const { t } = useTranslation("translation");
+  const { t } = useTranslation("translation", {
+    keyPrefix: "transactions",
+  });
 
-  const transactions: Transaction[] = [];
+  const [payments, setPayments] = useState<Transaction[]>([]);
+
+  async function fetchData() {
+    try {
+      const paymentsResponse = await msg.request<{
+        payments: Payment[];
+      }>("getPayments");
+
+      const transactions: Transaction[] = paymentsResponse.payments.map(
+        (payment) => ({
+          ...payment,
+          id: `${payment.id}`,
+          type: "sent",
+          date: dayjs(payment.createdAt).fromNow(),
+          title: payment.name || payment.description,
+          publisherLink: payment.location,
+        })
+      );
+
+      setPayments(transactions);
+    } catch (e) {
+      console.error(e);
+      if (e instanceof Error) toast.error(`Error: ${e.message}`);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <Container>
       <h2 className="mt-12 mb-2 text-2xl font-bold dark:text-white">
-        {t("transactions.title")}
+        {t("title")}
       </h2>
 
       <p className="mb-6 text-gray-500 dark:text-neutral-500">
-        {t("transactions.description")}
+        {t("description")}
       </p>
 
-      <div className="mb-12">
-        <div>
-          {transactions?.length > 0 && (
-            <TransactionsTable transactions={transactions} />
-          )}
-          {!transactions?.length && (
-            <p className="mb-6 text-gray-500 dark:text-neutral-500">
-              {t("transactions.list_empty")}
-            </p>
-          )}
-        </div>
+      <div>
+        {payments?.length > 0 && <TransactionsTable transactions={payments} />}
       </div>
     </Container>
   );
