84102de130b65f9ee825d33baa0fdb434b9225bf
Roland Bewick <roland.bewick@gmail.com>
2023-02-01 10:31:22 +0700

    chore: fix validateAccount error handling

--- src/extension/background-script/actions/setup/validateAccount.ts
+++ src/extension/background-script/actions/setup/validateAccount.ts
@@ -6,12 +6,14 @@ const validateAccount = async (
   message: {
     args: {
       connector: ConnectorType;
+      // FIXME: add typing
       config: unknown;
     };
   },
   sender: unknown
 ): Promise<{ data: ValidateAccountResponse }> => {
   const account = message.args;
+  // FIXME: this would not be needed if each connector took a config parameter
   const connector = new connectors[account.connector](account.config as never);
   await connector.init();
 
@@ -22,7 +24,13 @@ const validateAccount = async (
     return { data: { valid: true, info: info } };
   } catch (e) {
     console.error(e);
-    return { data: { valid: false, error: (e as Error).message } };
+    // TODO: consider adding a utility function to get a message from any type of exception
+    return {
+      data: {
+        valid: false,
+        error: e instanceof Error ? e.message : JSON.stringify(e),
+      },
+    };
   }
 };
 
