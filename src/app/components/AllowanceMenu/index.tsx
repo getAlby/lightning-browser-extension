a6d55e43aacae58dbe07860aa6f9af6e116815fe
Adithya Vardhan <imadithyavardhan@gmail.com>
2023-02-07 18:09:18 +0530

    fix: total budget is set to null

--- src/app/components/AllowanceMenu/index.tsx
+++ src/app/components/AllowanceMenu/index.tsx
@@ -47,7 +47,7 @@ function AllowanceMenu({ allowance, onEdit, onDelete }: Props) {
   const hasPermissions = !isLoadingPermissions && !!permissions?.length;
 
   const enableSubmit =
-    parseInt(budget) !== allowance.totalBudget ||
+    parseInt(budget || "0") !== allowance.totalBudget ||
     lnurlAuth !== allowance.lnurlAuth ||
     getChangedPermissionsIds().length;
 
@@ -116,7 +116,7 @@ function AllowanceMenu({ allowance, onEdit, onDelete }: Props) {
     try {
       await msg.request("updateAllowance", {
         id: allowance.id,
-        totalBudget: parseInt(budget),
+        totalBudget: parseInt(budget || "0"),
         lnurlAuth,
       });
 
