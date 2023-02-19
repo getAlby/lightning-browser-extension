abc0e5fcb7b67f7509dbe802151887d8607724d2
Luke Jones <luke@lukeojones.com>
2023-02-19 10:33:28 +0000

    feat(battery): remove substack class

--- src/extension/content-script/batteries/Substack.ts
+++ src/extension/content-script/batteries/Substack.ts
@@ -10,43 +10,15 @@ interface Article {
 }
 interface Person {
   name: string;
-  description: string;
-  image: { contentUrl: string } | string;
+  description?: string;
+  image?: { contentUrl: string } | string;
 }
 
-class SubstackLightningRecipient {
-  readonly name: string;
-  readonly address: string;
-  readonly description: string;
-  private readonly imageURL: string;
-
-  constructor(
-    name: string,
-    address: string,
-    description: string,
-    imageURL: string
-  ) {
-    this.name = name;
-    this.address = address;
-    this.description = description;
-    this.imageURL = imageURL;
-  }
-
-  /**
-   * Returns a re-sized version of the Recipients Image URL for the Substack CDN
-   */
-  getResizedImageURL(): string {
-    const match = this.imageURL.match(/fetch\/(.*)\//i);
-    if (!match) return this.imageURL;
-
-    const originalOptions = match[1].split(",");
-    const optionsToAdd = ["w_128", "h_128", "c_limit"];
-    const options = Array.from(
-      new Set([...originalOptions, ...optionsToAdd])
-    ).join(",");
-
-    return this.imageURL.replace(match[1], options);
-  }
+interface Recipient {
+  name: string;
+  address: string;
+  description: string;
+  imageURL: string;
 }
 
 const urlMatcher = /^https:\/\/(.+\.)?substack.com\/(profile|p)\//;
@@ -58,59 +30,67 @@ const urlMatcher = /^https:\/\/(.+\.)?substack.com\/(profile|p)\//;
  * If an Article has multiple Authors, then the first Lightning-enabled author is
  * used for creating Lightning Recipient.
  */
-const findRecipient = async (): Promise<SubstackLightningRecipient | null> => {
+const findRecipient = async (): Promise<Recipient | null> => {
   const script = await elementReady("script[type='application/ld+json']");
   const context = JSON.parse(script?.textContent ?? "null");
-  const recipient = createRecipient(
-    context["@type"] === "Person" ? context : null
-  );
+
+  const recipient =
+    context["@type"] === "Person" ? createRecipient(context) : null;
   if (recipient) return recipient;
 
   const article: Article = context["@type"] === "NewsArticle" ? context : null;
   const person = article?.author.find((p) =>
-    findLightningAddressInText(p.description)
+    findLightningAddressInText(p.description ?? "")
   );
-  return createRecipient(person);
+  return person ? createRecipient(person) : null;
 };
 
-const createRecipient = (
-  person?: Person
-): SubstackLightningRecipient | null => {
-  if (!person || !person.name || !person.description) return null;
-
-  const address = findLightningAddressInText(person.description);
+const createRecipient = (person: Person): Recipient | null => {
+  const address = findLightningAddressInText(person.description ?? "");
   if (!address) return null;
 
-  return new SubstackLightningRecipient(
-    person.name,
+  return {
+    name: person.name,
     address,
-    person.description,
-    extractImageUrl(person)
-  );
+    description: person.description ?? "",
+    imageURL: extractImageUrl(person),
+  };
 };
 
 const extractImageUrl = (person: Person): string => {
-  if (!person.image) return "";
-  if (typeof person.image === "string") return person.image;
-  return person.image.contentUrl;
+  if (typeof person.image === "string") {
+    return getResizedImageURLForCDN(person.image);
+  }
+  return getResizedImageURLForCDN(person.image?.contentUrl ?? "");
 };
 
-const battery = (): void => {
-  findRecipient().then((recipient) => {
-    if (!recipient) {
-      return;
-    }
-    setLightningData([
-      {
-        method: "lnurl",
-        address: recipient.address,
-        ...getOriginData(),
-        description: recipient.description,
-        name: recipient.name,
-        icon: recipient.getResizedImageURL(),
-      },
-    ]);
-  });
+const getResizedImageURLForCDN = (imageURL: string): string => {
+  const match = imageURL.match(/fetch\/(.*)\//i);
+  if (!match) return imageURL;
+
+  const originalOptions = match[1].split(",");
+  const optionsToAdd = ["w_128", "h_128", "c_limit"];
+  const options = Array.from(
+    new Set([...originalOptions, ...optionsToAdd])
+  ).join(",");
+
+  return imageURL.replace(match[1], options);
+};
+
+const battery = async (): Promise<void> => {
+  const recipient = await findRecipient();
+  if (!recipient) return;
+
+  setLightningData([
+    {
+      method: "lnurl",
+      address: recipient.address,
+      ...getOriginData(),
+      description: recipient.description,
+      name: recipient.name,
+      icon: recipient.imageURL,
+    },
+  ]);
 };
 
 const Substack = {
