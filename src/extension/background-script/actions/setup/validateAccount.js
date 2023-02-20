e0b159382086f59b7db1cbdf2af42349ede4d04f
Adithya Vardhan <imadithyavardhan@gmail.com>
2023-02-20 16:06:20 +0530

    chore: revert validateAccount

---
+++ src/extension/background-script/actions/setup/validateAccount.js
@@ -0,0 +1,19 @@
+import connectors from "../../connectors";
+
+const validateAccount = async (message, sender) => {
+  const account = message.args;
+  const connector = new connectors[account.connector](account.config);
+  await connector.init();
+
+  try {
+    const info = await connector.getInfo();
+    await connector.unload(); // unload the connector again, we just checked if it works but have no persistence
+
+    return { data: { valid: true, info: info } };
+  } catch (e) {
+    console.error(e);
+    return { data: { valid: false, error: e.message } };
+  }
+};
+
+export default validateAccount;
