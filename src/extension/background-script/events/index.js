821075a2a1ecbb4522fc7c4f96acfd7efb6f94d5
im-adithya <imadithyavardhan@gmail.com>
2023-02-28 01:13:12 +0530

    chore: add notifications for keysend payments

--- src/extension/background-script/events/index.js
+++ src/extension/background-script/events/index.js
@@ -2,16 +2,18 @@ import PubSub from "pubsub-js";
 
 import { updateAllowance } from "./allowances";
 import {
-  paymentSuccessNotification,
-  paymentFailedNotification,
-  lnurlAuthSuccessNotification,
   lnurlAuthFailedNotification,
+  lnurlAuthSuccessNotification,
+  paymentFailedNotification,
+  paymentSuccessNotification,
 } from "./notifications";
 import { persistSuccessfullPayment } from "./persistPayments";
 
 const subscribe = () => {
   PubSub.subscribe("ln.sendPayment.success", paymentSuccessNotification);
   PubSub.subscribe("ln.sendPayment.failed", paymentFailedNotification);
+  PubSub.subscribe("ln.keysend.success", paymentSuccessNotification);
+  PubSub.subscribe("ln.keysend.failed", paymentFailedNotification);
 
   PubSub.subscribe("ln.sendPayment.success", persistSuccessfullPayment);
   PubSub.subscribe("ln.sendPayment.success", updateAllowance);
