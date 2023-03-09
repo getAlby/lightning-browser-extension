fd11f594badfc65af1371d65a4cd91d3f6e354c3
René Aaron <rene@twentyuno.net>
2023-03-09 09:13:07 +0100

    fix: updated other Alby Account usages

--- tests/e2e/001-createWallets.spec.ts
+++ tests/e2e/001-createWallets.spec.ts
@@ -101,7 +101,7 @@ test.describe("Create or connect wallets", () => {
     const createNewWalletButton = await getByText($document, "Sign up");
     createNewWalletButton.click();
 
-    await findByText($document, "Your Alby account");
+    await findByText($document, "Your Alby Account");
 
     // type user email
     const emailField = await getByLabelText($document, "Email Address");
@@ -136,7 +136,7 @@ test.describe("Create or connect wallets", () => {
     const loginButton = await getByText($document, "Log in");
     loginButton.click();
 
-    await findByText($document, "Your Alby account");
+    await findByText($document, "Your Alby Account");
 
     // type user email
     const emailField = await getByLabelText(
