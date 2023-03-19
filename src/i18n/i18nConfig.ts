53d64ec309cd7a26d58a9dfc2515af51acbc0250
escapedcat <github@htmlcss.de>
2023-03-19 18:46:50 +0100

    feat(i18n): support polish language

--- src/i18n/i18nConfig.ts
+++ src/i18n/i18nConfig.ts
@@ -5,6 +5,7 @@ import "dayjs/locale/es";
 import "dayjs/locale/fi";
 import "dayjs/locale/fr";
 import "dayjs/locale/it";
+import "dayjs/locale/pl";
 import "dayjs/locale/pt-br";
 import "dayjs/locale/sv";
 import "dayjs/locale/zh-cn";
@@ -19,6 +20,7 @@ import es from "~/i18n/locales/es/translation.json";
 import fi from "~/i18n/locales/fi/translation.json";
 import fr from "~/i18n/locales/fr/translation.json";
 import it from "~/i18n/locales/it/translation.json";
+import pl from "~/i18n/locales/pl/translation.json";
 import pt_BR from "~/i18n/locales/pt_BR/translation.json";
 import sv from "~/i18n/locales/sv/translation.json";
 import zh_Hans from "~/i18n/locales/zh_Hans/translation.json";
@@ -68,6 +70,12 @@ export const resources = {
     components: it.components,
     permissions: it.permissions,
   },
+  pl: {
+    translation: pl.translation,
+    common: pl.common,
+    components: pl.components,
+    permissions: pl.permissions,
+  },
   "pt-BR": {
     translation: pt_BR.translation,
     common: pt_BR.common,
@@ -96,6 +104,7 @@ export const supportedLocales = [
   { locale: "es", label: "Español" },
   { locale: "fr", label: "Français" },
   { locale: "it", label: "Italiano" },
+  { locale: "pl", label: "Polski" },
   { locale: "pt-BR", label: "Português (Brasil)" },
   { locale: "fi", label: "Suomalainen" },
   { locale: "sv", label: "Svenska" },
