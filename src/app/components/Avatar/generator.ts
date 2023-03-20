017732614e60435acb82b4b056fe170d1e6b92db
René Aaron <rene@twentyuno.net>
2023-03-20 18:10:32 +0100

    fix: ts

--- src/app/components/Avatar/generator.ts
+++ src/app/components/Avatar/generator.ts
@@ -1,4 +1,3 @@
-const maxColor = parseInt("fff", 16);
 const defaultSize = 128;
 const defaultSvgNs = "http://www.w3.org/2000/svg";
 const minShapes = 2;
@@ -9,15 +8,19 @@ const maxDist = 0.6;
 const minSpreadDist = 0.1;
 const colorSpread = 0.15;
 
-function generateSvgGAvatar(seed = "", options) {
+type Options = {
+  size: number;
+};
+
+function generateSvgGAvatar(seed = "", options: Options) {
   options.size = options.size || defaultSize;
   const g = document.createElementNS(defaultSvgNs, "g");
   const rect = document.createElementNS(defaultSvgNs, "rect");
-  const random = rand(seed).double;
+  const random = new RandomGenerator(seed).double;
   const shapes = minShapes + Math.floor(0.1234 * (randomShapes + 1));
-  const color = new ColorGen(random);
-  rect.setAttributeNS(null, "width", options.size);
-  rect.setAttributeNS(null, "height", options.size);
+  const color = new ColorGenerator(random);
+  rect.setAttributeNS(null, "width", options.size.toString());
+  rect.setAttributeNS(null, "height", options.size.toString());
   rect.setAttributeNS(null, "fill", randomColor(random));
   g.appendChild(rect);
   for (let shape = 0; shape < shapes; ++shape) {
@@ -26,7 +29,11 @@ function generateSvgGAvatar(seed = "", options) {
   return g;
 }
 
-function genShape(random, color, options) {
+function genShape(
+  random: () => number,
+  color: ColorGenerator,
+  options: Options
+) {
   let aX = random() * options.size;
   let aY = random() * options.size;
   let bX = random() * options.size;
@@ -70,24 +77,20 @@ function genShape(random, color, options) {
     `${aX} ${aY}`;
   const element = document.createElementNS(defaultSvgNs, "path");
   element.setAttributeNS(null, "d", path);
-  element.setAttributeNS(null, "fill", color.next(random));
-  // debug(g, [aX, aY]);
-  // debug(g, [aX + Math.cos(dir1b) * sAb, aY - Math.sin(dir1b) * sAb], "#0f0");
-  // debug(g, [aX + Math.cos(dir1a) * sAa, aY - Math.sin(dir1a) * sAa], "#00f");
-  // debug(g, [bX, bY]);
-  // debug(g, [bX + Math.cos(dir2b) * sBb, bY - Math.sin(dir2b) * sBb], "#0f0");
-  // debug(g, [bX + Math.cos(dir2a) * sBa, bY - Math.sin(dir2a) * sBa], "#00f");
-  // debugLine(g, [aX, aY, dir]);
+  element.setAttributeNS(null, "fill", color.next());
   return element;
 }
 
-class ColorGen {
-  constructor(random) {
+class ColorGenerator {
+  private random: () => number;
+  private colors: number[];
+
+  constructor(random: () => number) {
     this.random = random;
     this.colors = [-1, -1, -1];
   }
 
-  next() {
+  public next(): string {
     this.colors = this.colors.map((c) =>
       Math.max(
         0,
@@ -105,12 +108,13 @@ class ColorGen {
   }
 }
 
-function randomColor(random) {
+function randomColor(random: () => number): string {
+  const maxColor = 16777215; // 0xffffff
   return `#${Math.floor(random() * maxColor).toString(16)}`;
 }
 
-function distance(x1, y1, x2, y2) {
-  return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+function distance(x1: number, y1: number, x2: number, y2: number): number {
+  return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
 }
 
 // Source: https://github.com/davidbau/seedrandom/blob/released/lib/alea.js
@@ -140,70 +144,66 @@ function distance(x1, y1, x2, y2) {
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-function Alea(seed) {
-  let me = this,
-    mash = Mash();
+class Alea {
+  private c: number;
+  private s0: number;
+  private s1: number;
+  private s2: number;
 
-  me.next = function () {
-    let t = 2091639 * me.s0 + me.c * 2.3283064365386963e-10; // 2^-32
-    me.s0 = me.s1;
-    me.s1 = me.s2;
-    return (me.s2 = t - (me.c = t | 0));
-  };
+  constructor(seed: string) {
+    const mash = Mash();
 
-  // Apply the seeding algorithm from Baagoe.
-  me.c = 1;
-  me.s0 = mash(" ");
-  me.s1 = mash(" ");
-  me.s2 = mash(" ");
-  me.s0 -= mash(seed);
-  if (me.s0 < 0) {
-    me.s0 += 1;
+    this.next = this.next.bind(this);
+
+    this.c = 1;
+    this.s0 = mash(" ");
+    this.s1 = mash(" ");
+    this.s2 = mash(" ");
+
+    this.s0 -= mash(seed);
+    if (this.s0 < 0) {
+      this.s0 += 1;
+    }
+
+    this.s1 -= mash(seed);
+    if (this.s1 < 0) {
+      this.s1 += 1;
+    }
+
+    this.s2 -= mash(seed);
+    if (this.s2 < 0) {
+      this.s2 += 1;
+    }
   }
-  me.s1 -= mash(seed);
-  if (me.s1 < 0) {
-    me.s1 += 1;
+
+  public next(): number {
+    const t = 2091639 * this.s0 + this.c * 2.3283064365386963e-10; // 2^-32
+    this.s0 = this.s1;
+    this.s1 = this.s2;
+    this.c = t | 0;
+    this.s2 = t - this.c;
+    return this.s2;
   }
-  me.s2 -= mash(seed);
-  if (me.s2 < 0) {
-    me.s2 += 1;
-  }
-  mash = null;
 }
 
-function copy(f, t) {
-  t.c = f.c;
-  t.s0 = f.s0;
-  t.s1 = f.s1;
-  t.s2 = f.s2;
-  return t;
-}
+class RandomGenerator {
+  private xg: Alea;
+  private state?: object;
 
-function rand(seed, opts) {
-  let xg = new Alea(seed),
-    state = opts && opts.state,
-    prng = xg.next;
-  prng.int32 = function () {
-    return (xg.next() * 0x100000000) | 0;
-  };
-  prng.double = function () {
+  constructor(seed: string) {
+    this.xg = new Alea(seed);
+    this.double = this.double.bind(this);
+  }
+
+  double = (): number => {
+    const prng = this.xg.next;
     return prng() + ((prng() * 0x200000) | 0) * 1.1102230246251565e-16; // 2^-53
   };
-  prng.quick = prng;
-  if (state) {
-    if (typeof state == "object") copy(state, xg);
-    prng.state = function () {
-      return copy(xg, {});
-    };
-  }
-  return prng;
 }
-
 function Mash() {
   let n = 0xefc8249d;
 
-  let mash = function (data) {
-    data = data.toString();
+  const mash = function (data: string) {
     for (let i = 0; i < data.length; i++) {
       n += data.charCodeAt(i);
       let h = 0.02519603282416938 * n;
