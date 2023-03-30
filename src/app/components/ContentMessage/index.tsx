22238d3cada37f0816dee6292ab014f12e3243eb
SiddheshKukade <littlelord124@gmail.com>
2023-03-30 16:18:49 +0530

    feat: add custom dialog for liking nostr post

--- src/app/components/ContentMessage/index.tsx
+++ src/app/components/ContentMessage/index.tsx
@@ -1,3 +1,5 @@
+import { t } from "i18next";
+
 type Props = {
   heading: string;
   content: string;
@@ -9,7 +11,16 @@ function ContentMessage({ heading, content }: Props) {
       <dl className="my-4 p-4 shadow bg-white dark:bg-surface-02dp rounded-lg overflow-hidden">
         <dt className="font-medium dark:text-white">{heading}</dt>
         <dd className="text-gray-500 dark:text-gray-400 break-all">
-          {content}
+          {content == "+" ? (
+            <>
+              <dt className="font-medium dark:text-white">
+                {t("nostr.kinds.7")}
+              </dt>
+              {t("nostr.reactions.like")}
+            </>
+          ) : (
+            content
+          )}
         </dd>
       </dl>
     </>
