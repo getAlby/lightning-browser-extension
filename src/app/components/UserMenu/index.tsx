4f1d3d32079595b4cb071e40336acea35d42b7ec
Ankit Singh <ankit.singh.zm@gmail.com>
2023-04-01 23:16:10 +0530

    fix: code refractoring/cleaning for navbar

--- src/app/components/UserMenu/index.tsx
+++ src/app/components/UserMenu/index.tsx
@@ -42,141 +42,95 @@ export default function UserMenu() {
       console.error(e);
     }
   }
-
-  if (window.location.pathname == "/options.html") {
-    return (
-      <Menu as="div" className="relative">
-        <Menu.Button className="flex items-center text-gray-700 dark:text-white hover:text-black dark:hover:text-white transition-colors duration-200">
-          <MenuIcon className="h-6 w-6" />
-        </Menu.Button>
-        <Menu.List position="left">
-          <Menu.ItemButton
-            onClick={() => {
-              openOptions("settings");
-            }}
-          >
-            <GearIcon className="h-5 w-5 mr-2 text-gray-700 dark:text-neutral-300" />
-            {tCommon("settings")}
-          </Menu.ItemButton>
-          <Menu.ItemButton
-            onClick={() => {
-              openOptions("accounts");
-            }}
-          >
-            <AddressBookIcon className="h-5 w-5 mr-2 text-gray-700 dark:text-neutral-300" />
-            {tCommon("accounts")}
-          </Menu.ItemButton>
-          <Menu.ItemButton
-            onClick={() => {
-              utils.openUrl("https://feedback.getalby.com");
-            }}
-          >
-            <MagicWandIcon className="h-5 w-5 mr-2 text-gray-700 dark:text-neutral-300" />
-            {tCommon("feedback")}
-          </Menu.ItemButton>
-          <Menu.ItemButton
-            onClick={() => {
-              utils.openUrl("https://guides.getalby.com");
-            }}
-          >
-            <HelpIcon className="h-5 w-5 mr-2 text-gray-700 dark:text-neutral-300" />
-            {tCommon("help")}
-          </Menu.ItemButton>
-          <Menu.Divider />
-          <Menu.ItemButton onClick={lock}>
-            <LockIcon className="h-5 w-5 mr-2 text-gray-700 dark:text-neutral-300" />
-            {tCommon("actions.lock")}
-          </Menu.ItemButton>
-        </Menu.List>
-      </Menu>
-    );
-  } else {
-    return (
-      <Menu as="div" className="relative">
-        <Menu.Button className="flex items-center text-gray-700 dark:text-white hover:text-black dark:hover:text-white transition-colors duration-200">
-          <MenuIcon className="h-6 w-6" />
-        </Menu.Button>
-        <Menu.List position="left">
-          <Menu.ItemButton
-            onClick={() => {
-              openOptions("discover");
-            }}
-          >
-            <RocketIcon className="h-5 w-5 mr-2 text-gray-700 dark:text-neutral-300" />
-            {tCommon("discover")}
-          </Menu.ItemButton>
-          <Menu.ItemButton
-            onClick={() => {
-              openOptions("publishers");
-            }}
-          >
-            <ConnectedSiteIcon className="h-5 w-5 mr-2 text-gray-700 dark:text-neutral-300" />
-            {tCommon("connected_sites")}
-          </Menu.ItemButton>
-          <Menu.ItemButton
-            onClick={() => {
-              navigate("/send");
-            }}
-          >
-            <SendIcon className="w-6 h-6 -ml-0.5 mr-2 text-gray-700 dark:text-neutral-300" />
-            {tCommon("actions.send")}
-          </Menu.ItemButton>
-          <Menu.ItemButton
-            onClick={() => {
-              navigate("/receive");
-            }}
-          >
-            <ReceiveIcon className="w-6 h-6 -ml-0.5 mr-2 text-gray-700 dark:text-neutral-300" />
-            {tCommon("actions.receive")}
-          </Menu.ItemButton>
-          <Menu.ItemButton
-            onClick={() => {
-              openOptions("transactions");
-            }}
-          >
-            <TransactionsIcon className="h-5 w-5 mr-2 text-gray-700 dark:text-neutral-300" />
-            {tCommon("transactions")}
-          </Menu.ItemButton>
-          <Menu.Divider />
-          <Menu.ItemButton
-            onClick={() => {
-              openOptions("settings");
-            }}
-          >
-            <GearIcon className="h-5 w-5 mr-2 text-gray-700 dark:text-neutral-300" />
-            {tCommon("settings")}
-          </Menu.ItemButton>
-          <Menu.ItemButton
-            onClick={() => {
-              openOptions("accounts");
-            }}
-          >
-            <AddressBookIcon className="h-5 w-5 mr-2 text-gray-700 dark:text-neutral-300" />
-            {tCommon("accounts")}
-          </Menu.ItemButton>
-          <Menu.ItemButton
-            onClick={() => {
-              utils.openUrl("https://feedback.getalby.com");
-            }}
-          >
-            <MagicWandIcon className="h-5 w-5 mr-2 text-gray-700 dark:text-neutral-300" />
-            {tCommon("feedback")}
-          </Menu.ItemButton>
-          <Menu.ItemButton
-            onClick={() => {
-              utils.openUrl("https://guides.getalby.com");
-            }}
-          >
-            <HelpIcon className="h-5 w-5 mr-2 text-gray-700 dark:text-neutral-300" />
-            {tCommon("help")}
-          </Menu.ItemButton>
-          <Menu.Divider />
-          <Menu.ItemButton onClick={lock}>
-            <LockIcon className="h-5 w-5 mr-2 text-gray-700 dark:text-neutral-300" />
-            {tCommon("actions.lock")}
-          </Menu.ItemButton>
-        </Menu.List>
-      </Menu>
-    );
-  }
+  return (
+    <Menu as="div" className="relative">
+      <Menu.Button className="flex items-center text-gray-700 dark:text-white hover:text-black dark:hover:text-white transition-colors duration-200">
+        <MenuIcon className="h-6 w-6" />
+      </Menu.Button>
+      <Menu.List position="left">
+        {window.location.pathname !== "/options.html" && (
+          <>
+            <Menu.ItemButton
+              onClick={() => {
+                openOptions("discover");
+              }}
+            >
+              <RocketIcon className="h-5 w-5 mr-2 text-gray-700 dark:text-neutral-300" />
+              {tCommon("discover")}
+            </Menu.ItemButton>
+            <Menu.ItemButton
+              onClick={() => {
+                openOptions("publishers");
+              }}
+            >
+              <ConnectedSiteIcon className="h-5 w-5 mr-2 text-gray-700 dark:text-neutral-300" />
+              {tCommon("connected_sites")}
+            </Menu.ItemButton>
+            <Menu.ItemButton
+              onClick={() => {
+                navigate("/send");
+              }}
+            >
+              <SendIcon className="w-6 h-6 -ml-0.5 mr-2 text-gray-700 dark:text-neutral-300" />
+              {tCommon("actions.send")}
+            </Menu.ItemButton>
+            <Menu.ItemButton
+              onClick={() => {
+                navigate("/receive");
+              }}
+            >
+              <ReceiveIcon className="w-6 h-6 -ml-0.5 mr-2 text-gray-700 dark:text-neutral-300" />
+              {tCommon("actions.receive")}
+            </Menu.ItemButton>
+            <Menu.ItemButton
+              onClick={() => {
+                openOptions("transactions");
+              }}
+            >
+              <TransactionsIcon className="h-5 w-5 mr-2 text-gray-700 dark:text-neutral-300" />
+              {tCommon("transactions")}
+            </Menu.ItemButton>
+            <Menu.Divider />
+          </>
+        )}
+        <Menu.ItemButton
+          onClick={() => {
+            openOptions("settings");
+          }}
+        >
+          <GearIcon className="h-5 w-5 mr-2 text-gray-700 dark:text-neutral-300" />
+          {tCommon("settings")}
+        </Menu.ItemButton>
+        <Menu.ItemButton
+          onClick={() => {
+            openOptions("accounts");
+          }}
+        >
+          <AddressBookIcon className="h-5 w-5 mr-2 text-gray-700 dark:text-neutral-300" />
+          {tCommon("accounts")}
+        </Menu.ItemButton>
+        <Menu.ItemButton
+          onClick={() => {
+            utils.openUrl("https://feedback.getalby.com");
+          }}
+        >
+          <MagicWandIcon className="h-5 w-5 mr-2 text-gray-700 dark:text-neutral-300" />
+          {tCommon("feedback")}
+        </Menu.ItemButton>
+        <Menu.ItemButton
+          onClick={() => {
+            utils.openUrl("https://guides.getalby.com");
+          }}
+        >
+          <HelpIcon className="h-5 w-5 mr-2 text-gray-700 dark:text-neutral-300" />
+          {tCommon("help")}
+        </Menu.ItemButton>
+        <Menu.Divider />
+        <Menu.ItemButton onClick={lock}>
+          <LockIcon className="h-5 w-5 mr-2 text-gray-700 dark:text-neutral-300" />
+          {tCommon("actions.lock")}
+        </Menu.ItemButton>
+      </Menu.List>
+    </Menu>
+  );
 }
