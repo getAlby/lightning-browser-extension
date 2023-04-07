3fdb6157f1c168e4c068dbd8db02e39b08b93440
René Aaron <100827540+reneaaron@users.noreply.github.com>
2023-04-07 18:09:09 +0200

    feat: avatars & account menu overhaul (#2195)
    * feat: avatars & account menu overhaul
    
    * fix: links & styles
    
    * fix: ts
    
    * fix: translations
    
    * fix: remove alias from account name, add account menu item
    
    * fix: update test
    
    * fix: add arrow
    
    * fix: add size restrictions to dropdown
    
    * fix: cleanup
    
    * fix: navbar margin
    
    * fix: navigation, cleanup
    
    * fix: tests

--- src/app/components/Avatar/index.tsx
+++ src/app/components/Avatar/index.tsx
@@ -1,12 +1,38 @@
-import { WalletIcon } from "@bitcoin-design/bitcoin-icons-react/outline";
+import { useEffect, useRef } from "react";
+import { generateSvgGAvatar } from "~/app/components/Avatar/generator";
 
 type Props = {
   name: string;
-  size?: number | string;
+  size: number;
 };
 
 const Avatar = (props: Props) => {
-  return <WalletIcon className="-ml-1 w-8 h-8 opacity-50 dark:text-white" />;
+  const svgRef = useRef<SVGSVGElement>(null);
+
+  useEffect(() => {
+    if (svgRef.current) {
+      const svgElement = svgRef.current;
+      svgElement.appendChild(
+        generateSvgGAvatar(props.name, {
+          size: props.size,
+        })
+      );
+    }
+  }, [props.name, props.size]);
+
+  return (
+    <>
+      <div className="overflow-hidden rounded-full">
+        <svg
+          ref={svgRef}
+          width={props.size}
+          height={props.size}
+          viewBox={`0 0 ${props.size} ${props.size}`}
+          xmlns="http://www.w3.org/2000/svg"
+        />
+      </div>
+    </>
+  );
 };
 
 export default Avatar;
