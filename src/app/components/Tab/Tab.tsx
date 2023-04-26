f759659fa580eac81d58a44ee67f8d1aaa17ba89
Roland Bewick <roland.bewick@gmail.com>
2023-04-26 16:34:46 +0700

    fix: tab width

--- src/app/components/Tab/Tab.tsx
+++ src/app/components/Tab/Tab.tsx
@@ -16,7 +16,7 @@ export function Tab({ icon, onClick, label, className, ...otherProps }: Props) {
       onClick={onClick}
       className={({ selected }) =>
         classNames(
-          "font-bold text-base gap-[6px] inline-flex px-2 flex-1 py-2 justify-center items-center rounded-md focus:outline-none transition duration-150",
+          "font-bold text-base gap-[6px] flex px-2 py-2 justify-center items-center rounded-md focus:outline-none transition duration-150 w-44",
           selected
             ? "text-gray-700 h-10 drop-shadow-xl dark:text-neutral-200 bg-white dark:bg-surface-16dp z-20 first:translate-x-1 last:translate-x-[-4px]"
             : "text-gray-500 h-[38px] dark:text-neutral-500 hover:text-gray-700 hover:bg-gray-200 bg-gray-100 dark:bg-surface-02dp dark:hover:text-gray-300",
