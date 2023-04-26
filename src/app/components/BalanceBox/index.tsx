a5b0886d9245a4eb51ff8d1507b4353088a88876
René Aaron <rene@twentyuno.net>
2023-04-26 23:33:11 +0200

    fix: increase size

--- src/app/components/BalanceBox/index.tsx
+++ src/app/components/BalanceBox/index.tsx
@@ -1,26 +1,30 @@
 import { useAccount } from "~/app/context/AccountContext";
+import { classNames } from "~/app/utils";
 
 type Props = {
   className?: string;
 };
 
-function BalanceBox(props: Props) {
+function BalanceBox({ className }: Props) {
   const { balancesDecorated } = useAccount();
   const balanceParts = balancesDecorated.accountBalance.split(" ");
 
   return (
     <div
-      className="w-full flex flex-col items-center justify-center dark:text-white p-4"
+      className={classNames(
+        "w-full flex flex-col items-center justify-center dark:text-white p-4",
+        className ?? ""
+      )}
     >
       {balancesDecorated.accountBalance && (
-        <div className="text-xl">
-          <span className="font-medium slashed-zero">{balanceParts[0]}</span>
+        <div className="text-2xl">
+          <span className="font-medium">{balanceParts[0]}</span>
           <span>&nbsp;{balanceParts[1]}</span>
         </div>
       )}
 
       {balancesDecorated.fiatBalance && (
-        <span className="text-sm text-gray-500 mt-2">
+        <span className="text-gray-500 mt-2">
           ~{balancesDecorated.fiatBalance}
         </span>
       )}
