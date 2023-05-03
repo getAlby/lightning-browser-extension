3d524335ae6118ac27120ca38c97f934cda459b1
Lukas Jakob <lu.jakob@googlemail.com>
2023-05-03 16:31:40 -0500

    feat: remove connector back button

--- src/app/screens/connectors/ChooseConnector/index.tsx
+++ src/app/screens/connectors/ChooseConnector/index.tsx
@@ -1,7 +1,4 @@
-import Button from "@components/Button";
 import LinkButton from "@components/LinkButton";
-import { useTranslation } from "react-i18next";
-import { useNavigate } from "react-router-dom";
 import { ConnectorRoute } from "~/app/router/connectorRoutes";
 
 type Props = {
@@ -15,10 +12,6 @@ export default function ChooseConnector({
   description,
   connectorRoutes,
 }: Props) {
-  const { t: tCommon } = useTranslation("common");
-
-  const navigate = useNavigate();
-
   return (
     <>
       <div className="relative mt-14 mb-4 lg:mb-14 lg:grid lg:gap-8 text-center">
@@ -44,13 +37,6 @@ export default function ChooseConnector({
             ))}
           </div>
         </div>
-      </div>
-      <div className="mb-4 mt-8 md:my-8 flex flex-col-reverse justify-center gap-4 md:flex-row">
-        <Button
-          label={tCommon("actions.back")}
-          onClick={() => navigate(-1)}
-          className="max-sm:w-full"
-        />
       </div>
     </>
   );
