ed6ea4ef7ca3494db33cfb06ee7d02f24f67a880
M. Quasar <marcosm@gmail.com>
2023-05-10 23:13:19 -0300

    fix: removed findByText

--- tests/e2e/helpers/loadExtension.ts
+++ tests/e2e/helpers/loadExtension.ts
@@ -92,11 +92,6 @@ export const createNewWalletWithPassword = async (options?: {
     page.waitForNavigation(), // The promise resolves after navigation has finished
   ]);
 
-  await findByText(
-    $document,
-    "To start using the Alby Extension, use your Alby Account or connect to your lightning wallet."
-  );
-
   if (options?.openConnectOtherWallet) {
     const connectTexts = await findAllByText($document, "Connect");
     connectTexts[1].click(); // we have headline and button using "connect", button is second
