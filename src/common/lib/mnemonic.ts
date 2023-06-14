e7e922a6717f9c65a78521485d07f3e3951ad329
im-adithya <imadithyavardhan@gmail.com>
2023-06-14 15:27:00 +0530

    fix: liquid derivation path and add regtest

--- src/common/lib/mnemonic.ts
+++ src/common/lib/mnemonic.ts
@@ -2,7 +2,8 @@ import * as secp256k1 from "@noble/secp256k1";
 import { HDKey } from "@scure/bip32";
 import * as bip39 from "@scure/bip39";
 
-export const LIQUID_DERIVATION_PATH = "m/44'/1776'/0'/0/0";
+export const LIQUID_DERIVATION_PATH = "m/84'/1776'/0'/0/0";
+export const LIQUID_DERIVATION_PATH_REGTEST = "m/84'/1'/0'/0/0";
 export const NOSTR_DERIVATION_PATH = "m/44'/1237'/0'/0/0"; // NIP-06
 export const BTC_TAPROOT_DERIVATION_PATH = "m/86'/0'/0'/0/0";
 export const BTC_TAPROOT_DERIVATION_PATH_REGTEST = "m/86'/1'/0'/0/0";
