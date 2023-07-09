688cfc56e3fc33e9bba548dd65128c40bd7c3f6c
René Aaron <rene@twentyuno.net>
2023-07-09 22:18:37 +0200

    fix: inject scripts without dependencies first

--- src/extension/content-script/onstart.ts
+++ src/extension/content-script/onstart.ts
@@ -10,22 +10,23 @@ async function onstart() {
     return;
   }
 
-  const account = await api.getAccount();
   // window.webln
   injectScript(browser.runtime.getURL("js/inpageScriptWebLN.bundle.js"));
 
-  // window.webbtc
-  if (account.hasMnemonic) {
-    injectScript(browser.runtime.getURL("js/inpageScriptWebBTC.bundle.js"));
-  }
+  // window.alby
+  injectScript(browser.runtime.getURL("js/inpageScriptAlby.bundle.js"));
+
+  const account = await api.getAccount();
 
   // window.nostr
   if (account.nostrEnabled) {
     injectScript(browser.runtime.getURL("js/inpageScriptNostr.bundle.js"));
   }
 
-  // window.alby
-  injectScript(browser.runtime.getURL("js/inpageScriptAlby.bundle.js"));
+  // window.webbtc
+  if (account.hasMnemonic) {
+    injectScript(browser.runtime.getURL("js/inpageScriptWebBTC.bundle.js"));
+  }
 }
 
 onstart();
