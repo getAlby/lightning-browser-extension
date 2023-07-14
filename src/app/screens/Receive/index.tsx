5506d9bccdb8959df23557e4f68cc87d4670f93c
René Aaron <rene@twentyuno.net>
2023-07-14 14:25:16 +0200

    fix: redeem LNURL for all connectors

--- src/app/screens/Receive/index.tsx
+++ src/app/screens/Receive/index.tsx
@@ -280,27 +280,26 @@ function Receive() {
               </Container>
             </fieldset>
           </form>
-          {isAlbyUser && (
-            <div>
-              <Container justifyBetween maxWidth="sm">
-                <div className="relative flex  items-center mb-8">
-                  <div className="flex-grow border-t border-gray-300 dark:border-gray-700"></div>
-                  <span className="flex-shrink mx-4  text-gray-500 dark:text-gray-400 fw-bold">
-                    {tCommon("or")}
-                  </span>
-                  <div className="flex-grow border-t  border-gray-300 dark:border-gray-700"></div>
-                </div>
-                <div className="mb-4">
-                  <Button
-                    type="button"
-                    label={t("redeem_lnurl")}
-                    fullWidth
-                    onClick={() => {
-                      navigate("/lnurlRedeem");
-                    }}
-                  />
-                </div>
-
+          <div>
+            <Container justifyBetween maxWidth="sm">
+              <div className="relative flex  items-center mb-8">
+                <div className="flex-grow border-t border-gray-300 dark:border-gray-700"></div>
+                <span className="flex-shrink mx-4  text-gray-500 dark:text-gray-400 fw-bold">
+                  {tCommon("or")}
+                </span>
+                <div className="flex-grow border-t  border-gray-300 dark:border-gray-700"></div>
+              </div>
+              <div className="mb-4">
+                <Button
+                  type="button"
+                  label={t("redeem_lnurl")}
+                  fullWidth
+                  onClick={() => {
+                    navigate("/lnurlRedeem");
+                  }}
+                />
+              </div>
+              {isAlbyUser && (
                 <div className="mb-4">
                   <Button
                     type="button"
@@ -311,9 +310,9 @@ function Receive() {
                     }}
                   />
                 </div>
-              </Container>
-            </div>
-          )}
+              )}
+            </Container>
+          </div>
         </div>
       )}
     </div>
