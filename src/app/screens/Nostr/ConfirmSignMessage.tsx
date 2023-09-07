d64bdde5f241b3dfcbc967e21f3b860d10e661f6
René Aaron <rene@twentyuno.net>
2023-09-07 09:29:29 +0200

    fix: nostr kind permissions

--- src/app/screens/Nostr/ConfirmSignMessage.tsx
+++ src/app/screens/Nostr/ConfirmSignMessage.tsx
@@ -3,7 +3,6 @@ import Container from "@components/Container";
 import ContentMessage from "@components/ContentMessage";
 import PublisherCard from "@components/PublisherCard";
 import SuccessMessage from "@components/SuccessMessage";
-import Checkbox from "@components/form/Checkbox";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
@@ -105,7 +104,7 @@ function ConfirmSignMessage() {
             </div>
             <div>
               <div className="flex items-center mb-4">
-                <Checkbox
+                {/* <Checkbox
                   id="remember_permission"
                   name="remember_permission"
                   checked={rememberPermission}
@@ -118,7 +117,25 @@ function ConfirmSignMessage() {
                   className="cursor-pointer ml-2 block text-sm text-gray-900 font-medium dark:text-white"
                 >
                   {tCommon("actions.remember")}
-                </label>
+                </label> */}
+                {/* <Hyperlink>Manage permissions</Hyperlink> */}
+
+                <label
+                  htmlFor="countries"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                ></label>
+                <select
+                  id="countries"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                >
+                  <option selected>Ask me every time</option>
+                  <option value="US">
+                    Remember for short messages on snort.social
+                  </option>
+                  <option value="CA">
+                    Remember for all events on snort.social
+                  </option>
+                </select>
               </div>
               <ConfirmOrCancel
                 disabled={loading}
