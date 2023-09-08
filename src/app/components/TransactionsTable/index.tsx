61e979ecaf6aa1b9a085e4a115e43bf48721d672
René Aaron <rene@twentyuno.net>
2023-09-08 11:16:12 +0200

    feat: new transaction list

--- src/app/components/TransactionsTable/index.tsx
+++ src/app/components/TransactionsTable/index.tsx
@@ -1,12 +1,13 @@
-import { CaretDownIcon } from "@bitcoin-design/bitcoin-icons-react/filled";
+import {
+  ArrowDownIcon,
+  ArrowUpIcon,
+} from "@bitcoin-design/bitcoin-icons-react/filled";
 import Loading from "@components/Loading";
-import { Disclosure } from "@headlessui/react";
 import { useTranslation } from "react-i18next";
-import Button from "~/app/components/Button";
+import Badge from "~/app/components/Badge";
 import { useSettings } from "~/app/context/SettingsContext";
+import { classNames } from "~/app/utils";
 import { Transaction } from "~/types";
-
-import Badge from "../Badge";
 
 export type Props = {
   transactions: Transaction[] | null | undefined;
@@ -29,146 +30,90 @@ export default function TransactionsTable({
   ) : !transactions?.length && noResultMsg ? (
     <p className="text-gray-500 dark:text-neutral-400">{noResultMsg}</p>
   ) : (
-    <>
-      <div className="shadow overflow-hidden rounded-lg bg-white divide-y divide-gray-200 dark:divide-white/10 dark:bg-surface-02dp">
-        {transactions?.map((tx) => (
-          <div key={tx.id} className="px-3 py-2">
-            <Disclosure>
-              {({ open }) => (
-                <>
-                  <div className="flex">
-                    <div className="overflow-hidden mr-3">
-                      <div
-                        className="
-                  text-sm font-medium text-gray-900 truncate dark:text-white"
-                      >
-                        <p className="truncate">
-                          {tx.publisherLink && tx.title ? (
-                            <a
-                              target="_blank"
-                              href={tx.publisherLink}
-                              rel="noopener noreferrer"
-                            >
-                              {tx.title}
-                            </a>
-                          ) : (
-                            tx.title || tx.boostagram?.message || "\u00A0"
-                          )}
-                        </p>
-                      </div>
-                      <p className="text-xs text-gray-600 dark:text-neutral-400">
-                        {tx.date}
-                      </p>
-                    </div>
-                    {tx.badges && (
-                      <div className="ml-6 space-x-3">
-                        {tx.badges.map((badge) => (
-                          <Badge
-                            key={badge.label}
-                            label={badge.label}
-                            color={badge.color}
-                            textColor={badge.textColor}
-                          />
-                        ))}
-                      </div>
-                    )}
-                    <div className="flex ml-auto text-right space-x-3 shrink-0">
-                      <div>
-                        <p className="text-sm font-medium dark:text-white">
-                          {[tx.type && "sent", "sending"].includes(tx.type)
-                            ? "-"
-                            : "+"}
-                          {getFormattedSats(tx.totalAmount)}
-                        </p>
-                        {!!tx.totalAmountFiat && (
-                          <p className="text-xs text-gray-600 dark:text-neutral-400">
-                            ~{tx.totalAmountFiat}
-                          </p>
-                        )}
-                      </div>
-                      {(!!tx.description ||
-                        [tx.type && "sent", "sending"].includes(tx.type) ||
-                        (tx.type === "received" && tx.boostagram)) && (
-                        <Disclosure.Button className="block text-gray-500 hover:text-black dark:hover:text-white transition-color duration-200">
-                          <CaretDownIcon
-                            className={`${open ? "rotate-180" : ""} w-5 h-5`}
-                          />
-                        </Disclosure.Button>
-                      )}
-                    </div>
+    <div className="shadow overflow-hidden rounded-lg bg-white divide-y divide-gray-200 dark:divide-white/10 dark:bg-surface-02dp">
+      {transactions?.map((tx) => {
+        const type = [tx.type && "sent", "sending"].includes(tx.type)
+          ? "outgoing"
+          : "incoming";
+
+        return (
+          <div
+            key={tx.id}
+            className="px-3 py-2 hover:bg-gray-100 dark:hover:bg-surface-02dp cursor-pointer"
+          >
+            <div className="flex gap-3">
+              <div className="flex items-center">
+                {type == "outgoing" ? (
+                  <div className="flex justify-center items-center bg-orange-100 rounded-full w-8 h-8">
+                    <ArrowUpIcon className="w-6 h-6 text-orange-400" />
                   </div>
-                  <Disclosure.Panel>
-                    <div className="text-xs text-gray-600 dark:text-neutral-400">
-                      {(tx.description || tx.boostagram) && (
-                        <div className="my-2">
-                          {tx.description && <p>{tx.description}</p>}
-                          {tx.boostagram && (
-                            <ul>
-                              <li>
-                                {t("transactionsTable.boostagram.sender")}:{" "}
-                                {tx.boostagram.sender_name}
-                              </li>
-                              <li>
-                                {t("transactionsTable.boostagram.message")}:{" "}
-                                {tx.boostagram.message}
-                              </li>
-                              <li>
-                                {t("transactionsTable.boostagram.app")}:{" "}
-                                {tx.boostagram.app_name}
-                              </li>
-                              <li>
-                                {t("transactionsTable.boostagram.podcast")}:{" "}
-                                {tx.boostagram.podcast}
-                              </li>
-                            </ul>
-                          )}
-                        </div>
-                      )}
-                      {(tx.totalFees !== undefined || tx.location) && (
-                        <div className="my-2 flow-root">
-                          {tx.totalFees !== undefined && (
-                            <p className="float-left">
-                              <span className="font-bold">
-                                {t("transactionsTable.fee")}
-                              </span>
-                              <br />
-                              {getFormattedSats(tx.totalFees)}
-                            </p>
-                          )}
-                          {tx.location && (
-                            <a
-                              className="float-right"
-                              href={tx.location}
-                              target="_blank"
-                              rel="noreferrer noopener"
-                            >
-                              <Button
-                                primary
-                                label={t("transactionsTable.open_location")}
-                              />
-                            </a>
-                          )}
-                        </div>
-                      )}
-                      {tx.preimage && (
-                        <div className="my-2 flow-root">
-                          <p className="float-left break-all">
-                            <span className="font-bold">
-                              {t("transactionsTable.preimage")}
-                            </span>
-                            <br />
-                            {tx.preimage}
-                          </p>
-                        </div>
-                      )}
-                    </div>
-                  </Disclosure.Panel>
-                </>
+                ) : (
+                  <div className="flex justify-center items-center bg-green-100 rounded-full w-8 h-8">
+                    <ArrowDownIcon className="w-6 h-6 text-green-400" />
+                  </div>
+                )}
+              </div>
+              <div className="overflow-hidden mr-3">
+                <div
+                  className="
+      text-sm font-medium text-black truncate dark:text-white"
+                >
+                  <p className="truncate">
+                    {tx.publisherLink && tx.title ? (
+                      <a
+                        target="_blank"
+                        href={tx.publisherLink}
+                        rel="noopener noreferrer"
+                      >
+                        {tx.title}
+                      </a>
+                    ) : (
+                      tx.title ||
+                      tx.boostagram?.message ||
+                      (type == "incoming" ? "Received" : "Sent")
+                    )}
+                  </p>
+                </div>
+                <p className="text-xs text-gray-600 dark:text-neutral-400">
+                  {tx.date}
+                </p>
+              </div>
+              {tx.badges && (
+                <div className="ml-6 space-x-3">
+                  {tx.badges.map((badge) => (
+                    <Badge
+                      key={badge.label}
+                      label={badge.label}
+                      color={badge.color}
+                      textColor={badge.textColor}
+                    />
+                  ))}
+                </div>
               )}
-            </Disclosure>
+              <div className="flex ml-auto text-right space-x-3 shrink-0 dark:text-white">
+                <div>
+                  <p
+                    className={classNames(
+                      "text-sm font-medium",
+                      type == "incoming"
+                        ? "text-green-600 dark:color-green-400"
+                        : ""
+                    )}
+                  >
+                    {type == "outgoing" ? "-" : "+"}
+                    {getFormattedSats(tx.totalAmount)}
+                  </p>
+                  {!!tx.totalAmountFiat && (
+                    <p className="text-xs text-gray-600 dark:text-neutral-400">
+                      ~{tx.totalAmountFiat}
+                    </p>
+                  )}
+                </div>
+              </div>
+            </div>
           </div>
-        ))}
-      </div>
-    </>
+        );
+      })}
+    </div>
   );
 }
