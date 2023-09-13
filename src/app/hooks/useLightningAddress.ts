928214eed6343f98720a7d1ddc49d6b4138a37c9
Roland Bewick <roland.bewick@gmail.com>
2023-09-13 14:54:44 +0700

    fix: update lightning address on account switch

--- src/app/hooks/useLightningAddress.ts
+++ src/app/hooks/useLightningAddress.ts
@@ -1,11 +1,19 @@
 import { useEffect, useState } from "react";
+import { useAccount } from "~/app/context/AccountContext";
 import api from "~/common/lib/api";
 
 export const useLightningAddress = () => {
+  const { account } = useAccount();
   const [lightningAddress, setLightningAddress] = useState("");
-  const [loadingLightningAddress, setLoadingLightningAddress] = useState(true);
+  const [loadingLightningAddress, setLoadingLightningAddress] = useState(false);
 
   useEffect(() => {
+    // TODO: replace with connector-supported functionality
+    if (account?.connectorType !== "alby") {
+      setLightningAddress("");
+      setLoadingLightningAddress(false);
+      return;
+    }
     (async () => {
       setLoadingLightningAddress(true);
       const response = await api.getAccountInfo();
@@ -13,7 +21,7 @@ export const useLightningAddress = () => {
       if (lightningAddress) setLightningAddress(lightningAddress);
       setLoadingLightningAddress(false);
     })();
-  }, []);
+  }, [account?.connectorType]);
 
   return {
     loadingLightningAddress,
