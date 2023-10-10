4e4a2fbaa4b98d66091eefe41aca476fbeb6380f
Roland Bewick <roland.bewick@gmail.com>
2023-10-10 11:53:59 +0700

    chore: update comment to add more info about sendPaymentAsync

--- src/extension/background-script/actions/ln/sendPaymentAsync.ts
+++ src/extension/background-script/actions/ln/sendPaymentAsync.ts
@@ -19,9 +19,11 @@ export default async function sendPayment(message: MessageSendPayment) {
   const connector = await state.getState().getConnector();
 
   // NOTE: currently there is no way to know if the initial payment
-  // succeeds or not. The payment might not work at all or the http request might time out
-  // before the HODL invoice is paid or times out itself.
-  // any errors thrown by sendPayment will not be caught.
+  //   succeeds or not. The payment might not work at all or the http request might time out
+  //   before the HODL invoice is paid or times out itself.
+  //   any errors thrown by sendPayment will not be caught.
+  // NOTE: it is the receiver's responsibility to check if they have received the payment or not
+  //   and update the UI or re-prompt the user if they haven't received a payment.
   connector.sendPayment({
     paymentRequest,
   });
