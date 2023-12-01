2765db6a26fb954dee3f22df9d4d05f79713c6d4
pavanjoshi914 <pavanj914@gmail.com>
2023-12-01 16:51:24 +0530

    feat: add navigation to see all transactions page

--- src/app/screens/Transactions/index.tsx
+++ src/app/screens/Transactions/index.tsx
@@ -1,8 +1,12 @@
+import { CaretLeftIcon } from "@bitcoin-design/bitcoin-icons-react/filled";
 import Container from "@components/Container";
 import Loading from "@components/Loading";
 import TransactionsTable from "@components/TransactionsTable";
 import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+import Header from "~/app/components/Header";
+import IconButton from "~/app/components/IconButton";
 import { useAccount } from "~/app/context/AccountContext";
 import { useTransactions } from "~/app/hooks/useTransactions";
 
@@ -10,6 +14,8 @@ function Transactions() {
   const { t } = useTranslation("translation", {
     keyPrefix: "transactions",
   });
+
+  const navigate = useNavigate();
 
   const { accountLoading } = useAccount();
   const { transactions, isLoadingTransactions, loadTransactions } =
@@ -22,27 +28,40 @@ function Transactions() {
   }, [loadTransactions]);
 
   return (
-    <Container>
-      <h2 className="mt-12 mb-2 text-2xl font-bold dark:text-white">
+    <div className="h-full flex flex-col overflow-y-auto no-scrollbar">
+      <Header
+        headerLeft={
+          <IconButton
+            onClick={() => navigate(-1)}
+            icon={<CaretLeftIcon className="w-4 h-4" />}
+          />
+        }
+      >
         {t("title")}
-      </h2>
+      </Header>
 
-      <p className="mb-6 text-gray-500 dark:text-neutral-500">
-        {t("description")}
-      </p>
+      <Container>
+        <h2 className="mt-12 mb-2 text-2xl font-bold dark:text-white">
+          {t("title")}
+        </h2>
 
-      {isLoading ? (
-        <div className="flex justify-center mt-12">
-          <Loading />
-        </div>
-      ) : (
-        <div>
-          {transactions && transactions.length > 0 && (
-            <TransactionsTable transactions={transactions} />
-          )}
-        </div>
-      )}
-    </Container>
+        <p className="mb-6 text-gray-500 dark:text-neutral-500">
+          {t("description")}
+        </p>
+
+        {isLoading ? (
+          <div className="flex justify-center mt-12">
+            <Loading />
+          </div>
+        ) : (
+          <div>
+            {transactions && transactions.length > 0 && (
+              <TransactionsTable transactions={transactions} />
+            )}
+          </div>
+        )}
+      </Container>
+    </div>
   );
 }
 
