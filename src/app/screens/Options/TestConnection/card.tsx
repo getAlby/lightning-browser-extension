48ff2ee863931a1e4ae9fb56ed5bd379534e30c3
pavanjoshi914 <pavanj914@gmail.com>
2023-12-29 14:10:59 +0530

    feat: responsive testconnection separate account name and alias

--- src/app/screens/Options/TestConnection/card.tsx
+++ src/app/screens/Options/TestConnection/card.tsx
@@ -1,5 +1,6 @@
 type Props = {
   alias: string;
+  accountName: string;
   satoshis: string;
   fiat?: string;
   color: string;
@@ -8,6 +9,7 @@ type Props = {
 
 export default function TestConnectionResultCard({
   alias,
+  accountName,
   satoshis,
   fiat,
   color,
@@ -15,7 +17,12 @@ export default function TestConnectionResultCard({
 }: Props) {
   return (
     <div className={`${color} rounded-lg py-6 dark:bg-gray-600`}>
-      <p className="font-normal text-black ml-6 dark:text-white">{alias}</p>
+      <p className="font-normal text-black ml-6 dark:text-white break-words">
+        Account Name: {accountName}
+      </p>
+      <p className="font-normal text-black ml-6 dark:text-white">
+        Alias: {alias}
+      </p>
       <p className="text-2xl font-bold text-black ml-6 mt-2 dark:text-white">
         {satoshis}
       </p>
