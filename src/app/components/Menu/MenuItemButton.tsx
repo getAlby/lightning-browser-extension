f9c1d0b9ab545142acd40160deac707ff947d78c
pavanjoshi914 <pavanj914@gmail.com>
2024-01-15 12:31:07 +0530

    chore: few design correction

--- src/app/components/Menu/MenuItemButton.tsx
+++ src/app/components/Menu/MenuItemButton.tsx
@@ -22,7 +22,7 @@ function MenuItemButton({
         <button
           className={classNames(
             active ? "bg-gray-50 dark:bg-white/10" : "",
-            danger ? "text-red-700" : "text-gray-700",
+            danger ? "text-red-700" : "text-gray-800",
             disabled ? "cursor-not-allowed" : "cursor-pointer",
             "flex items-center block w-full text-left p-4 text-sm dark:text-white"
           )}
