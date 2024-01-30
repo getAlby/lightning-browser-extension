bcab94448ce1c4c8a597ff217e65b61c09440af1
Pavan Joshi <55848322+pavanjoshi914@users.noreply.github.com>
2024-01-31 03:38:08 +0530

    feat: locale switcher for initial setup (#2981)
    * feat: custom locale switcher
    
    * feat: correct layout
    
    * feat: set min-height for ui content including locale switcher
    
    * chore: remove explicit margin

--- src/app/router/Welcome/Welcome.tsx
+++ src/app/router/Welcome/Welcome.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Outlet, Route, HashRouter as Router, Routes } from "react-router-dom";
 import Container from "~/app/components/Container";
+import LocaleSwitcher from "~/app/components/LocaleSwitcher/LocaleSwitcher";
 import Toaster from "~/app/components/Toast/Toaster";
 import { AccountProvider } from "~/app/context/AccountContext";
 import { SettingsProvider } from "~/app/context/SettingsContext";
@@ -62,28 +63,33 @@ function Layout() {
   });
 
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="w-full">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-4">
-          <div className="text-center font-serif font-medium text-2xl mt-8 dark:text-white">
-            <p>
-              {t("welcome.title")}
-              <img
-                src="assets/icons/alby_icon_yellow.svg"
-                alt="Alby"
-                className="dark:hidden inline align-middle w-6 ml-2"
-              />
-              <img
-                src="assets/icons/alby_icon_yellow_dark.svg"
-                alt="Alby"
-                className="hidden dark:inline align-middle w-6 ml-2"
-              />
-            </p>
+    <div className="flex flex-col min-h-screen">
+      <div className="ml-6 mt-4">
+        <LocaleSwitcher className="text-sm border-transparent text-gray-600 hover:text-gray-700 bg-gray-100 dark:bg-surface-00dp dark:text-neutral-400 dark:hover:text-neutral-300" />
+      </div>
+      <div className="flex justify-center items-center">
+        <div className="w-full">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-8">
+            <div className="text-center font-serif font-medium text-2xl mt-8 dark:text-white">
+              <p>
+                {t("welcome.title")}
+                <img
+                  src="assets/icons/alby_icon_yellow.svg"
+                  alt="Alby"
+                  className="dark:hidden inline align-middle w-6 ml-2"
+                />
+                <img
+                  src="assets/icons/alby_icon_yellow_dark.svg"
+                  alt="Alby"
+                  className="hidden dark:inline align-middle w-6 ml-2"
+                />
+              </p>
+            </div>
           </div>
+          <Container maxWidth="xl">
+            <Outlet />
+          </Container>
         </div>
-        <Container maxWidth="xl">
-          <Outlet />
-        </Container>
       </div>
     </div>
   );
