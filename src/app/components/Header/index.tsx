f027a1da9baeef2cbdb5a619b97a82e0e528c0a4
Pavan Joshi <55848322+pavanjoshi914@users.noreply.github.com>
2024-01-31 15:36:14 +0530

    feat: responsive header, navigation and headings and avatars (#2959)
    * feat: add margins around navigation icons
    
    * feat: responsive headings in hader
    
    * chore: change class placements
    
    ---------
    
    Co-authored-by: René Aaron <rene@twentyuno.net>

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -8,9 +8,11 @@ function Header({ children, headerLeft, headerRight }: Props) {
   return (
     <div className="bg-white py-[6px] border-b border-gray-200 dark:bg-surface-01dp dark:border-neutral-700">
       <div className="flex justify-between items-center container max-w-screen-lg px-4 mx-auto">
-        <div className="w-8 h-8">{headerLeft}</div>
-        <h1 className="text-lg font-medium dark:text-white">{children}</h1>
-        <div className="w-8 h-8">{headerRight}</div>
+        <div className="w-8 h-8 mr-3">{headerLeft}</div>
+        <h1 className="text-lg font-medium dark:text-white overflow-hidden">
+          {children}
+        </h1>
+        <div className="w-8 h-8 ml-3">{headerRight}</div>
       </div>
     </div>
   );
