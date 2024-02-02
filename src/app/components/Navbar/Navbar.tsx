f4a210ecd913f7839acf3aba10f82cb12142fbbd
Pavan Joshi <55848322+pavanjoshi914@users.noreply.github.com>
2024-02-02 16:37:22 +0530

    chore: not to allow text to split up on next line (#3012)

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export default function Navbar({ children }: Props) {
   return (
-    <div className="py-[6px] bg-white border-b border-gray-200 dark:bg-surface-01dp dark:border-neutral-700">
+    <div className="py-[6px] bg-white border-b border-gray-200 dark:bg-surface-01dp dark:border-neutral-700 whitespace-nowrap">
       <div className="max-w-screen-lg px-4 flex justify-between items-center mx-auto w-full">
         <div className="flex items-center">
           <UserMenu />
