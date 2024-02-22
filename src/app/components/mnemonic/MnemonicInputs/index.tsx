cc36288fde784a3fa514721e05d8aa252ed528e5
René Aaron <rene@twentyuno.net>
2024-02-22 17:08:15 +0000

    fix: update flex layout

--- src/app/components/mnemonic/MnemonicInputs/index.tsx
+++ src/app/components/mnemonic/MnemonicInputs/index.tsx
@@ -46,10 +46,10 @@ export default function MnemonicInputs({
           const inputId = `mnemonic-word-${i}`;
           return (
             <div key={i} className="flex justify-center items-center gap-2">
-              <span className="flex w-6 text-gray-600 dark:text-neutral-400">
+              <span className="w-6 text-gray-600 dark:text-neutral-400">
                 {i + 1}.
               </span>
-              <div className="flex w-full">
+              <div className="w-full">
                 <Input
                   id={inputId}
                   autoFocus={!readOnly && i === 0}
