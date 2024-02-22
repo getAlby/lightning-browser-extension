fb016117a43daf41a56e2c639a55b0fb778c24fe
René Aaron <rene@twentyuno.net>
2024-02-22 17:30:54 +0000

    fix: cleanup icons

--- src/app/components/PasswordViewAdornment/index.tsx
+++ src/app/components/PasswordViewAdornment/index.tsx
@@ -21,16 +21,16 @@ export default function PasswordViewAdornment({ onChange, isRevealed }: Props) {
     <button
       type="button"
       tabIndex={-1}
-      className="flex justify-center items-center w-10 h-8"
+      className="flex justify-center items-center w-10 h-8 text-gray-800 dark:text-neutral-200"
       onClick={() => {
         setRevealed(!_isRevealed);
         onChange(!_isRevealed);
       }}
     >
       {_isRevealed ? (
-        <EyeCrossedIcon className="h-6 w-6 text-gray-800 dark:text-neutral-200" />
+        <EyeCrossedIcon className="h-6 w-6 " />
       ) : (
-        <PopiconsEyeLine className="h-5 w-5 text-gray-800 dark:text-neutral-200" />
+        <PopiconsEyeLine className="h-6 w-6" />
       )}
     </button>
   );
