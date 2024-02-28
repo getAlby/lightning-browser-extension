510cd91823b5f983e85de57308ebb97465b7a788
pavanjoshi914 <pavanj914@gmail.com>
2024-02-28 15:36:00 +0530

    fix: proper error handling

--- src/app/screens/LNURLWithdraw/index.tsx
+++ src/app/screens/LNURLWithdraw/index.tsx
@@ -83,12 +83,16 @@ function LNURLWithdraw() {
           msg.reply(response.data);
         }
       } else {
-        toast.error(`Error: ${response.data.reason}`);
+        throw new Error(response.data.reason);
       }
     } catch (e) {
       console.error(e);
-      if (e instanceof Error) {
-        toast.error(e.message);
+      if (axios.isAxiosError(e)) {
+        const error =
+          (e.response?.data as { reason?: string })?.reason || e.message;
+        toast.error(`Error: ${error}`);
+      } else if (e instanceof Error) {
+        toast.error(`Error: ${e.message}`);
       }
     } finally {
       setLoadingConfirm(false);
