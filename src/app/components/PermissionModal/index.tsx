3c0372cbc467c77d22708ed805f8562f1d3d9230
pavanjoshi914 <pavanj914@gmail.com>
2024-03-06 18:23:41 +0530

    refactor: permissiom modal

--- src/app/components/PermissionModal/index.tsx
+++ src/app/components/PermissionModal/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import Button from "~/app/components/Button";
 import Modal from "~/app/components/Modal";
 import PublisherCard from "~/app/components/PublisherCard";
@@ -21,8 +21,12 @@ export default function PermissionModal({
 }: Props) {
   const { t: tCommon } = useTranslation("common");
 
+  const { t } = useTranslation("components", {
+    keyPrefix: "permissions_modal",
+  });
+
   const navState = useNavigationState();
-  const [permissionOption, setPermissionOption] = useState("ask_every_time");
+  const [permissionOption, setPermissionOption] = useState("ask_everytime");
   const origin = navState.origin as OriginData;
 
   return (
@@ -42,63 +46,13 @@ export default function PermissionModal({
           isSmall={false}
           isCard={false}
         />
-        <ul className="m-4 flex flex-col gap-4">
-          <li className="flex gap-4 items-center">
-            <input
-              type="radio"
-              id="mode-companion"
-              name="mode"
-              value="companion"
-              checked={permissionOption === "ask_every_time"}
-              onChange={() => {
-                setPermissionOption("ask_every_time");
-              }}
-            />
-            <label
-              htmlFor="mode-companion"
-              className="inline-flex justify-between text-sm text-gray-500 cursor-pointer dark:hover:text-gray-300 dark:peer-checked:text-primary peer-checked:border-primary peer-checked:text-primary hover:text-gray-600 dark:text-gray-400 "
-            >
-              {`Ask everytime for ${permission}`}
-            </label>
-          </li>
+        <div className="m-4 flex flex-col gap-4">
+          <p className="text-sm font-bold">{t("set_permissions")}</p>
 
-          <li className="flex gap-4 items-center">
-            <input
-              type="radio"
-              id="mode-companion"
-              name="mode"
-              value="companion"
-              checked={permissionOption === "dont_ask_again_current"}
-              onChange={() => {
-                setPermissionOption("dont_ask_again_current");
-              }}
-            />
-            <label
-              htmlFor="mode-companion"
-              className="inline-flex justify-between text-sm  text-gray-500 cursor-pointer dark:hover:text-gray-300 dark:peer-checked:text-primary peer-checked:border-primary peer-checked:text-primary hover:text-gray-600 dark:text-gray-400 "
-            >
-              {`Don't ask again for ${permission}`}
-            </label>
-          </li>
-          <li className="flex gap-4 items-center">
-            <input
-              type="radio"
-              id="mode-companion"
-              name="mode"
-              value="companion"
-              checked={permissionOption === "dont_ask_again_all"}
-              onChange={() => {
-                setPermissionOption("dont_ask_again_all");
-              }}
-            />
-            <label
-              htmlFor="mode-companion"
-              className="inline-flex justify-between text-sm  text-gray-500 cursor-pointer dark:hover:text-gray-300 dark:peer-checked:text-primary peer-checked:border-primary peer-checked:text-primary hover:text-gray-600 dark:text-gray-400 "
-            >
-              {`Don't ask again for any Nostr Requests`}
-            </label>
-          </li>
-        </ul>
+          <ListItem checkedValue="ask_everytime" />
+          <ListItem checkedValue="dont_ask_current" />
+          <ListItem checkedValue="dont_ask_any" />
+        </div>
 
         <div className="flex justify-center mt-6 w-64 mx-auto">
           <Button
@@ -111,4 +65,39 @@ export default function PermissionModal({
       </div>
     </Modal>
   );
+
+  type ListItemProps = {
+    checkedValue: "ask_everytime" | "dont_ask_current" | "dont_ask_any";
+  };
+
+  function ListItem({ checkedValue }: ListItemProps) {
+    return (
+      <div className="flex flex-row gap-4 items-center">
+        <input
+          type="radio"
+          id="set-permission"
+          name="permission"
+          value="permission"
+          checked={permissionOption === checkedValue}
+          onChange={() => {
+            setPermissionOption(checkedValue);
+          }}
+        />
+        <label
+          htmlFor="set-permission"
+          className="text-sm text-gray-500 cursor-pointer dark:hover:text-gray-300 dark:peer-checked:text-primary peer-checked:border-primary peer-checked:text-primary hover:text-gray-600 dark:text-gray-400 "
+        >
+          <Trans
+            i18nKey={checkedValue}
+            t={t}
+            values={{ permission }}
+            components={[
+              // eslint-disable-next-line react/jsx-key
+              <b></b>,
+            ]}
+          />
+        </label>
+      </div>
+    );
+  }
 }
