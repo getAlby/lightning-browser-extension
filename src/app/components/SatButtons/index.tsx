b5a78091b2a53de7a75db898c7ca710fafbd8cb8
René Aaron <rene@twentyuno.net>
2024-03-13 22:45:33 +0100

    fix: add satoshi icon

--- src/app/components/SatButtons/index.tsx
+++ src/app/components/SatButtons/index.tsx
@@ -1,3 +1,4 @@
+import SatoshiIcon from "~/app/icons/SatoshiIcon";
 import Button from "../Button";
 
 type Props = {
@@ -15,16 +16,7 @@ function SatButtons({ onClick, disabled, min, max }: Props) {
       {sizes.map((size) => (
         <Button
           key={size}
-          icon={
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-              className="w-4 h-4"
-            >
-              <path d="M12.75 18.5V21h-1.5v-2.5h1.5zM17 16.75H7v-1.5h10v1.5zm0-4H7v-1.5h10v1.5zm0-4H7v-1.5h10v1.5zM12.75 3v2.5h-1.5V3h1.5z" />
-            </svg>
-          }
+          icon={<SatoshiIcon className="w-4 h-4" />}
           label={size / 1000 + "k"}
           onClick={() => onClick(size.toString())}
           fullWidth
