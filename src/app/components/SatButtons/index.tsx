aa22c75fd5ce721681d8db70262b3afb0e776041
René Aaron <rene@twentyuno.net>
2024-03-13 22:13:41 +0100

    fix: svg optimize

--- src/app/components/SatButtons/index.tsx
+++ src/app/components/SatButtons/index.tsx
@@ -17,17 +17,12 @@ function SatButtons({ onClick, disabled, min, max }: Props) {
           key={size}
           icon={
             <svg
-              className="w-4 h-4"
-              data-v-52a72b4a=""
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
               fill="currentColor"
+              viewBox="0 0 24 24"
+              className="w-4 h-4"
             >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M12.75 18.5V21h-1.5v-2.5h1.5zM17 16.75H7v-1.5h10v1.5zM17 12.75H7v-1.5h10v1.5zM17 8.75H7v-1.5h10v1.5zM12.75 3v2.5h-1.5V3h1.5z"
-              ></path>
+              <path d="M12.75 18.5V21h-1.5v-2.5h1.5zM17 16.75H7v-1.5h10v1.5zm0-4H7v-1.5h10v1.5zm0-4H7v-1.5h10v1.5zM12.75 3v2.5h-1.5V3h1.5z" />
             </svg>
           }
           label={size / 1000 + "k"}
