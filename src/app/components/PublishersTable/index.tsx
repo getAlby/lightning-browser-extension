c09d55df7d8d170b6b0265a5f72aa353ca4729b3
René Aaron <rene@twentyuno.net>
2024-03-13 21:24:43 +0100

    fix: publishers table

--- src/app/components/PublishersTable/index.tsx
+++ src/app/components/PublishersTable/index.tsx
@@ -53,7 +53,7 @@ export default function PublishersTable({
                       </p>
                       <BadgesList allowance={publisher} />
                     </div>
-                    <div className="text-sm space-x-2 text-gray-500 dark:text-neutral-400">
+                    <div className="text-sm space-x-2 text-gray-600 dark:text-neutral-400">
                       <span>{publisher.host}</span>
                       <span>•</span>
                       <span>
@@ -84,7 +84,7 @@ export default function PublishersTable({
                 </div>
               </td>
               <td className="w-10">
-                <PopiconsChevronRightLine className="h-6 w-6 text-gray-500" />
+                <PopiconsChevronRightLine className="h-6 w-6 text-gray-600 dark:text-neutral-400" />
               </td>
             </tr>
           ))}
