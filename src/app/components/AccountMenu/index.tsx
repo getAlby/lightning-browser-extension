0d0f244c2fc35ec1e761c7162fab28818dea456a
René Aaron <rene@twentyuno.net>
2024-03-13 21:08:20 +0100

    fix: accountmenu, add missing icon

--- src/app/components/AccountMenu/index.tsx
+++ src/app/components/AccountMenu/index.tsx
@@ -1,8 +1,7 @@
 import {
-  PopiconsBookmarkSolid,
   PopiconsChevronBottomLine,
-  PopiconsPlusSolid,
-  PopiconsWalletSolid,
+  PopiconsCirclePlusLine,
+  PopiconsWalletLine,
 } from "@popicons/react";
 import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
@@ -15,6 +14,7 @@ import { useAccounts } from "~/app/context/AccountsContext";
 import { isAlbyLNDHubAccount, isAlbyOAuthAccount } from "~/app/utils";
 import utils from "~/common/lib/utils";
 
+import PopiconsGlobeLine from "~/app/icons/popicons/GlobeLine";
 import Menu from "../Menu";
 
 export type Props = {
@@ -57,7 +57,7 @@ function AccountMenu({ showOptions = true }: Props) {
   }
 
   return (
-    <div className="relative pl-2 flex justify-end w-72">
+    <div className="relative pl-2 flex justify-end w-72 text-gray-800 dark:text-neutral-200">
       <Menu as="div">
         <Menu.Button className="h-full px-2 rounded-md hover:bg-gray-100 dark:hover:bg-white/10 transition-colors duration-200">
           <div className="flex items-center">
@@ -78,7 +78,7 @@ function AccountMenu({ showOptions = true }: Props) {
             >
               <div
                 title={title || ""}
-                className="text-sm font-medium text-gray-700 dark:text-neutral-400 text-ellipsis overflow-hidden whitespace-nowrap"
+                className="text-sm font-medium text-ellipsis overflow-hidden whitespace-nowrap"
               >
                 {accountLoading ? (
                   <SkeletonLoader className="w-20" />
@@ -87,7 +87,7 @@ function AccountMenu({ showOptions = true }: Props) {
                 )}
               </div>
             </div>
-            <PopiconsChevronBottomLine className="h-5 w-5 dark:text-white" />
+            <PopiconsChevronBottomLine className="h-4 w-4" />
             <span className="sr-only">{t("screen_reader")}</span>
           </div>
         </Menu.Button>
@@ -98,7 +98,7 @@ function AccountMenu({ showOptions = true }: Props) {
                 !title && !balancesDecorated ? "w-28" : ""
               }`}
             >
-              <span className="text-xs text-gray-500 dark:text-neutral-300">
+              <span className="text-xs text-gray-600 dark:text-neutral-400">
                 {tCommon("balance")}
               </span>
               <p className="flex justify-between">
@@ -109,7 +109,7 @@ function AccountMenu({ showOptions = true }: Props) {
                     balancesDecorated.accountBalance
                   )}
                 </span>
-                <span className="text-gray-500 dark:text-neutral-300">
+                <span className="text-gray-600 dark:text-neutral-400">
                   {accountLoading ? (
                     <SkeletonLoader className="w-12" />
                   ) : (
@@ -126,7 +126,7 @@ function AccountMenu({ showOptions = true }: Props) {
               openOptions(`accounts/${authAccount?.id}`);
             }}
           >
-            <PopiconsWalletSolid className="h-5 w-5 mr-2 text-gray-700 dark:text-neutral-300 shrink-0" />
+            <PopiconsWalletLine className="w-4 h-4 mr-2 shrink-0" />
             {t("options.account.wallet_settings")}
           </Menu.ItemButton>
           {(isAlbyLNDHubAccount(
@@ -139,29 +139,7 @@ function AccountMenu({ showOptions = true }: Props) {
                 window.open(`https://getalby.com/user`, "_blank");
               }}
             >
-              <svg
-                className="h-5 w-5 mr-2 text-gray-700 dark:text-neutral-300 shrink-0"
-                data-v-52a72b4a=""
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10.51 6.617c-.518 1.341-.855 3.245-.855 5.385 0 2.14.337 4.044.856 5.385.26.673.552 1.166.838 1.478.285.312.506.385.651.385.145 0 .366-.073.652-.385.285-.312.577-.805.837-1.478.52-1.341.856-3.245.856-5.385 0-2.14-.337-4.044-.856-5.385-.26-.673-.552-1.166-.838-1.478-.285-.312-.506-.385-.651-.385-.145 0-.366.073-.651.385-.286.312-.578.805-.838 1.478zm-.268-2.49c.455-.499 1.048-.873 1.758-.873s1.303.374 1.758.872c.455.497.83 1.175 1.13 1.95.601 1.553.957 3.649.957 5.926s-.356 4.373-.957 5.926c-.3.774-.675 1.453-1.13 1.95-.455.497-1.048.872-1.758.872s-1.303-.375-1.758-.872c-.455-.497-.83-1.175-1.13-1.95-.601-1.553-.957-3.649-.957-5.926s.356-4.373.957-5.926c.3-.775.675-1.453 1.13-1.95z"
-                  clipRule="evenodd"
-                ></path>
-                <path
-                  fillRule="evenodd"
-                  d="M12 4.746a7.25 7.25 0 100 14.5 7.25 7.25 0 000-14.5zm-8.75 7.25a8.75 8.75 0 1117.5 0 8.75 8.75 0 01-17.5 0z"
-                  clipRule="evenodd"
-                ></path>
-                <path
-                  fillRule="evenodd"
-                  d="M3.25 11.99a.75.75 0 01.75-.75l16 .012a.75.75 0 110 1.5l-16-.011a.75.75 0 01-.75-.75z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
+              <PopiconsGlobeLine className="w-4 h-4 mr-2 shrink-0" />
               {t("options.account.go_to_web_wallet")} →
             </Menu.ItemButton>
           )}
@@ -213,16 +191,8 @@ function AccountMenu({ showOptions = true }: Props) {
                   openOptions("accounts/new");
                 }}
               >
-                <PopiconsPlusSolid className="h-5 w-5 mr-2 text-gray-700 dark:text-neutral-300" />
+                <PopiconsCirclePlusLine className="h-4 w-4 mr-2 shrink-0" />
                 {t("options.account.add")}
-              </Menu.ItemButton>
-              <Menu.ItemButton
-                onClick={() => {
-                  openOptions("accounts");
-                }}
-              >
-                <PopiconsBookmarkSolid className="h-5 w-5 mr-2 text-gray-700 dark:text-neutral-300" />
-                {t("options.account.manage")}
               </Menu.ItemButton>
             </>
           )}
