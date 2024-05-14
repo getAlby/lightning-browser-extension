0167573df622b1cce17b1cf18fa487de7ac3001a
pavanjoshi914 <pavanj914@gmail.com>
2024-05-14 16:50:38 +0530

    feat: update alert dialogs

--- src/app/components/Alert/index.tsx
+++ src/app/components/Alert/index.tsx
@@ -9,11 +9,11 @@ export default function Alert({ type, children }: Props) {
   return (
     <div
       className={classNames(
-        "rounded-md p-3",
+        "border rounded-md p-3",
         type == "warn" &&
-          "text-orange-700 dark:text-orange-300 bg-orange-50 dark:bg-orange-900 border border-orange-100",
+          "text-orange-700 dark:text-orange-300 bg-orange-50 dark:bg-orange-900  border-orange-100 dark:border-orange-900",
         type == "info" &&
-          "text-blue-700 dark:text-blue-300 bg-blue-50 dark:bg-blue-900 border border-blue-100"
+          "text-blue-700 dark:text-blue-300 bg-blue-50 dark:bg-blue-900 border-blue-100 dark:border-blue-900"
       )}
     >
       {children}
