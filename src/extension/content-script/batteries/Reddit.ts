a75430cf26084d07889a8665da7a2e63edff7dde
pavanjoshi914 <pavanj914@gmail.com>
2024-12-10 11:34:01 +0530

    fix: reddit battery

--- src/extension/content-script/batteries/Reddit.ts
+++ src/extension/content-script/batteries/Reddit.ts
@@ -4,30 +4,28 @@ import { findLightningAddressInText, setLightningData } from "./helpers";
 const urlMatcher = /^https:\/\/www.reddit\.com\/user\/(\w+).*/;
 
 function battery(): void {
-  const descriptionElement = document.querySelector<HTMLMetaElement>(
-    'head > meta[name="description"]'
+  const descriptionElement = document.querySelector<HTMLElement>(
+    'p[data-testid="profile-description"]'
   );
-  const imageUrl = document.querySelector<HTMLMetaElement>(
-    'head > meta[property="og:image"]'
-  )?.content;
+
+  const imageUrl = document.querySelector<HTMLImageElement>(
+    'img[data-testid="profile-icon"]'
+  )?.src;
 
   if (!descriptionElement || !imageUrl) {
     return;
   }
 
-  const content = descriptionElement.content.split(/:(.*)/s);
-  const userName = content[0];
-  const description =
-    document.querySelector<HTMLMetaElement>(
-      `h4 + a[href*='user/${userName.split("/")[1]}'] + div`
-    )?.textContent ?? content[1].slice(1);
+  const content = descriptionElement.textContent || "";
+  const userName =
+    document.querySelector("aside div h2")?.textContent?.trim() || "";
 
   let match;
   let recipient;
   // attempt to extract lnurlp: from the description text
-  if ((match = (description || "").match(/lnurlp:(\S+)/i))) {
+  if ((match = (content || "").match(/lnurlp:(\S+)/i))) {
     recipient = match[1];
-  } else if ((match = findLightningAddressInText(description))) {
+  } else if ((match = findLightningAddressInText(content || ""))) {
     recipient = match;
   }
   // if we still did not find anything ignore it.
@@ -40,7 +38,7 @@ function battery(): void {
       method: "lnurl",
       address: recipient,
       ...getOriginData(),
-      description,
+      description: content,
       icon: imageUrl,
       name: userName,
     },
