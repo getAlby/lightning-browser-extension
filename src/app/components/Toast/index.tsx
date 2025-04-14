77a58a18a453d486ee08364e4dcfea61b17f4092
pavanjoshi914 <pavanj914@gmail.com>
2025-04-14 12:51:09 +0530

    fix: cross icon position

--- src/app/components/Toast/index.tsx
+++ src/app/components/Toast/index.tsx
@@ -1,5 +1,5 @@
-import { PopiconsXLine } from "@popicons/react";
 import { Transition } from "@headlessui/react";
+import { PopiconsXLine } from "@popicons/react";
 import { ReactNode } from "react";
 import {
   CheckmarkIcon,
@@ -40,23 +40,21 @@ const toast: ToastMethods = {
           show={t.visible}
         >
           <div className="bg-white dark:bg-surface-02dp px-4 py-3 drop-shadow-lg rounded-lg overflow-hidden flex flex-row items-center gap-3 text-gray-800 dark:text-neutral-200 max-w-sm sm:max-w-lg w-full break-words">
-            <div className="flex items-start gap-2 w-full">
-              <div className="shrink-0">
-                {type == "success" && <CheckmarkIcon />}
-                {type == "error" && <ErrorIcon />}
-              </div>
-              <div className="flex-1 text-sm max-h-[200px] overflow-auto">
-                {children}
-              </div>
-              {/* Add close icons for toasts that are displayed for a longer time or with longer legnth*/}
-              {(options?.duration && options?.duration > 10_000) ||
-              (typeof children === "string" && children.length > 50) ? (
-                <PopiconsXLine
-                  className="w-4 h-4 cursor-pointer mt-1 shrink-0 hover:opacity-80 transition-opacity"
-                  onClick={() => hotToast.dismiss(t.id)}
-                />
-              ) : null}
+            <div className="shrink-0">
+              {type == "success" && <CheckmarkIcon />}
+              {type == "error" && <ErrorIcon />}
             </div>
+            <div className="flex-1 text-sm max-h-[200px] overflow-auto">
+              {children}
+            </div>
+            {/* Add close icons for toasts that are displayed for a longer time or with longer legnth*/}
+            {(options?.duration && options?.duration > 10_000) ||
+            (typeof children === "string" && children.length > 50) ? (
+              <PopiconsXLine
+                className="w-4 h-4 cursor-pointer mt-1 shrink-0 hover:opacity-80 transition-opacity"
+                onClick={() => hotToast.dismiss(t.id)}
+              />
+            ) : null}
           </div>
         </Transition>
       ),
