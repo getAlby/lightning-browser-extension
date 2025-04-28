4f92d13ad73b55e42a3f1de4539d2af3f5a4db57
pavanjoshi914 <pavanj914@gmail.com>
2025-04-28 16:16:08 +0530

    fix: remove battries from extension

--- src/extension/content-script/batteries/helpers.ts
+++ src/extension/content-script/batteries/helpers.ts
@@ -9,45 +9,6 @@ enum ExtensionIcon {
   Active = "alby_icon_green",
 }
 
-/**
- * Inspects the DOM for an element matching the required selector
- * If no element is found it registers an observer and resolves if/when one is.
- * @param selector - DOM selector to check readiness of
- */
-export const elementReady = async (selector: string): Promise<Element> => {
-  return new Promise((resolve) => {
-    const el = document.querySelector(selector);
-    if (el) {
-      resolve(el);
-      return;
-    }
-    new MutationObserver((mutationRecords, observer) => {
-      const element = document.querySelector(selector);
-      if (element) {
-        resolve(element);
-        observer.disconnect();
-      }
-    }).observe(document.documentElement, {
-      childList: true,
-      subtree: true,
-    });
-  });
-};
-
-export const findLightningAddressInText = (text: string): string | null => {
-  // The second lightning emoji is succeeded by an invisible
-  // variation selector-16 character: https://regex101.com/r/Bf2GpN/1
-  const match = text.match(
-    /((⚡|⚡️):?|lightning:|lnurl:)\s?([\w-.]+@[\w-.]+[.][\w-.]+)/i
-  );
-  if (match) return match[3];
-  const matchAlbyLink = text.match(
-    /http(s)?:\/\/(www[.])?getalby\.com\/p\/(\w+)/
-  );
-  if (matchAlbyLink) return matchAlbyLink[3] + "@getalby.com";
-  return null;
-};
-
 export const setLightningData = (data: [Battery]): void => {
   browser.runtime.sendMessage({
     application: "LBE",
