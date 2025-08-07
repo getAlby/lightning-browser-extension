0d47af821e99ba51fe87565b1ca1742bd72e9c08
Dunsin <ayodejiabisuwa23@gmail.com>
2025-08-07 15:08:28 +0100

    fix:  style more consistent with actual design

--- src/app/screens/Onboard/PinExtension/index.tsx
+++ src/app/screens/Onboard/PinExtension/index.tsx
@@ -31,42 +31,47 @@ export default function PinExtension() {
 
   return (
     <div className="flex flex-col items-center text-lg ">
-      <div className="shadow-lg rounded-xl bg-white dark:bg-surface-02dp p-10 md:max-w-xl">
-        <h1 className="text-2xl  font-bold dark:text-white text-center">
-          {t("title")}
-        </h1>
-        <div className="mt-4 w-full flex justify-center">{getImage()}</div>
-        <div className="text-gray-500 mt-6 dark:text-gray-400 text-sm">
-          <Trans
-            i18nKey={"explanation"}
-            t={t}
-            components={[
-              // eslint-disable-next-line react/jsx-key
-              <img
-                src="assets/icons/extension.svg"
-                className="w-5 inline dark:invert mr-2"
-              />,
-              // eslint-disable-next-line react/jsx-key
-              <span className="block h-4" />,
-              // eslint-disable-next-line react/jsx-key
-              <img
-                src="assets/icons/keyboard.svg"
-                className="w-5 inline dark:invert mr-2"
-              />,
-            ]}
-          />
-        </div>
+      <div className="shadow-lg rounded-xl bg-white dark:bg-surface-02dp p-10 md:max-w-xl w-full mx-auto">
+        <div className="md:max-w-[512px] mx-auto">
+          <h1 className="text-2xl  font-bold dark:text-white text-center">
+            {t("title")}
+          </h1>
 
-        {/* keyboard shortcut */}
-        <div className="flex justify-center gap-3 mt-6">
-          <div className="text-black dark:text-white text-xl rounded-xl py-4 px-5 border-2 border-primary font-bold">
-            {getOS() === "MacOS"
-              ? t("keyboard_shortcut.mac_modifier_key")
-              : t("keyboard_shortcut.windows_modifier_key")}
-          </div>
+          <div className="flex flex-col  items-center mt-4 max-w-[396px] w-full mx-auto">
+            <div className="flex justify-center">{getImage()}</div>
+            <div className="text-gray-500 mt-8 dark:text-gray-400 text-sm">
+              <Trans
+                i18nKey={"explanation"}
+                t={t}
+                components={[
+                  // eslint-disable-next-line react/jsx-key
+                  <img
+                    src="assets/icons/extension.svg"
+                    className="w-5 inline dark:invert mr-2"
+                  />,
+                  // eslint-disable-next-line react/jsx-key
+                  <span className="block h-5" />,
+                  // eslint-disable-next-line react/jsx-key
+                  <img
+                    src="assets/icons/keyboard.svg"
+                    className="w-5 inline dark:invert mr-2"
+                  />,
+                ]}
+              />
+            </div>
 
-          <div className="text-black dark:text-white text-xl rounded-xl py-4 px-5 border-2 border-[#F8C455] font-bold">
-            {t("keyboard_shortcut.second_key")}
+            {/* keyboard shortcut */}
+            <div className="flex w-full md:max-w-[152px] justify-center gap-3 mt-8">
+              <p className="text-black dark:text-white text-xl rounded-xl py-4 px-6 border-2 border-primary font-bold">
+                {getOS() === "MacOS"
+                  ? t("keyboard_shortcut.mac_modifier_key")
+                  : t("keyboard_shortcut.windows_modifier_key")}
+              </p>
+
+              <p className="text-black dark:text-white text-xl rounded-xl py-4 px-6 border-2 border-[#F8C455] font-bold">
+                {t("keyboard_shortcut.second_key")}
+              </p>
+            </div>
           </div>
         </div>
       </div>
