1f385176cd2c30352e6192a2dcde01bf4056db73
pavanjoshi914 <pavanj914@gmail.com>
2025-09-11 17:40:08 +0530

    feat: allow to connect cln nodes via nwc

--- jest.config.js
+++ jest.config.js
@@ -20,7 +20,6 @@ module.exports = {
   transform: {
     "^.+\\.(t|j)sx?$": ["@swc/jest", swcConfig],
   },
-  transformIgnorePatterns: ["node_modules/(?!(lnmessage))/"],
   moduleNameMapper: {
     "^dexie$": require.resolve("dexie"),
     // needs to align with "tsconfig.json"-paths
