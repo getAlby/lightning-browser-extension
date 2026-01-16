9947915f1aac6d12894468364500660b075be129
keshav0479 <keshav.rsk07@gmail.com>
2026-01-17 02:12:00 +0530

    fix: show ln address and qr for galoy

--- src/extension/background-script/connectors/galoy.ts
+++ src/extension/background-script/connectors/galoy.ts
@@ -94,6 +94,9 @@ class Galoy implements Connector {
               id
               username
           }
+          globals {
+            lightningAddressDomain
+          }
         }
       `,
     };
@@ -106,9 +109,24 @@ class Galoy implements Connector {
       const alias = data.me.username
         ? data.me.username.substr(0, 10)
         : data.me.id.substr(0, 8);
+
+      let domain = data.globals?.lightningAddressDomain;
+      const username = data.me.username;
+
+      // legacy domain to blink.sv map
+      if (domain === "pay.bbw.sv") {
+        domain = "blink.sv";
+      }
+
+      let lightningAddress: string | undefined;
+      if (username && domain) {
+        lightningAddress = `${username}@${domain}`;
+      }
+
       return {
         data: {
           alias,
+          lightning_address: lightningAddress,
         },
       };
     });
