3a65151405600bf3d46f6d068650579f083b2823
Jarvis3000Git <jarvis-bounty-bot@tuta.io>
2026-03-05 17:44:08 +0000

    feat: add tipping support for Reddit profiles

---
+++ src/extension/content-script/batteries/Reddit.ts
@@ -0,0 +1,50 @@
+import getOriginData from "../originData";
+import { findLightningAddressInText, setLightningData } from "./helpers";
+
+const urlMatcher = /https?:\/\/(?:www\.)?reddit\.com\/user\/([^/]+)/;
+
+const battery = (): void => {
+  // Reddit User Profile Extraktion
+  const bioSelectors = [
+    "#profile--about-card p",
+    "[data-testid='user-description']",
+    ".ProfileSidebar__about"
+  ];
+  
+  let bioText = "";
+  for (const selector of bioSelectors) {
+    const element = document.querySelector(selector);
+    if (element) {
+      bioText = (element as HTMLElement).innerText;
+      if (bioText) break;
+    }
+  }
+
+  const address = findLightningAddressInText(bioText);
+  if (!address) return;
+
+  const name = document.querySelector("h1")?.innerText || 
+               document.querySelector("[data-testid='user-name']")?.innerText || 
+               "Reddit User";
+
+  const icon = document.querySelector("img[src*='avatar']")?.getAttribute("src") || 
+               "";
+
+  setLightningData([
+    {
+      method: "lnurl",
+      address: address,
+      ...getOriginData(),
+      description: bioText.substring(0, 160),
+      name: name,
+      icon: icon,
+    },
+  ]);
+};
+
+const Reddit = {
+  urlMatcher,
+  battery,
+};
+
+export default Reddit;
